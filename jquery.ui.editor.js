e3de66f712d280bc86f7aa810caf0430e6e441ff
Michael Robinson <mike@panmedia.co.nz>
2011-11-14 11:08:28 +1300

    Moved view source button into a plugin.

--- jquery.ui.editor.js
+++ jquery.ui.editor.js
@@ -93,63 +93,6 @@
                     this.element.removeClass('ui-widget-editor-guides');
                 }
             },
-            view_source: {
-                title: 'View / Edit Source',
-                icons: {
-                    primary: 'ui-icon-view-source'
-                },
-                classes: 'ui-editor-icon ui-widget-editor-button-view-source',
-                click: function() {
-                    var editorInstance = this,
-                        dialog = $('.ui-widget-editor-dialog-view-source');
-
-                    if (!dialog.length) {
-                        dialog = $('<div style="display:none" class="ui-widget-editor-dialog-view-source">\
-                                        <textarea></textarea>\
-                                    </div>').appendTo(this._editor.toolbar);
-                    }
-
-                    dialog.dialog({
-                        modal: false,
-                        width: 600,
-                        height: 400,
-                        resizable: true,
-                        title: 'View Source',
-                        dialogClass: this.options.dialogClass + ' ui-widget-editor-view-source',
-                        show: this.options.dialogShowAnimation,
-                        hide: this.options.dialogHideAnimation,
-                        buttons: [
-                            {
-                                text: 'Reload Source',
-                                'class': 'reload-source',
-                                click: function() {
-                                    $(this).find('textarea').val(editorInstance.html());
-                                }
-                            },
-                            {
-                                text: 'Apply Source',
-                                'class': 'apply-source',
-                                click: function() {
-                                    editorInstance.html($(this).find('textarea').val());
-                                }
-                            }
-                        ],
-                        open: function() {
-                            editorInstance._dialog.applyButtonIcon('reload-source', 'refresh');
-                            editorInstance._dialog.applyButtonIcon('apply-source', 'circle-check');
-
-                            $(this).find('textarea').val(editorInstance.html());
-                        },
-                        close: function() {
-                            $(this).dialog('destroy');
-                        }
-                    }).dialog('open');
-                },
-                destroy: function() {
-                    var dialog = $('.ui-widget-editor-dialog-view-source');
-                    if (dialog.length) dialog.dialog('close');
-                }
-            },
             unorderedList: {
                 title: 'Unordered List',
                 icons: {
@@ -624,7 +567,7 @@
 
                 buttonOrder = [
                     ['save', 'cancel', 'showGuides'],
-                    ['view_source'],
+                    ['viewSource'],
                     ['undo', 'redo'],
                     ['alignLeft', 'center', 'justify', 'alignRight'],
                     ['bold', 'italic', 'underline', 'strikethrough'],
@@ -1829,7 +1772,7 @@
         },
         
         destroy: function() {
-            // Trigger buttons' destroy handlers
+            
             var editorInstance = this;
             editorInstance._editor.toolbar.find('button').each(function() {
                 var data = $(this).data(editorInstance._data.names.button);
