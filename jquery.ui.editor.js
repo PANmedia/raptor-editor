9d1534326a89ec5eb76bd006118ecb5caed4eebe
Michael Robinson <mike@panmedia.co.nz>
2011-11-14 11:14:05 +1300

    Moved show guides into a plugin

--- jquery.ui.editor.js
+++ jquery.ui.editor.js
@@ -81,18 +81,6 @@
                     }
                 }
             },
-            showGuides: {
-                title: 'Show Guides',
-                icons: {
-                    primary: 'ui-icon-pencil'
-                },
-                click: function() {
-                    this.element.toggleClass('ui-widget-editor-guides');
-                },
-                destroy: function() {
-                    this.element.removeClass('ui-widget-editor-guides');
-                }
-            },
             unorderedList: {
                 title: 'Unordered List',
                 icons: {
