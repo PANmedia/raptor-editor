eb0f62827aeec0a1bed113265578eeffc873acc7
Michael Robinson <mike@panmedia.co.nz>
2011-11-14 12:18:55 +1300

    Moved cancel button into a plugin

---
+++ plugins/cancel/jquery.ui.editor.cancel.js
@@ -0,0 +1,31 @@
+(function($) {
+    $.ui.editor.addButton('cancel', {
+        title: 'Cancel',
+        icons: {
+            primary: 'ui-icon-cancel'
+        },
+        click: function(event, button) {
+            // If the user has provided or bound their own cancel function 
+            // Allow them to cancel the default
+            if (this._trigger('cancel')) {
+                // confirm
+                var editorInstance = this,
+                    destroy = function() {
+                        editorInstance._content.reset.call(editorInstance);
+                        editorInstance.destroy();
+                    };
+                if (!this._content.dirtyBlocksExist.call(this)) {
+                    destroy();
+                } else {
+                    this._dialog.confirmation.show.call(this, {
+                        message: 'Are you sure you want to stop editing? <br/><br/>All changes will be lost!',
+                        title: 'Confirm Cancel Editing',
+                        ok: function(){
+                            destroy();
+                        }
+                    });
+                }
+            }
+        }
+    });
+})(jQuery);
