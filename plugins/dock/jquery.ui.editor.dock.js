5b5c2c3319e295646c9606f2a396a2313c93d023
Michael Robinson <mike@panmedia.co.nz>
2011-11-14 19:18:11 +1300

    Added more strict checks - for jQuery, jQuery UI Dialog, jQuery.cookie.
    Added persistence option.
    
    Added code in _init that fires plugin initialization events.
    
    Deleted some redundant code.
    
    Added some debugging statements detailing plugins, buttons and options that are added.
    
    Maded docking / detaching animate & reposition unsavedEditWarning nicely if it is loaded.
    
    Split positioning into its own function in unsavedEditWarning plugin - so other plugins may use this to reposition the plugin if they cause the editing element to change position / dimensions.
    
    Changed fontsize to use document.execCommand.
    
    Updated viewSource plugin so that its textarea is updated as one types into the editable element.

--- plugins/dock/jquery.ui.editor.dock.js
+++ plugins/dock/jquery.ui.editor.dock.js
@@ -1,6 +1,10 @@
 (function($) {
     
     $.ui.editor.addPlugin('dock', {
+        docked: false,
+        initialize: function() {
+            
+        },
         destroy: function() {
             var spacer = $('.ui-widget-editor-dock-spacer');
             if (spacer.length) spacer.hide('fast');
@@ -15,15 +19,12 @@
         },
         click: function(event, button) {
             var dialog = this._editor.toolbar.parent();
+                            
             dialog.toggleClass('ui-widget-editor-docked');
             this._editor.toolbar.toggleClass('ui-dialog-content').find('.ui-widget-editor-inner').toggleClass('ui-widget-header');
             
-            var spacer = $('.ui-widget-editor-dock-spacer');
-            if (!spacer.length) spacer = $('<div class="ui-widget-editor-dock-spacer"></div>').prependTo('body');
-
-            spacer.height(this._editor.toolbar.outerHeight()).toggle('fast');
-            
-            if (!$(button).hasClass('ui-icon-pin-w')) {  // Dock                
+            if (!this._plugins.dock.docked) {  // Dock
+                this._plugins.dock.docked = true;
                 $(button).find('span.ui-button-icon-primary')
                         .removeClass('ui-icon-pin-s').addClass('ui-icon-pin-w');
                 if (this.options.customTooltips) {
@@ -32,6 +33,7 @@
                     });
                 }
             } else {    // Detach
+                this._plugins.dock.docked = false;                
                 $(button).find('span.ui-button-icon-primary')
                     .removeClass('ui-icon-pin-w').addClass('ui-icon-pin-s');
                 if (this.options.customTooltips) {
@@ -39,6 +41,24 @@
                         content: 'Click to dock the toolbar'
                     });
                 }
+            }
+            
+            var spacer = $('.ui-widget-editor-dock-spacer');
+            if (!spacer.length) spacer = $('<div class="ui-widget-editor-dock-spacer"></div>').prependTo('body');
+            spacer.height(this._editor.toolbar.outerHeight());
+            if (this._plugins.unsavedEditWarning) {
+                if (this._data.exists(this.element, this.options.plugins.unsavedEditWarning.dataName)) {
+                    var warning = $(this.element.data(this.options.plugins.unsavedEditWarning.dataName)),
+                        editorInstance = this;
+                    $(warning).hide(this.options.plugins.unsavedEditWarning.animation, function() {
+                        spacer.toggle(function() {
+                            editorInstance._actions.stateChange.call(editorInstance);
+                            editorInstance._plugins.unsavedEditWarning.show.call(editorInstance);
+                        });
+                    });
+                }
+            } else {
+                spacer.toggle('fast');
             }
         }
     });
--- plugins/fontsize/jquery.ui.editor.fontsize.js
+++ plugins/fontsize/jquery.ui.editor.fontsize.js
@@ -1,4 +1,5 @@
 (function($) {
+   
     $.ui.editor.addButton('increaseFontSize', {
         title: 'Increase Font Size',
         icons: {
@@ -6,39 +7,10 @@
         },
         classes: 'ui-editor-icon',
         click: function() {
-            
             this._actions.beforeStateChange.call(this);
             this._selection.enforceLegality.call(this);
 
-            var editorInstance = this,
-                    content = null, increasedSize = null, replacement = null, style = null;
-
-            if (!this._selection.exists.call(this)) {
-                style = { 'font-size': '110%' };
-                if (!this._util.isRoot.call(this, this._editor.selectedElement)) this._editor.selectedElement.css(style);
-                else this.element.children().css(style);
-            } else {
-
-                $(rangy.getSelection().getAllRanges()).each(function(){
-                    content = this.createContextualFragment();
-                    if ((this.commonAncestorContainer == this.startContainer && this.commonAncestorContainer == this.endContainer)
-                        && (this.startOffset == 0 && this.endOffset == 1)) {
-
-                        increasedSize = ($(this.commonAncestorContainer).css('font-size').replace('px', '') * 1.1);
-                        $(this.commonAncestorContainer).css('font-size', increasedSize);
-                    } else {
-
-                        replacement = $('<span style="font-size:110%"></span>');
-
-                        this.splitBoundaries();
-                        $.each(this.getNodes(), function() {
-                            replacement.append(this);
-                        });
-
-                        editorInstance._selection.replace.call(editorInstance, replacement, this);
-                    }
-                });
-            }
+            document.execCommand('increasefontsize', false, null);
 
             this._actions.stateChange.call(this);
         }
@@ -54,34 +26,7 @@
             this._actions.beforeStateChange.call(this);
             this._selection.enforceLegality.call(this);
 
-            var editorInstance = this,
-                style = null, increasedSize = null, replacement = null;
-
-            if (!this._selection.exists.call(this)) {
-                style = { 'font-size': '90%' };
-                if (!this._util.isRoot.call(this, this._editor.selectedElement)) this._editor.selectedElement.css(style);
-                else this.element.children().css(style);
-            } else {
-                $(rangy.getSelection().getAllRanges()).each(function(){
-                    var content = this.createContextualFragment();
-                    if ((this.commonAncestorContainer == this.startContainer && this.commonAncestorContainer == this.endContainer)
-                        && (this.startOffset == 0 && this.endOffset == 1)) {
-
-                        increasedSize = ($(this.commonAncestorContainer).css('font-size').replace('px', '') * 0.9);
-                        $(this.commonAncestorContainer).css('font-size', increasedSize);
-                    } else {
-
-                        replacement = $('<span style="font-size:90%"></span>');
-
-                        this.splitBoundaries();
-                        $.each(this.getNodes(), function() {
-                            replacement.append(this);
-                        });
-
-                        editorInstance._selection.replace.call(editorInstance, replacement, this);
-                    }
-                });
-            }
+            document.execCommand('decreasefontsize', false, null);
 
             this._actions.stateChange.call(this);
         }
