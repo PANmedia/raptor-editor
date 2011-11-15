fc4288b4bba267e1b71eeb0173c49a4ef3f1572b
Michael Robinson <mike@panmedia.co.nz>
2011-11-15 14:00:42 +1300

    Added i18n CSS. Further work towards implementation of i18n.
    Translated some of zh_CN.
    
    Fixed bug in unsavededitwarning plugin.
    
    Moved editor._ content into _ function.

--- plugins/i18n/jquery.ui.editor.i18n.js
+++ plugins/i18n/jquery.ui.editor.i18n.js
@@ -1,13 +1,6 @@
 (function($) {
-    $.ui.editor.addPlugin('i18n', {
-        translations: {},
-        
-        translate: function(string, variables) {
-            
-        }
-    });
-    
-    $.ui.editor.prototype.addLocale = function(name, strings) {
+
+    $.ui.editor.addLocale = function(name, strings) {
         // <strict>
         if (!$.ui.editor.prototype._plugins.i18n) {
             console.error(_('i18n plugin not loaded'));
@@ -23,5 +16,84 @@
         // <debug> 
         console.log(_('Locale <*localeName*> added', { localeName: name }), strings);
         // </debug>
-    }
-});
+    };
+
+    $.ui.editor.addPlugin('i18n', {
+        translations: {},
+        
+        localeNames: {
+            zh_CN: _('Simplified Chinese'),
+            en: _('English')
+        },
+        
+        translate: function(string, variables) {
+            
+            // <debug>
+            var original_string = string;
+            // </debug>
+            
+            if (!variables) {
+            
+                // <debug>
+                //console.log('i18n: ' + string + ' | '  + original_string);
+                // </debug>
+            
+                return string;
+            } else {
+                
+                $.each(variables, function(key, value) {
+                    string = string.replace('<*' + key + '*>', value);
+                });
+            
+                // <debug>
+                //console.log('i18n: ' + string + ' | '  + original_string);
+                // </debug>
+            
+                return string;
+            }
+        }
+    });
+    
+    $.ui.editor.addButton('i18n', {
+        title: _('Change Language'),
+        initialize: function(object, button_group) {
+            var editorInstance = this,
+                menu = $('<select autocomplete="off" name="i18n" class="ui-editor-i18n-select"></select>'),
+                icons = [];
+                
+                $.each(this._plugins.i18n.translations, function(key){
+                    menu.append($('<option value="' + key + '" class="' + key + '">' + editorInstance._plugins.i18n.translate(key) + '</option>'));
+                    icons.push({
+                        find: '.' + key,
+                        icon: 'ui-widget-editor-i18n-' + key
+                    });
+                });
+                
+                menu.appendTo(button_group).data(editorInstance._data.names.button, object);
+
+            if ($.ui.selectmenu) {
+                menu.selectmenu({
+                    width: 150,
+                    icons: icons
+                });
+            }
+            
+            // <strict> 
+            else {
+                console.error(_('jQuery UI selectmenu not found. This library should have been included in the file you downloaded. If not, acquire it here: https://github.com/fnagel/jquery-ui'));
+            }
+            // </strict>
+
+            if (this.options.customTooltips) {
+                button_group.find('.ui-selectmenu').tipTip({
+                    content: _('Change jQuery Editor Interface Language'),
+                    maxWidth: 'auto'
+                });
+            }
+        },
+        destroy: function() {
+            if ($.ui.selectmenu) $('.ui-editor-i18n-select').selectmenu('destroy');
+        }
+    });
+    
+})(jQuery);
