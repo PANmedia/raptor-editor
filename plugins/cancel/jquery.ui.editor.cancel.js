3790508cb7aeb5d45f95f0818c3cb109fc0ea0fb
David Neilsen <david@panmedia.co.nz>
2011-11-18 08:39:45 +1300

    Fix cancel button confirmation.

--- plugins/cancel/jquery.ui.editor.cancel.js
+++ plugins/cancel/jquery.ui.editor.cancel.js
@@ -1,16 +1,60 @@
 console.info('FIXME: cancel button');
 console.info('FIXME: cancel button confimation dialog');
+console.info('FIXME: cancel button confimation dialog get removed on unbind');
+console.info('TODO: make cancel function avalible as a plugin');
 
 $.ui.editor.registerUi({
     cancel: function(editor) {
+        function cancel() {
+            editor.resetHtml();
+            editor.hideToolbar();
+            editor.disableEditing();
+        }
+        
         this.ui = editor.uiButton({
             name: 'cancel',
             title: _('Cancel'),
             icons: { primary: 'ui-icon-cancel' },
             click: function() {
-                editor.resetHtml();
-                editor.hideToolbar();
-                editor.disableEditing();
+                if (!editor.isDirty()) {
+                    cancel();
+                } else {
+                    $(editor.getTemplate('cancel.dialog')).dialog({
+                        modal: true,
+                        resizable: false,
+                        title: _('Confirm Cancel Editing'),
+                        dialogClass: editor.options.dialogClass + ' ui-widget-editor-cancel-dialog',
+                        show: editor.options.dialogShowAnimation,
+                        hide: editor.options.dialogHideAnimation,
+                        buttons: [
+                            {
+                                text: _('OK'),
+                                click: function() {
+                                    cancel();
+                                    $(this).dialog('close');
+                                }
+                            },
+                            {
+                                text: _('Cancel'),
+                                click: function() {
+                                    $(this).dialog('close');
+                                }
+                            }
+                        ],
+                        open: function() {
+                            // Apply custom icons to the dialog buttons
+                            var buttons = $(this).parent().find('.ui-dialog-buttonpane');
+                            buttons.find('button:eq(0)').button({ icons: { primary: 'ui-icon-circle-check' }});
+                            buttons.find('button:eq(1)').button({ icons: { primary: 'ui-icon-circle-close' }});
+                        },
+                        close: function() {
+                            $(this).dialog('destroy').remove();
+                        }
+                    });
+                }
+//                editor.resetHtml();
+//                editor.hideToolbar();
+//                editor.disableEditing();
 //                this._dialog.confirmation.show.call(this, {
 //                    message: _('Are you sure you want to stop editing? <br/><br/>All changes will be lost!'),
 //                    title: _('Confirm Cancel Editing'),
