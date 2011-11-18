fe3c9fb1ff52692714d5b18acb1916c2fd8edc32
David Neilsen <david@panmedia.co.nz>
2011-11-18 17:00:01 +1300

    Fix some more bugs.

--- plugins/tagmenu/jquery.ui.editor.tagmenu.js
+++ plugins/tagmenu/jquery.ui.editor.tagmenu.js
@@ -1,75 +1,29 @@
 (function($) {
     
-    var tagMenu = function(editor, options) {
-        this.titleTagList = function(current) {
-            var tagMenu = editor._editor.toolbar.find('select.ui-editor-tag-select'),
-                tagName = current[0].tagName.toLowerCase();
-                        
-            if (editor._util.isRoot.call(editor, current)) {
-                tagMenu.val('na');
-            } else if (tagMenu.find('option[value=' + tagName + ']').length) {
-                tagMenu.val(tagName);
-            } else {
-                tagMenu.val('other');
-            }
-            if ($.ui.selectmenu) tagMenu.selectmenu();
-        }
-    }
-    
-    $.ui.editor.addPlugin('tagMenu', tagMenu);
-    
     console.info('FIXME: tagmenu');
+    console.info('FIXME: tagmenu custom tooltips');
+    console.info('FIXME: tagmenu check destory detaches menu');
     $.ui.editor.registerUi({
         tagMenu: function(editor) {
-            this.ui = $(editor.getTemplate('tagmenu.menu'));
-            this.ui.bind('change.' + editor.widgetName, function() {
-                var tag = $(this).find(':selected').val();
-                if (tag == 'na') return false
-                else editor.changeTag(tag);
-                $.ui.editor.trigger('resize');
+            var menu = this.ui = editor.uiSelectMenu({
+                name: 'tagMenu',
+                title: _('Change HTML tag of selected element'),
+                select: $(editor.getTemplate('tagmenu.menu')),
+                change: function(value) {
+                    editor.changeTag(value);
+                }
             });
-                
-            console.info('FIXME: jquery ui select menu only works if the element is visible');
-//            if ($.ui.selectmenu) {
-//                this.ui.selectmenu({
-//                    width: 150
-//                });
-//            }
-            // <strict> 
-//            else {
-//                console.error(_('jQuery UI selectmenu not found. This library should have been included in the file you downloaded. If not, acquire it here: https://github.com/fnagel/jquery-ui'));
-//            }
-            // </strict>
-
-            console.info('FIXME: tagmenu customer tooltips');
-//            if (editor.options.customTooltips) {
-//                button_group.find('.ui-selectmenu').tipTip({
-//                    content: _('Change HTML tag of selected element'),
-//                    maxWidth: 'auto'
-//                });
-//            }
-//            editor.bind('change', function() {
-//                if ($.ui.selectmenu) {
-//                    var menu = $('.ui-editor-tag-select');
-//                    if (editor.isRoot(editor.getSelectedElements().eq(0))) {
-//                        menu.selectmenu('disable');
-//                    } else {
-//                        menu.selectmenu('enable');
-//                    }
-//                }
-//            });
-//            console.info('FIXME check change get fired');
-//            this.change = function() {
-//                if ($.ui.selectmenu) {
-//                    var menu = $('.ui-editor-tag-select');
-//                    if (this._util.isRoot.call(this, this._editor.selectedElement)) menu.selectmenu('disable');
-//                    else menu.selectmenu('enable');
-//                }
-//            }
-            console.info('FIXME check destory get fired');
-            this.destroy = function() {
-                if ($.ui.selectmenu) $('.ui-editor-tag-select').selectmenu('destroy');
-            }
+            
+            editor.bind('change', function() {
+                var tag = editor.getSelectedElements()[0];
+                if (!tag) return;
+                tag = tag.tagName.toLowerCase();
+                if (menu.find('option[value=' + tag + ']').length) {
+                    menu.val(tag);
+                } else {
+                    menu.val('na');
+                }
+            });
         }
     });
     
