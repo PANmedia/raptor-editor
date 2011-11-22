fd1ad23c34fb30f35d1a47c640fd9c61f3551871
David Neilsen <david@panmedia.co.nz>
2011-11-22 16:23:19 +1300

    Convert basic plugin to new UI def

--- plugins/basic/jquery.ui.editor.basic.js
+++ plugins/basic/jquery.ui.editor.basic.js
@@ -1,34 +1,42 @@
 $.ui.editor.registerUi({
-    'text-bold': function(editor) {
-        this.ui = editor.uiButton({
-            title: _('Bold'),
-            click: function() {
-                editor.toggleWrapper('strong');
-            }
-        });
+    'text-bold': {
+        init: function(editor) {
+            return this.editor.uiButton({
+                title: _('Bold'),
+                click: function() {
+                    this.editor.toggleWrapper('strong');
+                }
+            });
+        }
     },
-    'text-italic': function(editor) {
-        this.ui = editor.uiButton({
-            title: _('Italic'),
-            click: function() {
-                editor.toggleWrapper('em');
-            }
-        });
+    'text-italic': {
+        init: function(editor) {
+            return editor.uiButton({
+                title: _('Italic'),
+                click: function() {
+                    editor.toggleWrapper('em');
+                }
+            });
+        }
     },
-    'text-underline': function(editor) {
-        this.ui = editor.uiButton({
-            title: _('Underline'),
-            click: function() {
-                editor.toggleWrapper('span', { classes: 'underline' });
-            }
-        });
+    'text-underline': {
+        init: function(editor) {
+            return editor.uiButton({
+                title: _('Underline'),
+                click: function() {
+                    editor.toggleWrapper('span', { classes: 'underline' });
+                }
+            });
+        }
     },
-    'text-strike': function(editor) {
-        this.ui = editor.uiButton({
-            title: _('Strikethrough'),
-            click: function() {
-                editor.toggleWrapper('del');
-            }
-        });
+    'text-strike': {
+        init: function(editor) {
+            return editor.uiButton({
+                title: _('Strikethrough'),
+                click: function() {
+                    editor.toggleWrapper('del');
+                }
+            });
+        }
     }
 });
