8f5c47a708bb82d45efd1e149177cddb4472e593
David Neilsen <david@panmedia.co.nz>
2011-11-24 10:25:29 +1300

    Fix multiline edtior toolbar.

--- jquery.ui.editor.js
+++ jquery.ui.editor.js
@@ -1065,10 +1065,13 @@ var _;
                     }
                     // <strict>
                     else {
-//                        console.error(_('UI identified by key "{{ui}}" does not exist', { ui: ui }));
+                        console.error(_('UI identified by key "{{ui}}" does not exist', { ui: ui }));
                     }
                     // </strict>
                 }
+                
+                uiGroup.addClass(editor.options.baseClass + '-group');
+                
                 if (uiGroup.children().length > 1) {
                     uiGroup.addClass(editor.options.baseClass + '-buttonset');
                 }
@@ -1078,6 +1081,7 @@ var _;
                     uiGroup.appendTo(editor.selToolbar('.' + editor.options.baseClass + '-inner'));
                 }
             };
+            $('<div/>').addClass('ui-helper-clearfix').appendTo(editor.selToolbar('.' + editor.options.baseClass + '-inner'));
         },
         
         uiButton2: {
