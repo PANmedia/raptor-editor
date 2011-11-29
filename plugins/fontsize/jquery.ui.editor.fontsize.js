87b7ad9ec57f8f565aa784358c3e03abddc8cc5a
David Neilsen <david@panmedia.co.nz>
2011-11-29 18:19:07 +1300

    Fixes to big/small, clean, selectmenu

--- plugins/fontsize/jquery.ui.editor.fontsize.js
+++ plugins/fontsize/jquery.ui.editor.fontsize.js
@@ -1,20 +1,20 @@
 $.ui.editor.registerUi({
     'font-size-inc': {
-        init: function(editor) {
+        init: function(editor, options) {
             return editor.uiButton({
                 title: _('Increase Font Size'),
                 click: function() {
-                    editor.execCommand('increasefontsize', false, null);
+                    editor.inverseWrapWithTagClass('big', options.cssPrefix + 'big', 'small', options.cssPrefix + 'small');
                 }
             });
         }
     },
     'font-size-dec': {
-        init: function(editor) {
+        init: function(editor, options) {
             return editor.uiButton({
                 title: _('Decrease Font Size'),
                 click: function() {
-                    editor.execCommand('decreasefontsize', false, null);
+                    editor.inverseWrapWithTagClass('small', options.cssPrefix + 'small', 'big', options.cssPrefix + 'big');
                 }
             });
         }
