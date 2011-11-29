3a509f163797b3b62f83c6fc8ba81675064f0658
Michael Robinson <mike@panmedia.co.nz>
2011-11-29 17:58:21 +1300

    Changed tiptip plugin to position tooltips at bottom of buttons if editor is docked, othewise top.

--- plugins/tiptip/jquery.ui.editor.tiptip.js
+++ plugins/tiptip/jquery.ui.editor.tiptip.js
@@ -9,19 +9,19 @@
         },
         
         currentLocale: null,
-        
+        docked: false,
+
         init: function(editor, options) {
-            var tiptip = this;
+            var ui = this;
             
-            editor.bind('change', function() {
-                var tipTipOptions = {
-                    maxWidth: 'auto',
-                    delay: options.delay,
-                    fadeIn: options.fadeIn,
-                    fadeOut: options.fadeOut
-                };
-                // Only apply tiptip to the core elements if it hasn't yet been applied, or the locale has been changed
-                if (tiptip.currentLocale != options.locale) {
+            editor.bind('resize', function() {
+
+                // Need to check for the docking plugin, and if so whether the editor has been docked / undocked
+                var docked = this.getPlugin('dock') ? this.getPlugin('dock').isDocked() : false;
+
+                // Only apply tiptip to the core elements if it hasn't yet been applied, 
+                // or the locale has been changed or the editor docked / undocked
+                if (ui.currentLocale != options.locale || docked != ui.docked) {
                     // <strict>
                     // Ensure jQuery has been included
                     if (!$.isFunction($.fn.tipTip)) {
@@ -29,10 +29,20 @@
                         return;
                     }
                     // </strict>
-                    $.merge(editor.selToolbar('[title]'), editor.selTitle('[title]')).each(function() {
-                        $(this).tipTip($.extend(tipTipOptions, { defaultPosition: 'top' }));
+                    var tipTipOptions = {
+                        maxWidth: 'auto',
+                        delay: options.delay,
+                        fadeIn: options.fadeIn,
+                        fadeOut: options.fadeOut,
+                        defaultPosition: docked ? 'bottom' : 'top' 
+                    };
+
+                    // Apply tiptip to all children of the relevant elements that either have a title attribute or the required class
+                    $.merge(editor.selToolbar('[title], .' + options.baseClass + '-tiptip'), editor.selTitle('[title], .' + options.baseClass + '-tiptip')).each(function() {
+                        $(this).tipTip(tipTipOptions).addClass(options.baseClass + '-tiptip');
                     });
-                    this.currentLocale = options.locale;
+                    ui.docked = docked;
+                    ui.currentLocale = options.locale;
                 }
                 // Unsaved edit warnings could be added / removed on change, so we should check for them each time
                 $('body div.ui-editor-unsaved-edit-warning-warning[title]').tipTip($.extend(tipTipOptions, { defaultPosition: 'right' }));
