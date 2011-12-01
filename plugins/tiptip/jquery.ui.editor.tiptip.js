70512e994d64a34462ca84bcc6a044939750e855
David Neilsen <david@panmedia.co.nz>
2011-12-01 15:50:51 +1300

    Unwrap tiptip

--- plugins/tiptip/jquery.ui.editor.tiptip.js
+++ plugins/tiptip/jquery.ui.editor.tiptip.js
@@ -1,53 +1,49 @@
-(function($) {
-    
-    $.ui.editor.registerPlugin('tiptip', {
-        
-        options: {
-            delay: 400,
-            fadeIn: 200,
-            fadeOut: 200
-        },
-        
-        currentLocale: null,
-        docked: false,
+$.ui.editor.registerPlugin('tiptip', {
 
-        init: function(editor, options) {
-            var ui = this;
-            
-            editor.bind('resize', function() {
+    options: {
+        delay: 400,
+        fadeIn: 200,
+        fadeOut: 200
+    },
 
-                // Need to check for the docking plugin, and if so whether the editor has been docked / undocked
-                var docked = this.getPlugin('dock') ? this.getPlugin('dock').isDocked() : false;
+    currentLocale: null,
+    docked: false,
 
-                // Only apply tiptip to the core elements if it hasn't yet been applied, 
-                // or the locale has been changed or the editor docked / undocked
-                if (ui.currentLocale != options.locale || docked != ui.docked) {
-                    // <strict>
-                    // Ensure jQuery has been included
-                    if (!$.isFunction($.fn.tipTip)) {
-                        handleError(_('jquery.ui.editor.tiptip requires TipTip. '));
-                        return;
-                    }
-                    // </strict>
-                    var tipTipOptions = {
-                        maxWidth: 'auto',
-                        delay: options.delay,
-                        fadeIn: options.fadeIn,
-                        fadeOut: options.fadeOut,
-                        defaultPosition: docked ? 'bottom' : 'top' 
-                    };
+    init: function(editor, options) {
+        var ui = this;
 
-                    // Apply tiptip to all children of the relevant elements that either have a title attribute or the required class
-                    $.merge(editor.selToolbar('[title], .' + options.baseClass + '-tiptip'), editor.selTitle('[title], .' + options.baseClass + '-tiptip')).each(function() {
-                        $(this).tipTip(tipTipOptions).addClass(options.baseClass + '-tiptip');
-                    });
-                    ui.docked = docked;
-                    ui.currentLocale = options.locale;
+        editor.bind('resize', function() {
+
+            // Need to check for the docking plugin, and if so whether the editor has been docked / undocked
+            var docked = this.getPlugin('dock') ? this.getPlugin('dock').isDocked() : false;
+
+            // Only apply tiptip to the core elements if it hasn't yet been applied, 
+            // or the locale has been changed or the editor docked / undocked
+            if (ui.currentLocale != options.locale || docked != ui.docked) {
+                // <strict>
+                // Ensure jQuery has been included
+                if (!$.isFunction($.fn.tipTip)) {
+                    handleError(_('jquery.ui.editor.tiptip requires TipTip. '));
+                    return;
                 }
-                // Unsaved edit warnings could be added / removed on change, so we should check for them each time
-                $('body div.ui-editor-unsaved-edit-warning-warning[title]').tipTip($.extend(tipTipOptions, { defaultPosition: 'right' }));
-            });
-        }
-    });
+                // </strict>
+                var tipTipOptions = {
+                    maxWidth: 'auto',
+                    delay: options.delay,
+                    fadeIn: options.fadeIn,
+                    fadeOut: options.fadeOut,
+                    defaultPosition: docked ? 'bottom' : 'top' 
+                };
 
-})(jQuery);
+                // Apply tiptip to all children of the relevant elements that either have a title attribute or the required class
+                $.merge(editor.selToolbar('[title], .' + options.baseClass + '-tiptip'), editor.selTitle('[title], .' + options.baseClass + '-tiptip')).each(function() {
+                    $(this).tipTip(tipTipOptions).addClass(options.baseClass + '-tiptip');
+                });
+                ui.docked = docked;
+                ui.currentLocale = options.locale;
+            }
+            // Unsaved edit warnings could be added / removed on change, so we should check for them each time
+            $('body div.ui-editor-unsaved-edit-warning-warning[title]').tipTip($.extend(tipTipOptions, { defaultPosition: 'right' }));
+        });
+    }
+});
