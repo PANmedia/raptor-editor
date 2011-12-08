0f0b03d70b0ad8454354e06687f6fff19e9383c4
Michael Robinson <mike@panmedia.co.nz>
2011-12-08 15:57:51 +1300

    Moved save plugin's call to editor.save into its getData function.
    Removed commented out code.

--- plugins/save/jquery.ui.editor.save.js
+++ plugins/save/jquery.ui.editor.save.js
@@ -26,7 +26,7 @@ $.ui.editor.registerPlugin('save', {
 
     getData: function() {
         var data = {};
-        data[this.getId()] = this.editor.getHtml();
+        data[this.getId()] = this.editor.save();
         return data;
     },
 
@@ -41,7 +41,6 @@ $.ui.editor.registerPlugin('save', {
                 dirty++;
                 var plugin = editor.getPlugin('save');
                 $.extend(contentData, plugin.getData());
-                editor.save();
             }
         });
         this.dirty = dirty;
@@ -164,55 +163,3 @@ $.ui.editor.registerUi({
         }
     }
 });
-
-////$.ui.editor.registerUi({
-//    save: function(editor, options) {
-//        options = $.extend({
-//            saveUri: '/editor/save'
-//        }, options);
-//        var ui = this.ui = editor.uiButton({
-//            title: _('Save'),
-//            icon: 'ui-icon-disk',
-//            disabled: true,
-//            click: function() {
-//                // If the user has provided or bound their own save function, allow them to cancel the default
-//                if (this._trigger('save')) {
-//
-//                    this.message.loading.call(this, _('Saving changes...'), false);
-//
-//                    var error = function(response_code) {
-//                        editor.message.error.call(editor, [
-//                            _('Failed to save content'),
-//                            _('Response code {{responseCode}} from {{location}}', { responseCode: response_code, location: window.location.protocol + '//' + window.location.hostname + editor.options.saveUri })
-//                        ], 10000);
-//                    }, editor = this;
-//
-//                    $.ajax(saveUri, {
-//                        data: {
-//                            html: this.html(),
-//                            name: editor.target.attr('name')
-//                        },
-//                        type: 'post',
-//                        statusCode: {
-//                            404: function() {
-//                                error(404);
-//                            },
-//                            500: function() {
-//                                error(500);
-//                            }
-//                        },
-//                        success: function(data) {
-//                            editor.confirm.call(editor, _('Content saved'));
-//                            editor._data.clear.call(editor._data.names.originalHtml);
-//                        }
-//                    });
-//
-//                }
-//            }
-//        });
-//        editor.bind('change', function() {
-//            if (!editor.isDirty()) ui.disable();
-//            else ui.enable();
-//        });
-//    }
-//});
