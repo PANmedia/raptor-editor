32a96a0e37a6516e7711388a7592d318237300e4
Michael Robinson <mike@panmedia.co.nz>
2011-12-09 16:49:17 +1300

    Tidied up view source ui component.

--- plugins/viewsource/jquery.ui.editor.viewsource.js
+++ plugins/viewsource/jquery.ui.editor.viewsource.js
@@ -1,64 +1,55 @@
-// <debug>
-if (debugLevel >= MAX) {
-    info('FIXME: remove dialog on destroy');
-}
-// </debug>
-(function($) {
-
-    $.ui.editor.registerUi({
-        viewSource:  {
-            init: function(editor, options) {
-                var dialog = this.dialog = $(editor.getTemplate('viewsource.dialog'));
-                dialog.dialog({
-                    modal: false,
-                    width: 600,
-                    height: 400,
-                    resizable: true,
-                    title: _('View Source'),
-                    autoOpen: false,
-                    dialogClass: options.baseClass + ' ' + options.dialogClass,
-                    show: options.dialogShowAnimation,
-                    hide: options.dialogHideAnimation,
-                    buttons: [
-                        {
-                            text: _('Apply Source'),
-                            click: function() {
-                                editor.setHtml($(this).find('textarea').val());
-                            }
-                        },
-                        {
-                            text: _('Close'),
-                            click: function() {
-                                dialog.dialog('close');
-                            }
+$.ui.editor.registerUi({
+    viewSource:  {
+        init: function(editor, options) {
+            var dialog = this.dialog = $(editor.getTemplate('viewsource.dialog'));
+            dialog.dialog({
+                modal: false,
+                width: 600,
+                height: 400,
+                resizable: true,
+                title: _('View Source'),
+                autoOpen: false,
+                dialogClass: options.baseClass + ' ' + options.dialogClass,
+                show: options.dialogShowAnimation,
+                hide: options.dialogHideAnimation,
+                buttons: [
+                    {
+                        text: _('Apply Source'),
+                        click: function() {
+                            editor.setHtml($(this).find('textarea').val());
                         }
-                    ],
-                    open: function() {
-                        var buttons = dialog.parent().find('.ui-dialog-buttonpane');
-                        buttons.find('button:eq(0)').button({ icons: { primary: 'ui-icon-circle-check' }});
-                        buttons.find('button:eq(1)').button({ icons: { primary: 'ui-icon-circle-close' }});
-
-                        $(this).find('textarea').val(editor.getHtml());
+                    },
+                    {
+                        text: _('Close'),
+                        click: function() {
+                            dialog.dialog('close');
+                        }
                     }
-                });
+                ],
+                open: function() {
+                    var buttons = dialog.parent().find('.ui-dialog-buttonpane');
+                    buttons.find('button:eq(0)').button({ icons: { primary: 'ui-icon-circle-check' }});
+                    buttons.find('button:eq(1)').button({ icons: { primary: 'ui-icon-circle-close' }});
 
-                editor.bind('hide', this.hide, this);
-                editor.bind('destroy', this.destroy, this);
+                    $(this).find('textarea').val(editor.getHtml());
+                }
+            });
 
-                return editor.uiButton({
-                    title: _('View / Edit Source'),
-                    click: function() {
-                        dialog.dialog('open');
-                    }
-                });
-            },
-            hide: function() {
-                this.dialog.dialog('close');
-            },
-            destroy: function() {
-                this.dialog.dialog('destroy').remove();
-            }
+            editor.bind('hide', this.hide, this);
+            editor.bind('destroy', this.destroy, this);
+
+            return editor.uiButton({
+                title: _('View / Edit Source'),
+                click: function() {
+                    dialog.dialog('open');
+                }
+            });
+        },
+        hide: function() {
+            this.dialog.dialog('close');
+        },
+        destroy: function() {
+            this.dialog.dialog('destroy').remove();
         }
-    });
-
-})(jQuery);
+    }
+});
