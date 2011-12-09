1bf06ea1fe9f2fb66979961c30806ac412e13145
Michael Robinson <mike@panmedia.co.nz>
2011-12-09 15:50:06 +1300

    Added documentation for alignment ui.

--- plugins/alignment/jquery.ui.editor.alignment.js
+++ plugins/alignment/jquery.ui.editor.alignment.js
@@ -1,5 +1,56 @@
+/**
+ * @fileOverview Text alignment ui components
+ * @author David Neilson david@panmedia.co.nz
+ * @author Michael Robinson mike@panmedia.co.nz
+ */
+
+/**
+ * Aligns text left within the selected or nearest block-level element.
+ * <br/>
+ * <br/>
+ * Toggles <tt>text-align: left</tt>
+ * 
+ * @name $.editor.ui.alignLeft
+ * @class
+ */
+
+ /**
+ * Justifies text within the selected or nearest block-level element.
+ * <br/>
+ * <br/>
+ * Toggles <tt>text-align: justify</tt>
+ * 
+ * @name $.editor.ui.alignJustify
+ * @class
+ */
+
+ /**
+ * Centers text within the selected or nearest block-level element.
+ * <br/>
+ * <br/>
+ * Toggles: <tt>text-align: center</tt>
+ * 
+ * @name $.editor.ui.alignCenter
+ * @class
+ */
+
+ /**
+ * Aligns text right within the selected or nearest block-level element.
+ * <br/>
+ * <br/>
+ * Toggles <tt>text-align: right</tt>
+ * 
+ * @name $.editor.ui.alignRight
+ * @class
+ */
+
 $.ui.editor.registerUi({
-    alignLeft: {
+    alignLeft: /** @lends $.editor.ui.alignLeft.prototype */ {
+        /**
+         * Initialise the ui component
+         * @param  {$.editor} editor The editor instance 
+         * @return {$.editor.ui.alignLeft}
+         */
         init: function(editor) {
             return editor.uiButton({
                 title: _('Left Align'),
@@ -12,7 +63,12 @@ $.ui.editor.registerUi({
         }
     },
 
-    alignJustify: {
+    alignJustify: /** @lends $.editor.ui.alignJustify.prototype */ {
+        /**
+         * Initialise the ui component
+         * @param  {$.editor} editor The editor instance 
+         * @return {$.editor.ui.alignJustify}
+         */
         init: function(editor) {
             return editor.uiButton({
                 title: _('Justify'),
@@ -25,7 +81,12 @@ $.ui.editor.registerUi({
         }
     },
 
-    alignCenter: {
+    alignCenter: /** @lends $.editor.ui.alignCenter.prototype */  {
+        /**
+         * Initialise the ui component
+         * @param  {$.editor} editor The editor instance 
+         * @return {$.editor.ui.alignCenter}
+         */
         init: function(editor) {
             return editor.uiButton({
                 title: _('Center Align'),
@@ -38,7 +99,12 @@ $.ui.editor.registerUi({
         }
     },
 
-    alignRight: {
+    alignRight: /** @lends $.editor.ui.alignRight.prototype */  {
+        /**
+         * Initialise the ui component
+         * @param  {$.editor} editor The editor instance 
+         * @return {$.editor.ui.alignRight}
+         */        
         init: function(editor) {
             return editor.uiButton({
                 title: _('Right Align'),
