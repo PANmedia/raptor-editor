529e1eec5bc72fd5ea35db790cc731168e8947cb
Michael Robinson <mike@panmedia.co.nz>
2011-12-09 15:50:28 +1300

    Added documentation for blockquote ui.

--- plugins/blockquote/jquery.ui.editor.blockquote.js
+++ plugins/blockquote/jquery.ui.editor.blockquote.js
@@ -1,10 +1,29 @@
-$.ui.editor.registerUi({
-    quoteBlock: {
+/**
+ * @fileOverview Blockquote ui component
+ * @author David Neilson david@panmedia.co.nz
+ * @author Michael Robinson mike@panmedia.co.nz
+ */
+
+ /**
+  * Wraps (or unwraps) selection in &lt;blockquote&gt; tags
+  * <br/>
+  * Applies either {@link $.ui.editor.defaults.cssPrefix} + 'blockquote' or a custom class (if present) to the &lt;blockquote&gt; element 
+  * @name $.editor.ui.quoteBlock
+  * @class
+  */
+ $.ui.editor.registerUi({
+    quoteBlock: /** @lends $.editor.ui.quoteBlock.prototype */ {
+        /**
+         * Initialise the ui component
+         * @param  {$.editor} editor The editor instance 
+         * @param  {$.ui.editor.defaults} options The default editor options extended with any overrides set at initialisation
+         * @return {$.editor.plugin.quoteBlock}
+         */
         init: function(editor) {
             return editor.uiButton({
                 title: _('Blockquote'),
                 click: function() {
-                    editor.toggleWrapper('blockquote');
+                    editor.toggleWrapper('blockquote', { classes: options.classes || options.cssPrefix + 'blockquote' });
                 }
             });
         }
