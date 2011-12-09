0c2d3bb0553e74ff850fabac2e66aab3b4a5af30
Michael Robinson <mike@panmedia.co.nz>
2011-12-09 16:48:14 +1300

    Updated documentation for cancel ui component.

--- plugins/cancel/jquery.ui.editor.cancel.js
+++ plugins/cancel/jquery.ui.editor.cancel.js
@@ -1,64 +1,95 @@
+/**
+ * @fileOverview Cancel ui component
+ * @author David Neilson david@panmedia.co.nz
+ * @author Michael Robinson mike@panmedia.co.nz
+ */
+
+
+ /**
+  * Cancels editing
+  * @name $.editor.ui.cancel
+  * @class
+  */
+
 // <debug>
 if (debugLevel >= MAX) {
-    info('FIXME: cancel button confimation dialog get removed on unbind');
     info('TODO: make cancel function avalible as a plugin');
 }
 // </debug>
 
 $.ui.editor.registerUi({
-    cancel:  {
-        init: function(editor) {
-            var ui = this;
+    cancel: /** @lends $.editor.ui.cancel.prototype */ {
 
+        /**
+         * Initialise the ui component
+         * 
+         * @param  {$.editor} editor The editor instance 
+         * @return {$.editor.plugin.cancel}
+         */
+        init: function(editor) {
             return editor.uiButton({
                 name: 'cancel',
                 title: _('Cancel'),
                 icons: { primary: 'ui-icon-cancel' },
                 dialog: null,
                 click: function() {
-                    var editor = this.editor;
-                    if (!editor.isDirty()) {
-                        ui.cancel();
-                    } else {
-                        if (!this.dialog) this.dialog = $(editor.getTemplate('cancel.dialog'));
-                        this.dialog.dialog({
-                            modal: true,
-                            resizable: false,
-                            title: _('Confirm Cancel Editing'),
-                            dialogClass: editor.options.dialogClass + ' ' + editor.options.baseClass,
-                            show: editor.options.dialogShowAnimation,
-                            hide: editor.options.dialogHideAnimation,
-                            buttons: [
-                                {
-                                    text: _('OK'),
-                                    click: function() {
-                                        ui.cancel();
-                                        $(this).dialog('close');
-                                    }
-                                },
-                                {
-                                    text: _('Cancel'),
-                                    click: function() {
-                                        $(this).dialog('close');
-                                    }
-                                }
-                            ],
-                            open: function() {
-                                // Apply custom icons to the dialog buttons
-                                var buttons = $(this).parent().find('.ui-dialog-buttonpane');
-                                buttons.find('button:eq(0)').button({ icons: { primary: 'ui-icon-circle-check' }});
-                                buttons.find('button:eq(1)').button({ icons: { primary: 'ui-icon-circle-close' }});
-                            },
-                            close: function() {
-                                $(this).dialog('destroy').remove();
-                            }
-                        });
-                    }
+                    this.confirm();
                 }
             });
         },
+        /**
+         * If the editor is dirty, inform the user that to cancel editing will discard their unsaved changes.
+         * If the user accepts of if the editor is not dirty, cancel editing.
+         */
+        confirm: function() {
+            var ui = this;
+            var editor = this.editor;
+            if (!editor.isDirty()) {
+                ui.cancel();
+            } else {
+                if (!this.dialog) this.dialog = $(editor.getTemplate('cancel.dialog'));
+                this.dialog.dialog({
+                    modal: true,
+                    resizable: false,
+                    title: _('Confirm Cancel Editing'),
+                    dialogClass: editor.options.dialogClass + ' ' + editor.options.baseClass,
+                    show: editor.options.dialogShowAnimation,
+                    hide: editor.options.dialogHideAnimation,
+                    buttons: [
+                        {
+                            text: _('OK'),
+                            click: function() {
+                                ui.cancel();
+                                $(this).dialog('close');
+                            }
+                        },
+                        {
+                            text: _('Cancel'),
+                            click: function() {
+                                $(this).dialog('close');
+                            }
+                        }
+                    ],
+                    open: function() {
+                        // Apply custom icons to the dialog buttons
+                        var buttons = $(this).parent().find('.ui-dialog-buttonpane');
+                        buttons.find('button:eq(0)').button({ icons: { primary: 'ui-icon-circle-check' }});
+                        buttons.find('button:eq(1)').button({ icons: { primary: 'ui-icon-circle-close' }});
+                    },
+                    close: function() {
+                        $(this).dialog('destroy').remove();
+                    }
+                });
+            }
+        },
+
+        /**
+         * Cancel editing 
+         * by resetting the editor's html its pre-intitialisation state, hiding the toolbar and disabling editing on the element
+         */
         cancel: function() {
             this.editor.unify(function(editor) {
+                editor.fire('cancel');
                 editor.resetHtml();
                 editor.hideToolbar();
                 editor.disableEditing();
