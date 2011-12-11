5fcc4fe8b4c5ae7c7e04ec98d48a9f73b5376030
Michael Robinson <mike@panmedia.co.nz>
2011-12-12 11:01:14 +1300

    Updated documentation for hr plugin.

--- plugins/hr/jquery.ui.editor.hr.js
+++ plugins/hr/jquery.ui.editor.hr.js
@@ -1,5 +1,23 @@
-$.ui.editor.registerUi({
-    'hr': {
+/**
+ * @fileOverview Insert hr ui component
+ * @author David Neilson david@panmedia.co.nz
+ * @author Michael Robinson mike@panmedia.co.nz
+ */
+ $.ui.editor.registerUi({
+
+    /**
+     * Shows a message at the center of an editable block,
+     * informing the user that they may click to edit the block contents
+     * @name $.editor.ui.hr
+     * @class
+     */
+    hr: /** @lends $.editor.ui.hr.prototype */ {
+
+        /**
+         * Initialise the ui component
+         * @param  {$.editor} editor  The editor instance
+         * @return {$.editor.ui.hr}
+         */
         init: function(editor) {
             return editor.uiButton({
                 title: _('Insert Horizontal Rule'),
