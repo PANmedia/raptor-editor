75543d9fc30b8d5304ba6a1bde85a79d5b2d0200
Michael Robinson <mike@panmedia.co.nz>
2011-12-12 08:12:48 +1300

    Update documentation for clean plugin

--- plugins/clean/jquery.ui.editor.clean.js
+++ plugins/clean/jquery.ui.editor.clean.js
@@ -1,23 +1,64 @@
-$.ui.editor.registerPlugin('clean', {
+/**
+ * @fileOverview Clean plugin & ui component
+ * @author David Neilson david@panmedia.co.nz
+ * @author Michael Robinson mike@panmedia.co.nz
+ */
+
+ /**
+  * Strips empty tags and unwanted attributes from editing element
+  * @name $.editor.plugin.clean
+  * @class
+  */
+  $.ui.editor.registerPlugin('clean', /** @lends $.editor.plugin.clean.prototype */ {
+    
+    /**
+     * Attributes to be stripped, empty tags to be removed & attributes to be removed if empty
+     * @type {Object}
+     */
     options: {
+        /**
+         * Attributes to be completely removed
+         * @type {Array}
+         */
         stripAttrs: ['_moz_dirty'],
+        /**
+         * Attribute contents to be stripped
+         * @type {Object}
+         */
         stripAttrContent: {
             type: '_moz'
         },
+        /**
+         * Tags to be removed if empty
+         * @type {Array}
+         */
         stripEmptyTags: [
             'h1', 'h2', 'h3', 'h4', 'h5',  'h6',
             'p', 'b', 'i', 'u', 'strong', 'em',
             'big', 'small', 'div', 'span'
         ],
+        /**
+         * Attributes to be removed if empty
+         * @type {Array}
+         */
         stripEmptyAttrs: [
             'class', 'id', 'style'
         ]
     },
     
+    /**
+     * Initialise the plugin
+     * Binds the $.editor.plugin.clean.clean to the change event
+     * @param  {$.editor} editor  The editor instance
+     * @param  {$.ui.editor.defaults} options The default editor options extended with any overrides set at initialisation
+     */
     init: function(editor, options) {
         editor.bind('change', this.clean, this);
     },
-        
+      
+    /**
+     * Remove empty tags and unwanted attributes from the element
+     */  
     clean: function() {
         var i;
         for (i = 0; i < this.options.stripAttrs.length; i++) {
@@ -49,9 +90,17 @@ $.ui.editor.registerPlugin('clean', {
     }
 });
 
+/**
+  * UI component that calls {@link $.editor.plugin.clean#clean} when clicked
+  * @name $.editor.ui.clean
+  * @class
+  */
 $.ui.editor.registerUi({
-    clean: {
-        
+    clean: /** @lends $.editor.ui.clean.prototype */ {
+        /**
+         * Intialise the ui component
+         * @param  {$.editor} editor  The editor instance
+         */
         init: function(editor) {
             return editor.uiButton({
                 title: _('Remove unnecessary markup from editor content'),
