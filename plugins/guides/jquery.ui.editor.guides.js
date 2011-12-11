f5cdbb38b801da1aae712a58ce29133601dafb0b
Michael Robinson <mike@panmedia.co.nz>
2011-12-12 10:10:20 +1300

    Updated show guides documentation.

--- plugins/guides/jquery.ui.editor.guides.js
+++ plugins/guides/jquery.ui.editor.guides.js
@@ -1,5 +1,23 @@
+/**
+ * @fileOverview Show guides ui component
+ * @author David Neilson david@panmedia.co.nz
+ * @author Michael Robinson mike@panmedia.co.nz
+ */
+
 $.ui.editor.registerUi({
-    showGuides: {
+     /**
+     * Outlines elements contained within the editing element
+     * 
+     * @name $.editor.ui.showGuides
+     * @class
+     */
+    showGuides: /** @lends $.editor.ui.showGuides.prototype */ {
+        /**
+         * Initialise the ui component
+         * @param  {$.editor} editor The editor instance 
+         * @param  {$.ui.editor.defaults} options The default editor options extended with any overrides set at initialisation
+         * @return {$.editor.ui.showGuides}
+         */
         init: function(editor, options) {
             this.bind('destroy', function() {
                 this.editor.getElement().removeClass(options.baseClass + '-visible');
