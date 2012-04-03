888563d8f124a4d54975ba8a5ef3b7526e15c825
David Neilsen <david@panmedia.co.nz>
2012-04-03 15:23:12 +1200

    Add dev build

---
+++ src/include.php
@@ -0,0 +1,94 @@
+<!-- Libraries -->
+<script type="text/javascript" src="/jquery-raptor/dependencies/jquery.js"></script>
+<script type="text/javascript" src="/jquery-raptor/dependencies/jquery.cookie.js"></script>
+
+<script type="text/javascript" src="/jquery-raptor/dependencies/jquery-ui.js"></script>
+<link rel="stylesheet" href="/jquery-raptor/dependencies/themes/smoothness/jquery-ui.css"/>
+
+<script type="text/javascript" src="/jquery-raptor/dependencies/rangy/rangy-core.js"></script>
+<script type="text/javascript" src="/jquery-raptor/dependencies/rangy/rangy-cssclassapplier.js"></script>
+<script type="text/javascript" src="/jquery-raptor/dependencies/rangy/rangy-selectionsaverestore.js"></script>
+
+<link rel="stylesheet" href="/jquery-raptor/dependencies/tiptip/tiptip.css"/>
+<script type="text/javascript" src="/jquery-raptor/dependencies/tiptip/jquery.tiptip.js"></script>
+
+<script type="text/javascript" src="/jquery-raptor/dependencies/raptorize/jquery.raptorize.js"></script>
+
+<!-- Editor -->
+<script type="text/javascript" src="/jquery-raptor/jquery.ui.editor.init.js"></script>
+<script type="text/javascript" src="/jquery-raptor/jquery.ui.editor.domtools.js"></script>
+<script type="text/javascript" src="/jquery-raptor/jquery.ui.editor.js"></script>
+<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/jquery.ui.editor.less"/>
+
+<!-- Plugins -->
+<script type="text/javascript" src="/jquery-raptor/plugins/dock/jquery.ui.editor.dock.js"></script>
+<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/dock/jquery.ui.editor.dock.less"/>
+
+<script type="text/javascript" src="/jquery-raptor/plugins/tiptip/jquery.ui.editor.tiptip.js"></script>
+
+<script type="text/javascript" src="/jquery-raptor/plugins/clicktoedit/jquery.ui.editor.clicktoedit.js"></script>
+<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/clicktoedit/jquery.ui.editor.clicktoedit.less"/>
+
+<script type="text/javascript" src="/jquery-raptor/plugins/clean/jquery.ui.editor.clean.js"></script>
+<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/clean/jquery.ui.editor.clean.less"/>
+
+<script type="text/javascript" src="/jquery-raptor/plugins/float/jquery.ui.editor.float.js"></script>
+<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/float/jquery.ui.editor.float.less"/>
+
+<script type="text/javascript" src="/jquery-raptor/plugins/alignment/jquery.ui.editor.alignment.js"></script>
+<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/alignment/jquery.ui.editor.alignment.less"/>
+
+<script type="text/javascript" src="/jquery-raptor/plugins/basic/jquery.ui.editor.basic.js"></script>
+<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/basic/jquery.ui.editor.basic.less"/>
+
+<script type="text/javascript" src="/jquery-raptor/plugins/history/jquery.ui.editor.history.js"></script>
+<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/history/jquery.ui.editor.history.less"/>
+
+<script type="text/javascript" src="/jquery-raptor/plugins/viewsource/jquery.ui.editor.viewsource.js"></script>
+<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/viewsource/jquery.ui.editor.viewsource.less"/>
+
+<script type="text/javascript" src="/jquery-raptor/plugins/guides/jquery.ui.editor.guides.js"></script>
+<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/guides/jquery.ui.editor.guides.less"/>
+
+<script type="text/javascript" src="/jquery-raptor/plugins/save/jquery.ui.editor.savejson.js"></script>
+<script type="text/javascript" src="/jquery-raptor/plugins/save/jquery.ui.editor.saverest.js"></script>
+<script type="text/javascript" src="/jquery-raptor/plugins/save/jquery.ui.editor.saveui.js"></script>
+
+<script type="text/javascript" src="/jquery-raptor/plugins/raptorize/jquery.ui.editor.raptorize.js"></script>
+
+<script type="text/javascript" src="/jquery-raptor/plugins/paste/jquery.ui.editor.paste.js"></script>
+<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/paste/jquery.ui.editor.paste.less"/>
+
+<script type="text/javascript" src="/jquery-raptor/plugins/cancel/jquery.ui.editor.cancel.js"></script>
+
+<script type="text/javascript" src="/jquery-raptor/plugins/list/jquery.ui.editor.list.js"></script>
+<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/list/jquery.ui.editor.list.less"/>
+
+<script type="text/javascript" src="/jquery-raptor/plugins/fontsize/jquery.ui.editor.fontsize.js"></script>
+<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/fontsize/jquery.ui.editor.fontsize.less"/>
+
+<script type="text/javascript" src="/jquery-raptor/plugins/hr/jquery.ui.editor.hr.js"></script>
+<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/hr/jquery.ui.editor.hr.less"/>
+
+<script type="text/javascript" src="/jquery-raptor/plugins/blockquote/jquery.ui.editor.blockquote.js"></script>
+<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/blockquote/jquery.ui.editor.blockquote.less"/>
+
+<script type="text/javascript" src="/jquery-raptor/plugins/tagmenu/jquery.ui.editor.tagmenu.js"></script>
+<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/tagmenu/jquery.ui.editor.tagmenu.less"/>
+
+<script type="text/javascript" src="/jquery-raptor/plugins/link/jquery.ui.editor.link.js"></script>
+<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/link/jquery.ui.editor.link.less"/>
+
+<script type="text/javascript" src="/jquery-raptor/plugins/imageresize/jquery.ui.editor.imageresize.js"></script>
+<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/imageresize/jquery.ui.editor.imageresize.less"/>
+
+<script type="text/javascript" src="/jquery-raptor/plugins/unsavededitwarning/jquery.ui.editor.unsavededitwarning.js"></script>
+<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/unsavededitwarning/jquery.ui.editor.unsavededitwarning.less"/>
+
+<script type="text/javascript" src="/jquery-raptor/plugins/i18n/jquery.ui.editor.i18n.js"></script>
+<script type="text/javascript" src="/jquery-raptor/plugins/i18n/locales/en.js"></script>
+<script type="text/javascript" src="/jquery-raptor/plugins/i18n/locales/zh_CN.js"></script>
+<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/i18n/jquery.ui.editor.i18n.less"/>
+
+<script>less = { env: 'development' }</script>
+<script type="text/javascript" src="/jquery-raptor/dependencies/less.js"></script>
