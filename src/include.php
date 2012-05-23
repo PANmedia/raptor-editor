37c7d797d803dc05c2223d3b496658db5b1b8060
David Neilsen <david@panmedia.co.nz>
2012-05-24 10:48:41 +1200

    Update include file

--- src/include.php
+++ src/include.php
@@ -1,101 +1,68 @@
+<?php
+    $uri = isset($uri) ? $uri : '/jquery-raptor/';
+?>
+
 <!-- Libraries -->
-<script type="text/javascript" src="/jquery-raptor/dependencies/jquery.cookie.js"></script>
-
-<script type="text/javascript" src="/jquery-raptor/dependencies/rangy/rangy-core.js"></script>
-<script type="text/javascript" src="/jquery-raptor/dependencies/rangy/rangy-cssclassapplier.js"></script>
-<script type="text/javascript" src="/jquery-raptor/dependencies/rangy/rangy-selectionsaverestore.js"></script>
-
-<link rel="stylesheet" href="/jquery-raptor/dependencies/tiptip/tiptip.css"/>
-<script type="text/javascript" src="/jquery-raptor/dependencies/tiptip/jquery.tiptip.js"></script>
-
-<script type="text/javascript" src="/jquery-raptor/dependencies/raptorize/jquery.raptorize.js"></script>
-
-<!-- Editor -->
-<script type="text/javascript" src="/jquery-raptor/jquery.ui.editor.init.js"></script>
-<script type="text/javascript" src="/jquery-raptor/jquery.ui.editor.domtools.js"></script>
-<script type="text/javascript" src="/jquery-raptor/template.js"></script>
-<script type="text/javascript" src="/jquery-raptor/support.js"></script>
-<script type="text/javascript" src="/jquery-raptor/jquery.ui.editor.js"></script>
-<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/jquery.ui.editor.layout.less"/>
-<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/jquery.ui.editor.style.less"/>
-<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/support.less"/>
-
-<!-- Plugins -->
-<script type="text/javascript" src="/jquery-raptor/plugins/logo/jquery.ui.editor.logo.js"></script>
-<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/logo/jquery.ui.editor.logo.less"/>
-
-<script type="text/javascript" src="/jquery-raptor/plugins/dock/jquery.ui.editor.dock.js"></script>
-<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/dock/jquery.ui.editor.dock.less"/>
-
-<script type="text/javascript" src="/jquery-raptor/plugins/tiptip/jquery.ui.editor.tiptip.js"></script>
-
-<script type="text/javascript" src="/jquery-raptor/plugins/clicktoedit/jquery.ui.editor.clicktoedit.js"></script>
-<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/clicktoedit/jquery.ui.editor.clicktoedit.less"/>
-
-<script type="text/javascript" src="/jquery-raptor/plugins/clean/jquery.ui.editor.clean.js"></script>
-<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/clean/jquery.ui.editor.clean.less"/>
-
-<script type="text/javascript" src="/jquery-raptor/plugins/float/jquery.ui.editor.float.js"></script>
-<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/float/jquery.ui.editor.float.less"/>
-
-<script type="text/javascript" src="/jquery-raptor/plugins/alignment/jquery.ui.editor.alignment.js"></script>
-<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/alignment/jquery.ui.editor.alignment.less"/>
-
-<script type="text/javascript" src="/jquery-raptor/plugins/basic/jquery.ui.editor.basic.js"></script>
-<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/basic/jquery.ui.editor.basic.less"/>
-
-<script type="text/javascript" src="/jquery-raptor/plugins/history/jquery.ui.editor.history.js"></script>
-<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/history/jquery.ui.editor.history.less"/>
-
-<script type="text/javascript" src="/jquery-raptor/plugins/viewsource/jquery.ui.editor.viewsource.js"></script>
-<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/viewsource/jquery.ui.editor.viewsource.less"/>
-
-<script type="text/javascript" src="/jquery-raptor/plugins/guides/jquery.ui.editor.guides.js"></script>
-<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/guides/jquery.ui.editor.guides.less"/>
-
-<script type="text/javascript" src="/jquery-raptor/plugins/save/jquery.ui.editor.savejson.js"></script>
-<script type="text/javascript" src="/jquery-raptor/plugins/save/jquery.ui.editor.saverest.js"></script>
-<script type="text/javascript" src="/jquery-raptor/plugins/save/jquery.ui.editor.saveui.js"></script>
-
-<script type="text/javascript" src="/jquery-raptor/plugins/raptorize/jquery.ui.editor.raptorize.js"></script>
-
-<script type="text/javascript" src="/jquery-raptor/plugins/paste/jquery.ui.editor.paste.js"></script>
-<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/paste/jquery.ui.editor.paste.less"/>
-
-<script type="text/javascript" src="/jquery-raptor/plugins/cancel/jquery.ui.editor.cancel.js"></script>
-
-<script type="text/javascript" src="/jquery-raptor/plugins/list/jquery.ui.editor.list.js"></script>
-<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/list/jquery.ui.editor.list.less"/>
-
-<script type="text/javascript" src="/jquery-raptor/plugins/fontsize/jquery.ui.editor.fontsize.js"></script>
-<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/fontsize/jquery.ui.editor.fontsize.less"/>
-
-<script type="text/javascript" src="/jquery-raptor/plugins/hr/jquery.ui.editor.hr.js"></script>
-<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/hr/jquery.ui.editor.hr.less"/>
-
-<script type="text/javascript" src="/jquery-raptor/plugins/blockquote/jquery.ui.editor.blockquote.js"></script>
-<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/blockquote/jquery.ui.editor.blockquote.less"/>
-
-<script type="text/javascript" src="/jquery-raptor/plugins/tagmenu/jquery.ui.editor.tagmenu.js"></script>
-<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/tagmenu/jquery.ui.editor.tagmenu.less"/>
-
-<script type="text/javascript" src="/jquery-raptor/plugins/link/jquery.ui.editor.link.js"></script>
-<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/link/jquery.ui.editor.link.less"/>
-
-<script type="text/javascript" src="/jquery-raptor/plugins/imageresize/jquery.ui.editor.imageresize.js"></script>
-<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/imageresize/jquery.ui.editor.imageresize.less"/>
-
-<script type="text/javascript" src="/jquery-raptor/plugins/unsavededitwarning/jquery.ui.editor.unsavededitwarning.js"></script>
-<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/unsavededitwarning/jquery.ui.editor.unsavededitwarning.less"/>
-
-<script type="text/javascript" src="/jquery-raptor/plugins/i18n/jquery.ui.editor.i18n.js"></script>
-<script type="text/javascript" src="/jquery-raptor/plugins/i18n/locales/en.js"></script>
-<script type="text/javascript" src="/jquery-raptor/plugins/i18n/locales/zh_CN.js"></script>
-<link rel="stylesheet/less" type="text/css" href="/jquery-raptor/plugins/i18n/jquery.ui.editor.i18n.less"/>
-
 <script type="text/javascript">
-    if (debugLevel >= MID) {
-        less = { env: 'development' }
+    if (typeof jQuery === 'undefined') {
+        document.write('<script type="text/javascript" src="<?php echo $uri ?>dependencies/jquery.js"><' + '/script>');
     }
 </script>
-<script type="text/javascript" src="/jquery-raptor/dependencies/less.js"></script>
+<script type="text/javascript">
+    if (typeof jQuery.ui === 'undefined') {
+        document.write('<script type="text/javascript" src="<?php echo $uri ?>dependencies/jquery-ui.js"><' + '/script>');
+//        document.write('<link rel="stylesheet" href="<?php echo $uri ?>dependencies/themes/aristo/jquery-ui.css"/>');
+//        document.write('<link rel="stylesheet" href="<?php echo $uri ?>dependencies/themes/smoothness/jquery-ui.css"/>');
+        document.write('<link rel="stylesheet" href="<?php echo $uri ?>dependencies/themes/redmond/jquery-ui.css"/>');
+    }
+</script>
+<script type="text/javascript" src="<?php echo $uri ?>dependencies/jquery.cookie.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>dependencies/rangy/rangy-core.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>dependencies/rangy/rangy-serializer.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>dependencies/rangy/rangy-cssclassapplier.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>dependencies/rangy/rangy-selectionsaverestore.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>dependencies/raptorize/jquery.raptorize.js"></script>
+
+<!-- Theme -->
+<link rel="stylesheet" type="text/css" href="<?php echo $uri ?>theme/theme.css"/>
+
+<!-- Editor -->
+<script type="text/javascript" src="<?php echo $uri ?>jquery.ui.editor.i18n.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>jquery.ui.editor.init.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>jquery.ui.editor.domtools.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>jquery.ui.editor.template.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>jquery.ui.editor.support.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>jquery.ui.editor.js"></script>
+
+<!-- Plugins -->
+<script type="text/javascript" src="<?php echo $uri ?>plugins/toolbartip/jquery.ui.editor.toolbartip.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>plugins/logo/jquery.ui.editor.logo.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>plugins/dock/jquery.ui.editor.dock.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>plugins/clicktoedit/jquery.ui.editor.clicktoedit.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>plugins/clean/jquery.ui.editor.clean.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>plugins/float/jquery.ui.editor.float.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>plugins/alignment/jquery.ui.editor.alignment.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>plugins/basic/jquery.ui.editor.basic.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>plugins/history/jquery.ui.editor.history.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>plugins/viewsource/jquery.ui.editor.viewsource.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>plugins/guides/jquery.ui.editor.guides.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>plugins/save/jquery.ui.editor.savejson.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>plugins/save/jquery.ui.editor.saverest.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>plugins/save/jquery.ui.editor.saveui.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>plugins/raptorize/jquery.ui.editor.raptorize.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>plugins/paste/jquery.ui.editor.paste.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>plugins/cancel/jquery.ui.editor.cancel.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>plugins/list/jquery.ui.editor.list.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>plugins/fontsize/jquery.ui.editor.fontsize.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>plugins/hr/jquery.ui.editor.hr.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>plugins/blockquote/jquery.ui.editor.blockquote.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>plugins/tagmenu/jquery.ui.editor.tagmenu.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>plugins/link/jquery.ui.editor.link.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>plugins/imageresize/jquery.ui.editor.imageresize.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>plugins/unsavededitwarning/jquery.ui.editor.unsavededitwarning.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>plugins/embed/jquery.ui.editor.embed.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>plugins/length/jquery.ui.editor.length.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>plugins/i18n/jquery.ui.editor.i18n.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>plugins/i18n/locales/en.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>plugins/i18n/locales/zh_CN.js"></script>
+<script type="text/javascript" src="<?php echo $uri ?>plugins/debug/jquery.ui.editor.debug.js"></script>
