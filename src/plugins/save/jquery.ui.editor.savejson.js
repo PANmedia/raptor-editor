00c21207787f3c98d6a735fde407808360f1fc24
David Neilsen <david@panmedia.co.nz>
2012-06-28 10:21:46 +1200

    Fire cleaned on save

--- src/plugins/save/jquery.ui.editor.savejson.js
+++ src/plugins/save/jquery.ui.editor.savejson.js
@@ -169,6 +169,7 @@ $.ui.editor.registerPlugin('saveJson', /** @lends $.editor.plugin.saveJson.proto
                 } else if (this.failed > 0) {
                     this.editor.showError(_('Saved {{saved}} out of {{dirty}} content blocks.', this));
                 } else {
+                    this.editor.fire('cleaned');
                     this.editor.showConfirm(_('Successfully saved {{saved}} content block(s).', this), {
                         delay: 1000,
                         hide: function() {
