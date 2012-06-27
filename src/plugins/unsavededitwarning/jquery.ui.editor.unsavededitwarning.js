2f8fb65cc27695f862c691c77cf593d280f4350a
David Neilsen <david@panmedia.co.nz>
2012-06-28 10:21:33 +1200

    Fix unsaved edit warning

--- src/plugins/unsavededitwarning/jquery.ui.editor.unsavededitwarning.js
+++ src/plugins/unsavededitwarning/jquery.ui.editor.unsavededitwarning.js
@@ -3,7 +3,6 @@
  * @author David Neilsen david@panmedia.co.nz
  * @author Michael Robinson michael@panmedia.co.nz
  */
-
 (function() {
     /**
      * The warning message node.
@@ -17,104 +16,51 @@
      */
     var dirty = 0;
 
-/**
- * @name $.editor.plugin.unsavedEditWarning
- * @augments $.ui.editor.defaultPlugin
- * @see $.editor.plugin.unsavedEditWarning.options
- * @class
- */
-$.ui.editor.registerPlugin('unsavedEditWarning', /** @lends $.editor.plugin.unsavedEditWarning.prototype */ {
-
     /**
-     * @name $.editor.plugin.unsavedEditWarning.options
-     * @namespace Default options
-     * @see $.editor.plugin.unsavedEditWarning
-     * @type {Object}
+     * @name $.editor.plugin.unsavedEditWarning
+     * @augments $.ui.editor.defaultPlugin
+     * @see $.editor.plugin.unsavedEditWarning.options
+     * @class
      */
-    options: /** @lends $.editor.plugin.unsavedEditWarning.options.prototype */  {
+    $.ui.editor.registerPlugin('unsavedEditWarning', /** @lends $.editor.plugin.unsavedEditWarning.prototype */ {
 
         /**
-         * @see $.editor.plugin.unsavedEditWarning.options
-         * @type {Object}
+         * @see $.ui.editor.defaultPlugin#init
          */
-//        position: {
-//            collision: 'right bottom',
-//            at: 'right bottom',
-//            my: 'right bottom',
-//            using: function(position) {
-//                $(this).css({
-//                    position: 'absolute',
-//                    top: position.top,
-//                    left: position.left
-//                });
-//            }
-//        }
-    },
+        init: function(editor, options) {
+            var plugin = this;
 
-    /**
-     * @see $.ui.editor.defaultPlugin#init
-     */
-    init: function(editor, options) {
-        if (!warning) {
-            warning = $(editor.getTemplate('unsavededitwarning.warning', this.options))
-                .attr('id', editor.getUniqueId())
-                .appendTo('body');
+            if (!warning) {
+                warning = $(editor.getTemplate('unsavededitwarning.warning', this.options))
+                    .attr('id', editor.getUniqueId())
+                    .appendTo('body')
+                    .bind('mouseenter.' + editor.widgetName, function() {
+                        $.ui.editor.eachInstance(function(editor) {
+                            if (editor.isDirty()) {
+                                editor.getElement().addClass(plugin.options.baseClass + '-dirty');
+                            }
+                        });
+                    })
+                    .bind('mouseleave.' + editor.widgetName, function() {
+                        $('.' + plugin.options.baseClass + '-dirty').removeClass(plugin.options.baseClass + '-dirty');
+                    });
+            }
+
+            editor.bind('dirty', function() {
+                dirty++;
+                if (dirty > 0) {
+                    warning.addClass(plugin.options.baseClass + '-visible');
+                }
+            });
+
+            editor.bind('cleaned', function() {
+                dirty--;
+                if (dirty === 0) {
+                    warning.removeClass(plugin.options.baseClass + '-visible');
+                }
+            });
         }
 
-        var plugin = this;
-        editor.bind('dirty', function() {
-            dirty++;
-            if (dirty > 0) {
-                warning.addClass(plugin.options.baseClass + '-visible');
-            }
-        });
-
-        editor.bind('cleaned', function() {
-            dirty--;
-            if (dirty === 0) {
-                warning.removeClass(plugin.options.baseClass + '-visible');
-            }
-        });
-
-//        editor.bind('change', function() {
-//            if (editor.isDirty() && editor.isEditing()) this.show();
-//            else this.hide();
-//        }, this);
-//
-//        editor.bind('destroy', function() {
-//            this.warning.remove();
-//            this.warning = null;
-//        }, this);
-    },
-
-    /**
-     * Show the warning
-     */
-    show: function() {
-//        this.reposition();
-//        this.warning.addClass(this.options.baseClass + '-visible').show();
-    },
-
-    /**
-     * Hide the warning
-     */
-    hide: function() {
-//        this.warning.removeClass(this.options.baseClass + '-visible');
-    },
-
-    /**
-     * Reposition the warning according to the element's dimensions
-     */
-    reposition: function() {
-//        // Have to use the ID because if given the element, the browser will memory leak and crash
-//        this.options.position.of = '#' + this.editor.getElement().attr('id');
-//        // <strict>
-//        if (!$(this.options.position.of).length) {
-//            handleError(_('Editor element has been removed, unsaved edit warning plugin cannot reposition'));
-//        }
-//        // </strict>
-//        this.warning.position(this.options.position);
-    }
-});
+    });
 
 })();
