c9fd5cbf95f2bc4906537a7433d4007ccc6425de
Michael Robinson <mike@panmedia.co.nz>
2012-07-10 14:54:25 +1200

    Removed changeTag, tagSelection & tagSelectionWithin

--- src/jquery.ui.editor.domtools.js
+++ src/jquery.ui.editor.domtools.js
@@ -552,48 +552,6 @@ var domTools = {
                range.startContainer === range.endContainer;
     },
 
-    changeTag: function(range, tag) {
-        var contents = range.extractContents();
-        this.insertDomFragmentBefore(contents, range.startContainer, tag);
-        $(range.startContainer).remove();
-    },
-
-    /**
-     * Behaviour similar to {@link domTools.tagSelectionWithin}, only in cases where the selection or cursor acts on text nodes only, the wrapping element will be modified.
-     * @param  {String} tag Name of tag to change to or wrap selection with.
-     * @param  {RangySelection} selection A RangySelection, or by default, the current selection.
-     */
-    tagSelection: function(tag, selection) {
-        this.tagSelectionWithin(tag, null, selection);
-    },
-
-    /**
-     * If selection is empty, change the tag for the element the cursor is currently within, else wrap the selection with tag.
-     * @param  {String} tag Name of tag to change to or wrap selection with.
-     * @param  {jQuery|null} within The element to perform changes within. If The cursor is within, or the selection contains text nodes only, the text will be wrapped with tag. If null, changes will be applied to the wrapping tag.
-     * @param  {RangySelection} selection A RangySelection, or by default, the current selection.
-     */
-    tagSelectionWithin: function(tag, within, sel) {
-        selectionEachRange(function(range) {
-            if (this.isEmpty(range)) {
-                rangeExpandToParent(range);
-                within = $(within)[0];
-                if (typeof within !== 'undefined'
-                    && range.startContainer === within
-                    && range.endContainer === within) {
-                    // Apply to the content of the 'within' element
-                    this.wrapInner($(within), tag);
-                } else {
-                    // Apply to the whole element
-                    this.changeTag(range, tag);
-                }
-            } else {
-                var content = range.extractContents();
-                this.replaceRange(fragmentToHtml(content, tag), range);
-            }
-        }, sel, this);
-    },
-
     /**
      * Check that the given element is one of the the given tags
      * @param  {jQuery|Element} element The element to be tested.
