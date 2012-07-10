7010afef9821d955485256f29853c73012da9641
Michael Robinson <mike@panmedia.co.nz>
2012-07-10 14:55:02 +1200

    Updated tag menu addon to ensure replacements are inserted between invalid tags, and some bug fixing.

--- src/plugins/tagmenu/jquery.ui.editor.tagmenu.js
+++ src/plugins/tagmenu/jquery.ui.editor.tagmenu.js
@@ -12,6 +12,11 @@ $.ui.editor.registerUi({
      */
     tagMenu: /** @lends $.editor.plugin.tagMenu.prototype */ {
 
+        validParents: [
+            'blockquote', 'body', 'button', 'center', 'dd', 'div', 'fieldset', 'form', 'iframe', 'li',
+            'noframes', 'noscript', 'object', 'td', 'th'
+        ],
+
         /**
          * @see $.ui.editor.defaultUi#init
          */
@@ -28,10 +33,40 @@ $.ui.editor.registerUi({
                 change: function(value) {
                     // Prevent injection of illegal tags
                     if (typeof value === 'undefined' || value === 'na') {
-                        ui.change();
                         return;
                     }
-                    editor.tagSelectionWithin(value, editor.getElement());
+
+                    var editingElement = editor.getElement()[0];
+                    var selectedElement = editor.getSelectedElements();
+                    if (!editor.getSelectedHtml() || editor.getSelectedHtml() === '') {
+                        // Do not attempt to modify editing element's tag
+                        if ($(selectedElement)[0] === $(editingElement)[0]) {
+                            return;
+                        }
+                        editor.saveSelection();
+                        var replacementElement = $('<' + value + '>').html(selectedElement.html());
+                        selectedElement.replaceWith(replacementElement);
+                        editor.restoreSelection();
+                    } else {
+                        var selectedElementParent = $(editor.getSelectedElements()[0]).parent();
+                        var temporaryClass = this.options.baseClass + '-selection';
+                        var replacementHtml = $('<' + value + '>').html(editor.getSelectedHtml()).addClass(temporaryClass);
+
+                        /*
+                         * Replace selection if the selected element parent or the selected element is the editing element,
+                         * instead of splitting the editing element.
+                         */
+                        if (selectedElementParent === editingElement
+                            || editor.getSelectedElements()[0] === editingElement) {
+                            editor.replaceSelection(replacementHtml);
+                        } else {
+                            editor.replaceSelectionWithinValidTags(replacementHtml, this.validParents);
+                        }
+
+                        editor.selectInner(editor.getElement().find('.' + temporaryClass).removeClass(temporaryClass));
+                    }
+
+                    editor.checkChange();
                 }
             });
         },
@@ -41,13 +76,17 @@ $.ui.editor.registerUi({
          */
         change: function() {
             var tag = this.editor.getSelectedElements()[0];
-            if (!tag) return;
+            if (!tag) {
+                $(this.ui.button).toggleClass('ui-state-disabled', true);
+                return;
+            }
             tag = tag.tagName.toLowerCase();
             if (this.ui.select.find('option[value=' + tag + ']').length) {
                 this.ui.val(tag);
             } else {
                 this.ui.val('na');
             }
+            $(this.ui.button).toggleClass('ui-state-disabled', this.editor.getElement()[0] === this.editor.getSelectedElements()[0]);
         }
     }
 });
