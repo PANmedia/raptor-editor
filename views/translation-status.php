df7549ee7bd22045f48724c02cd1b1e32b18bc10
Michael Robinson <mike@panmedia.co.nz>
2012-08-02 11:36:29 +1200

    Updated translation status view

--- views/translation-status.php
+++ views/translation-status.php
@@ -22,8 +22,27 @@
             <em>Not specified</em>
             <?php endif; ?>
         </td>
-        <td><?php echo $data['percent']; ?>%</td>
+        <td>
+            <?php echo $data['percent']; ?>%
+            <?php if ($data['untranslated']): ?>
+            <a onclick="rpToggle(this, '<?php echo $name; ?>-untranslated')" href="#<?php echo $name; ?>-untranslated">View untranslated</a>
+            <div id="<?php echo $name; ?>-untranslated" style="display:none">
+                <?php foreach ($data['untranslated'] as $untranslated): ?>
+                <p><?php echo $untranslated; ?></p>
+                <?php endforeach; ?>
+            </div>
+            <?php endif; ?>
+        </td>
     </tr>
 <?php endforeach; ?>
     </tbody>
 </table>
+
+<script type="text/javascript">
+    function rpToggle(a, id) {
+        console.log(a);
+        var div = document.getElementById(id);
+        a.innerText = div.style.display == 'none' ? 'Hide untranslated' : 'View untranslated';
+        div.style.display = div.style.display == 'none' ? 'block' : 'none';
+    }
+</script>
