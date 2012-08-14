43c1fbac093d1bb52079ee5a4d780b47000e225d
David Neilsen <david@panmedia.co.nz>
2012-08-14 14:59:00 +1200

    Update build to run in background

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
-call phing 0deps
-call phing nc
-call phing light
-call phing debug
-call phing
+start /b cmd /c call phing 0deps
+start /b cmd /c call phing nc
+start /b cmd /c call phing debug
+start /b cmd /c call phing light
+start /b cmd /c call phing
