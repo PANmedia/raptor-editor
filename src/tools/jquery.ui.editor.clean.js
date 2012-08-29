e9355adcc149e02296c8675051d9c984e4991aa0
David Neilsen <david@panmedia.co.nz>
2012-08-30 09:41:17 +1200

    Add comments to clean helpers.

--- src/tools/jquery.ui.editor.clean.js
+++ src/tools/jquery.ui.editor.clean.js
@@ -1,9 +1,22 @@
 /**
- * @fileOverview Cleaning helper functions
+ * @fileOverview Cleaning helper functions.
  * @author David Neilsen - david@panmedia.co.nz
  * @author Michael Robinson - michael@panmedia.co.nz
  */
 
+/**
+ * Replaces elements in another elements. E.g.
+ *
+ * @example
+ * cleanReplaceElements('.content', {
+ *     'b': '<strong/>',
+ *     'i': '<em/>',
+ * });
+ *
+ * @param  {jQuery|Element|Selector} selector The element to be find and replace in.
+ * @param  {Object} replacements A map of selectors to replacements. The replacement
+ *   can be a jQuery object, an element, or a selector.
+ */
 function cleanReplaceElements(selector, replacements) {
     for (var find in replacements) {
         var replace = replacements[find];
@@ -21,6 +34,11 @@ function cleanReplaceElements(selector, replacements) {
     }
 }
 
+/**
+ * Unwrap function. Currently just wraps jQuery.unwrap() but may be extended in future.
+ *
+ * @param  {jQuery|Element|Selector} selector The element to unwrap.
+ */
 function cleanUnwrapElements(selector) {
     $(selector).unwrap();
 }
