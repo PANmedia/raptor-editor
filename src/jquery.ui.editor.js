eaf452e793c2b9fc56970fbf62c318bd0c29b954
Michael Robinson <mike@panmedia.co.nz>
2012-09-06 16:19:05 +1200

    Ensure tag tree can contain only elements up to and including the editing element

--- src/jquery.ui.editor.js
+++ src/jquery.ui.editor.js
@@ -466,22 +466,26 @@ $.widget('ui.editor',
             if (node.nodeType === 3) {
                 // If nodes common parent is a text node, then use its parent
                 element = $(node).parent();
-            // } else if(this.rangeEmptyTag(range)) {
-            //     element = $(this.domFragmentToHtml(range.cloneContents()));
             } else {
                 // Or else use the node
                 element = $(node);
             }
 
+            // Ensure the element is the editing element or a child of the editing element
+            if (!editor.isRoot(element) && !$.contains(editor.getElement().get(0), element.get(0))) {
+                element = editor.getElement();
+            }
+
             var list = [];
             lists.push(list);
-            // Loop untill we get to the root element, or the body tag
+            // Loop until we get to the root element, or the body tag
             while (element[0] && !editor.isRoot(element) && element[0].tagName.toLowerCase() !== 'body') {
                 // Add the node to the list
                 list.push(element);
                 element = element.parent();
             }
             list.reverse();
+            console.log($(list[0]));
             if (title) title += ' | ';
             title += this.getTemplate('root');
             for (var j = 0; j < list.length; j++) {
