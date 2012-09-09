7a02c130f5077e73031b432f9bdb0b8183a0d3c4
Michael Robinson <mike@panmedia.co.nz>
2012-09-10 10:19:56 +1200

    Added types tools

---
+++ src/tools/jquery.ui.types.js
@@ -0,0 +1,14 @@
+/**
+ * @fileOverview Type checking functions.
+ * @author Michael Robinson michael@panmedia.co.nz
+ * @author David Neilsen david@panmedia.co.nz
+ */
+
+/**
+ * Determine whether object is a number
+ * @param  {mixed} object The object to be tested
+ * @return {Boolean} True if the object is a number.
+ */
+function typeIsNumber(object) {
+    return !isNaN(object - 0) && object !== null;
+}
