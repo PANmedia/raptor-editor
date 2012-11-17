e5e734ab4228e3c13a3cfc2aecd37e72a7d1d8a8
David Neilsen <david@panmedia.co.nz>
2012-11-18 11:48:07 +1300

    Table helper stub

---
+++ src/tools/jquery.ui.editor.table.js
@@ -0,0 +1,12 @@
+
+// Detach table from DOM for speed before adding the rows.
+
+
+
+function tableCanMergeCells(table, startIndex, endIndex) {
+    
+}
+
+function tableInsertColumn() {};
+function tableInsertRow() {};
+function tableMergeCells() {};
