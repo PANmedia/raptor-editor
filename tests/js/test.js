80be2c57e0976989d80255f9f0b4fba3252b1243
David Neilsen <david@panmedia.co.nz>
2012-11-19 11:52:18 +1300

    Update test helper

--- tests/js/test.js
+++ tests/js/test.js
@@ -1,11 +1,24 @@
-function test(container, action) {
+function test(container, action, format) {
     var output = $(container).find('.test-output'),
         input = $(container).find('.test-input'),
         diff = $(container).find('.test-diff'),
         expected = $(container).find('.test-expected');
 
+    if (output.length === 0) {
+        output = $('<div>').addClass('test-output').appendTo(container);
+    }
+
+    if (diff.length === 0) {
+        diff = $('<div>').addClass('test-diff').appendTo(container);
+    }
+
     output.html(input.html());
     action(output);
+
+    if (typeof format === 'undefined' || format) {
+        formatElement(expected);
+        formatElement(output);
+    }
 
     diff.html(diffstr(expected.html(), output.html()));
 
