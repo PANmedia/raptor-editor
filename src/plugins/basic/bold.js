2be1df96d575de34298801c39886b8533af0dceb
David Neilsen <david@panmedia.co.nz>
2012-11-21 15:50:38 +1300

    Update new bold plugin

--- src/plugins/basic/bold.js
+++ src/plugins/basic/bold.js
@@ -1,11 +1,33 @@
-$.ui.editor.registerUi('textBold', new Button({
+Raptor.registerUi('textBold', new Button({
     name: 'bold',
     title: _('bold'),
     hotkey: 'ctrl+b',
-    options: {},
+    applier: null,
+    options: {
+        tag: null,
+        classes: null,
+        cssPrefix: null
+    },
+
     action: function() {
-        selectionToggleWrapper('strong', {
-            classes: this.options.classes || this.options.cssPrefix + 'bold'
-        });
+        var ranges = this.raptor.getRanges();
+        this.getApplier().toggleRanges(ranges);
+    },
+
+    getApplier: function() {
+        if (this.applier === null) {
+            this.applier = rangy.createCssClassApplier(this.getClass(), {
+                elementTagName: this.getTag()
+            });
+        }
+        return this.applier;
+    },
+
+    getTag: function() {
+        return this.options.tag || 'strong';
+    },
+
+    getClass: function() {
+        return this.options.classes || this.options.cssPrefix + 'bold'
     }
 }));
