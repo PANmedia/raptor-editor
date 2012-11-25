1c650ca2b705ad2ac06cf8c21bb5f9be71607dbd
David Neilsen <david@panmedia.co.nz>
2012-11-25 14:29:59 +1300

    Update button

--- src/ui/components/button.js
+++ src/ui/components/button.js
@@ -1,31 +1,38 @@
 function Button(overrides) {
-    this.editor = null;
-    this.buttonText = '';
+    this.raptor = null;
+    this.options = {};
     this.button = null;
+    this.buttonText = '';
     for (var key in overrides) {
         this[key] = overrides[key];
     }
 };
 
-Button.prototype.init = function(editor) {
-    this.editor = editor;
+Button.prototype.init = function(raptor) {
+    this.setOptions();
+    this.raptor = raptor;
     this.button = $('<div>')
-        .html(this.buttonText)
+        .html(this.text)
         .click($.proxy(this.click, this))
         .mouseenter($.proxy(this.mouseEnter, this))
         .mouseleave($.proxy(this.mouseLeave, this));
-    aButton(this.button);
+    aButton(this.button, this.options);
     return this.button;
+};
+            
+Button.prototype.setOptions = function() {
+    this.options.title = _(this.name);
+    this.options.icon = 'ui-icon-' + this.name;
 };
 
 Button.prototype.mouseEnter = function() {
-    this.editor.actionPreview(this.action.bind(this));
+    this.raptor.actionPreview(this.action.bind(this));
 };
 
 Button.prototype.mouseLeave = function() {
-    this.editor.actionPreviewRemove();
+    this.raptor.actionPreviewRestore();
 };
 
 Button.prototype.click = function() {
-    this.editor.actionApply(this.action.bind(this));
+    this.raptor.actionApply(this.action.bind(this));
 };
