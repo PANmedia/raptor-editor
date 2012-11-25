d5de3ebc409ff94a68e2dc36ce0386aaa6b379a9
David Neilsen <david@panmedia.co.nz>
2012-11-25 14:22:26 +1300

    Update jQuery UI button adapter

--- src/adapters/jquery-ui.js
+++ src/adapters/jquery-ui.js
@@ -1,3 +1,28 @@
+
+/**
+ * 
+ * @param {type} element
+ * @param {type} options
+ * @param {type} options.baseClass
+ * @param {type} options.title
+ * @param {type} options.disabled
+ * @param {type} options.text
+ * @param {type} options.label
+ * @param {type} options.icon
+ * @returns {unresolved}
+ */
 function aButton(element, options) {
-    return $(element).button(options);
+    options = options || {};
+    return $(element)
+        .addClass(options.baseClass)
+        .attr('name', name)
+        .attr('title', options.title)
+        .button({
+            icons: {
+                primary: options.icon || 'ui-icon-' + options.baseClass
+            },
+            disabled: options.disabled ? true : false,
+            text: options.text || false,
+            label: options.label || null
+        });
 }
