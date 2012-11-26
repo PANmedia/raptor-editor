e07392369b3d75a1de8c263df77f8d58e307fb0b
David Neilsen <david@panmedia.co.nz>
2012-11-26 19:18:36 +1300

    Add stub ui order to preset

--- src/presets/default.js
+++ src/presets/default.js
@@ -2,7 +2,29 @@ Raptor.registerPreset('toolbar', {
     layout: {
         type: 'toolbar',
         options: {
-            uiOrder: null
+//            uiOrder: [
+//                ['logo'],
+//                ['save', 'cancel'],
+//                ['dock', 'showGuides', 'clean'],
+//                ['viewSource'],
+//                ['undo', 'redo'],
+//                ['alignLeft', 'alignCenter', 'alignJustify', 'alignRight'],
+//                ['textBold', 'textItalic', 'textUnderline', 'textStrike'],
+//                ['textSuper', 'textSub'],
+//                ['listUnordered', 'listOrdered'],
+//                ['hr', 'quoteBlock'],
+//                ['fontSizeInc', 'fontSizeDec'],
+//                ['colorPickerBasic'],
+//                ['clearFormatting'],
+//                ['link', 'unlink'],
+//                ['embed'],
+//                ['floatLeft', 'floatNone', 'floatRight'],
+//                ['tagMenu'],
+//                ['i18n'],
+//                ['raptorize'],
+//                ['statistics'],
+//                ['debugReinit', 'debugDestroy']
+//            ]
         }
     }
 });
