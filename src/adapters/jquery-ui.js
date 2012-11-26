f2dd5663db61ef6af34968e7c4daeca634ff5bec
David Neilsen <david@panmedia.co.nz>
2012-11-27 11:01:06 +1300

    Update jquery ui adapter

--- src/adapters/jquery-ui.js
+++ src/adapters/jquery-ui.js
@@ -1,28 +1,20 @@
-
-/**
- * 
- * @param {type} element
- * @param {type} options
- * @param {type} options.baseClass
- * @param {type} options.title
- * @param {type} options.disabled
- * @param {type} options.text
- * @param {type} options.label
- * @param {type} options.icon
- * @returns {unresolved}
- */
 function aButton(element, options) {
-    options = options || {};
-    return $(element)
-        .addClass(options.baseClass)
-        .attr('name', name)
-        .attr('title', options.title)
-        .button({
-            icons: {
-                primary: options.icon || 'ui-icon-' + options.baseClass
-            },
-            disabled: options.disabled ? true : false,
-            text: options.text || false,
-            label: options.label || null
-        });
+    return $(element).button(options);
+}
+
+function aMenu(element, options) {
+    return $(element).menu(options);
+}
+
+function aDialog(element, options) {
+    return $(element).dialog(options);
+}
+
+function aDialogOpen(element) {
+    return $(element).dialog('open');
+}
+
+
+function aDialogClose(element) {
+    return $(element).dialog('close');
 }
