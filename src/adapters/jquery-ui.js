b9dc4852e1d4da9aa9905d68dbd050fd5127419a
David Neilsen <david@panmedia.co.nz>
2012-11-27 15:57:47 +1300

    Adapt dialog icons

--- src/adapters/jquery-ui.js
+++ src/adapters/jquery-ui.js
@@ -7,7 +7,17 @@ function aMenu(element, options) {
 }
 
 function aDialog(element, options) {
-    return $(element).dialog(options);
+    var dialog = $(element).dialog(options);
+    // TODO: Remove this when jQuery UI 1.10 is released
+    var buttons = dialog.parent().find('.ui-dialog-buttonpane');
+    for (var i = 0, l = options.buttons.length; i < l; i++) {
+        aButton(buttons.find('button:eq(' + i + ')'), {
+            icons: {
+                primary: options.buttons[i].icons.primary
+            }
+        });
+    }
+    return dialog;
 }
 
 function aDialogOpen(element) {
