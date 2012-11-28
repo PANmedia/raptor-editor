76e9cc7d9f150625f1cbf9d023b8af0c53480dd6
David Neilsen <david@panmedia.co.nz>
2012-11-28 18:00:29 +1300

    Update style tools

--- src/tools/style.js
+++ src/tools/style.js
@@ -1,8 +1,32 @@
 function styleSwapState(element, newState) {
-    var previousState = {};
+    var node = element.get(0),
+        previousState = {};
+    // Double loop because jQuery will automatically assign other style properties like 'margin-left' when setting 'margin'
     for (var key in newState) {
-        previousState[key] = element.get(0).style[key];
-        element.css(key, newState[key]);
+        previousState[key] = node.style[key];
+    }
+    for (var key in newState) {
+        element.css(key, newState[key])
     }
     return previousState;
+}
+
+function styleSwapWithWrapper(wrapper, inner, newState) {
+    var innerNode = inner.get(0),
+        previousState = {};
+    // Double loop because jQuery will automatically assign other style properties like 'margin-left' when setting 'margin'
+    for (var key in newState) {
+        previousState[key] = innerNode.style[key];
+    }
+    for (var key in newState) {
+        wrapper.css(key, inner.css(key));
+        inner.css(key, newState[key])
+    }
+    return previousState;
+}
+
+function styleRestoreState(element, state) {
+    for (var key in state) {
+        element.css(key, state[key] || '');
+    }
 }
