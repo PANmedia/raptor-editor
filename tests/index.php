9a63306fde346403fa63ce937034e8679f255484
David Neilsen <david@panmedia.co.nz>
2012-11-29 15:11:39 +1300

    Automatic testing

---
+++ tests/index.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="cache-control" content="no-cache" />
+        <link type="text/css" rel="stylesheet" href="css/style.css"/>
+        <script src="../src/dependencies/jquery.js"></script>
+        <script type="text/javascript">
+            $(function() {
+                function iframeResize() {
+                    $(this).height($($(this).get(0).contentDocument).find('html').outerHeight());
+                }
+                
+                function iframeAdd(src) {
+                    $('<iframe>')
+                        .attr('src', src)
+                        .load(iframeResize)
+                        .appendTo('.iframes');
+                }
+                
+                $('[name=run-all]').click(function() {
+                    $('iframe').remove();
+                    var time = 0;
+                    $('.tests a').each(function() {
+                        setTimeout(function() {
+                            iframeAdd($(this).attr('href'));
+                        }.bind(this), time += 50);
+                    });
+                });
+                
+                $('[name=verbose]').change(function() {
+                    $('iframe').each(function() {
+                        $('body', $(this).get(0).contentDocument).toggleClass('verbose').toggleClass('simple');
+                        iframeResize.call(this);
+                    });
+                });
+                
+                $('nav a').click(function() {
+                    $('iframe').remove();
+                    iframeAdd($(this).attr('href'));
+                    return false;
+                });
+            });
+        </script>
+    </head>
+    <body>
+        <aside>
+            <h1>Tests:</h1>
+            <div>
+                <button name="run-all">Run All</button>
+                <input type="checkbox" name="verbose" id="verbose" />
+                <label for="verbose">Verbose Output</label>
+            </div>
+            <nav class="tests">
+                <?php
+                    $findTests = function($case) {
+                        foreach (glob($case . '/*.html') as $file) {
+                            echo '<a target="test" href="cases/' . basename($case) . '/' . basename($file) . '">' . basename($case) . ' - ' . basename($file) . '</a>';
+                        }
+                    };
+                    foreach (glob(__DIR__ . '/cases/*') as $case) {
+                        $findTests($case);
+                    }
+                ?>
+            </nav>
+        </aside>
+        <div class="iframes">
+            <iframe id="test" name="test" src="test.html"></iframe>
+        </div>
+    </body>
+</html>
---
+++ tests/js/case.js
@@ -0,0 +1,39 @@
+function loadJS(src) {
+    document.write('<script type="text/javascript" src="../../' + src + '"></script>');
+}
+
+function loadCSS(href) {
+    document.write('<link type="text/css" rel="stylesheet" href="../../' + href + '" />');
+}
+
+// document.write('<meta charset="utf-8" />');
+// document.write('<meta http-equiv="cache-control" content="no-cache" />');
+
+loadCSS('css/case.css');
+
+loadJS('../src/dependencies/jquery.js');
+loadJS('../src/dependencies/jquery-ui.js');
+loadJS('../src/dependencies/rangy/rangy-core.js');
+loadJS('../src/dependencies/rangy/rangy-cssclassapplier.js');
+loadJS('../src/dependencies/rangy/rangy-selectionsaverestore.js');
+loadJS('../src/dependencies/rangy/rangy-serializer.js');
+loadJS('../src/dependencies/rangy/rangy-textrange.js');
+loadJS('../src/tools/action.js');
+loadJS('../src/tools/clean.js');
+loadJS('../src/tools/dock.js');
+loadJS('../src/tools/element.js');
+loadJS('../src/tools/fragment.js');
+loadJS('../src/tools/list.js');
+loadJS('../src/tools/range.js');
+loadJS('../src/tools/selection.js');
+loadJS('../src/tools/state.js');
+loadJS('../src/tools/string.js');
+loadJS('../src/tools/style.js');
+loadJS('../src/tools/table.js');
+loadJS('../src/tools/tag.js');
+loadJS('../src/tools/types.js');
+loadJS('js/beautify-html.js');
+loadJS('js/diff.js');
+loadJS('js/helpers.js');
+loadJS('js/test.js');
+loadJS('js/tokens-to-ranges.js');
