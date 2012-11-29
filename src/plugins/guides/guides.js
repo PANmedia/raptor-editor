064bf13457e272c5bf02c929c6bd08fba9286461
David Neilsen <david@panmedia.co.nz>
2012-11-29 14:26:46 +1300

    Remove log

--- src/plugins/guides/guides.js
+++ src/plugins/guides/guides.js
@@ -1,7 +1,6 @@
 Raptor.registerUi(new PreviewButton({
     name: 'guides',
     action: function() {
-        console.log(this.raptor.getElement().is('.raptor-ui-guides-visible'));
         this.raptor.getElement().toggleClass(this.options.baseClass + '-visible');
     }
 }));
