f6f3f139d275b0709f769cef8ebaef84c59233b0
David Neilsen <david@panmedia.co.nz>
2012-12-03 21:34:53 +1300

    Add custom menu class

---
+++ src/ui/components/custom-menu.js
@@ -0,0 +1,21 @@
+Menu.prototype.getMenu = function() {
+    if (!this.menu) {
+        this.menu = $('<div>')
+            .addClass('ui-menu ui-widget ui-widget-content ui-corner-all ' + this.options.baseClass + '-menu ' + this.raptor.options.baseClass + '-menu')
+            .html(this.menuContent)
+            .css('position', 'fixed')
+            .hide()
+            .appendTo('body')
+            .mousedown(function(event) {
+                // Prevent losing the selection on the editor target
+                event.preventDefault();
+            });
+        // Click off close event
+        $('html').click(function(event) {
+            if (this.button.button.has(event.target).length === 0) {
+                this.menu.hide();
+            }
+        }.bind(this));
+    }
+    return this.menu;
+};
