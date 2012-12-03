f751b270a1e293479c50d4c39f0e134b9c9c316c
David Neilsen <david@panmedia.co.nz>
2012-12-04 11:14:03 +1300

    Update tag menu plugin

--- src/plugins/tag-menu/tag-menu.js
+++ src/plugins/tag-menu/tag-menu.js
@@ -6,32 +6,109 @@ function TagMenu(options) {
 
 TagMenu.prototype = Object.create(SelectMenu.prototype);
 
+TagMenu.prototype.init = function(raptor) {
+    raptor.bind('selectionChange', this.updateButton.bind(this));
+    return SelectMenu.prototype.init.apply(this, arguments);
+};
+
+TagMenu.prototype.changeTag = function(tag) {
+    // Prevent injection of illegal tags
+    if (typeof tag === 'undefined' || tag === 'na') {
+        return;
+    }
+
+    var editingElement = this.raptor.getElement()[0];
+    var selectedElement = selectionGetElements();
+    if (!selectionGetHtml() || selectionGetHtml() === '') {
+        // Do not attempt to modify editing element's tag
+        if ($(selectedElement)[0] === $(editingElement)[0]) {
+            return;
+        }
+        selectionSave();
+        var replacementElement = $('<' + tag + '>').html(selectedElement.html());
+        selectedElement.replaceWith(replacementElement);
+        selectionRestore();
+    } else {
+        var selectedElementParent = $(selectionGetElements()[0]).parent();
+        var temporaryClass = this.options.baseClass + '-selection';
+        var replacementHtml = $('<' + tag + '>').html(selectionGetHtml()).addClass(temporaryClass);
+
+        /*
+         * Replace selection if the selected element parent or the selected element is the editing element,
+         * instead of splitting the editing element.
+         */
+        if (selectedElementParent === editingElement ||
+            selectionGetElements()[0] === editingElement) {
+            selectionReplace(replacementHtml);
+        } else {
+            selectionReplaceWithinValidTags(replacementHtml, this.validParents);
+        }
+
+        selectionSelectInner(this.raptor.getElement().find('.' + temporaryClass).removeClass(temporaryClass));
+    }
+
+    this.raptor.checkChange();
+};
+
 TagMenu.prototype.apply = function(event) {
+    this.raptor.actionApply(function() {
+        this.changeTag($(event.currentTarget).data('value'));
+    }.bind(this));
 };
 
 TagMenu.prototype.preview = function(event) {
+    if (this.preview) {
+        this.raptor.actionPreview(function() {
+            this.changeTag($(event.currentTarget).data('value'));
+        }.bind(this));
+    }
 };
 
 TagMenu.prototype.previewRestore = function(event) {
+    if (this.preview) {
+        this.raptor.actionPreviewRestore();
+    }
+};
+
+TagMenu.prototype.updateButton = function() {
+    var tag = selectionGetElements()[0],
+        button = this.getButton().getButton();
+    if (!tag) {
+        aButtonDisable(button);
+        return;
+    }
+    var tagName = tag.tagName.toLowerCase(),
+        option = this.getMenu().find('[data-value=' + tagName + ']');
+    if (option.length) {
+        aButtonSetLabel(button, option.html());
+    } else {
+        aButtonSetLabel(button, _('tagMenuTagNA'));
+    }
+    if (this.editor.getElement()[0] === tag) {
+        aButtonDisable(button);
+    } else {
+        aButtonEnable(button);
+    }
+};
+
+TagMenu.prototype.getButton = function() {
+    if (!this.button) {
+        this.button = new Button({
+            name: this.name,
+            action: this.show.bind(this),
+            preview: false,
+            options: this.options,
+            icon: false,
+        });
+    }
+    return this.button;
 };
 
 TagMenu.prototype.getMenuItems = function() {
-    if (!this.menuItems) {
-        this.menuItems = this.editor.getTemplate('tag-menu.menu', this.options);
-    }
-    
+    return $(this.editor.getTemplate('tag-menu.menu', this.options))
+        .click(this.apply.bind(this))
+        .mouseenter(this.preview.bind(this))
+        .mouseleave(this.previewRestore.bind(this));
 };
-
-TagMenu.prototype.getMenu = function() {
-    if (!this.menu) {
-        this.menuContent = this.editor.getTemplate('tag-menu.menu', this.options);
-        console.log(this.menuContent = $(this.menuContent).menu());
-        SelectMenu.prototype.getMenu.call(this)
-            .on('click', 'li', this.apply.bind(this))
-            .on('mouseenter', 'li', this.preview.bind(this))
-            .mouseleave(this.previewRestore.bind(this));
-    }
-    return this.menu;
-}
 
 Raptor.registerUi(new TagMenu());
