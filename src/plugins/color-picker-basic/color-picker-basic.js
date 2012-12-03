41373e23c2aaab544eee3af6e6d5cd960a643b78
David Neilsen <david@panmedia.co.nz>
2012-12-04 10:42:13 +1300

    Update color picker basic

--- src/plugins/color-picker-basic/color-picker-basic.js
+++ src/plugins/color-picker-basic/color-picker-basic.js
@@ -1,11 +1,54 @@
 function ColorPickerBasicMenu(options) {
     this.preview = true;
-    Menu.call(this, {
+    this.colors = [
+        'white',
+        'black',
+        'grey',
+        'blue',
+        'red',
+        'green',
+        'purple',
+        'orange'
+    ]
+    SelectMenu.call(this, {
         name: 'colorPickerBasic'
     });
 }
 
-ColorPickerBasicMenu.prototype = Object.create(Menu.prototype);
+ColorPickerBasicMenu.prototype = Object.create(SelectMenu.prototype);
+
+ColorPickerBasicMenu.prototype.init = function(raptor) {
+    raptor.bind('selectionChange', this.updateButton.bind(this));
+    return SelectMenu.prototype.init.apply(this, arguments);
+};
+
+ColorPickerBasicMenu.prototype.updateButton = function() {
+    var tag = selectionGetElements()[0],
+        button = this.getButton().getButton(),
+        color = null,
+        closest = null;
+    // TODO: set automatic icon color to the color of the text
+    aButtonSetLabel(button, _('colorPickerBasicAutomatic'));
+    aButtonSetIcon(button, false);
+    if (!tag) {
+        return;
+    }
+    tag = $(tag);
+    for (var i = 0, l = this.colors.length; i < l; i++) {
+        closest = $(tag).closest('.' + this.options.cssPrefix + this.colors[i]);
+        if (closest.length) {
+            color = this.colors[i];
+            break;
+        }
+    }
+    if (color) {
+        aButtonSetLabel(button, _('colorPickerBasic' + (color.charAt(0).toUpperCase() + color.slice(1))));
+        aButtonSetIcon(button, 'ui-icon-swatch');
+        // FIXME: set color in an adapter friendly way
+        button.find('.ui-icon').css('background-color', closest.css('color'));
+        return;
+    }
+};
 
 ColorPickerBasicMenu.prototype.changeColor = function(color) {
     this.raptor.actionApply(function() {
@@ -18,7 +61,7 @@ ColorPickerBasicMenu.prototype.changeColor = function(color) {
                 };
             });
         } else {
-            selectionToggleWrapper('color', {
+            selectionToggleWrapper('span', {
                 classes: this.options.classes || this.options.cssPrefix + 'color ' + this.options.cssPrefix + color
             });
         }
@@ -49,16 +92,26 @@ ColorPickerBasicMenu.prototype.click = function() {
     }.bind(this));
 };
 
-ColorPickerBasicMenu.prototype.getMenu = function() {
-    if (!this.menu) {
-        this.menuContent = this.editor.getTemplate('color-picker-basic.menu', this.options);
-        var menu = Menu.prototype.getMenu.call(this)
-            .find('[data-color]')
-            .click(this.click.bind(this))
-            .mouseenter(this.mouseEnter.bind(this))
-            .mouseleave(this.mouseLeave.bind(this));
+ColorPickerBasicMenu.prototype.getButton = function() {
+    if (!this.button) {
+        this.button = new Button({
+            name: this.name,
+            action: this.show.bind(this),
+            preview: false,
+            options: this.options,
+            text: true,
+            icon: false,
+            label: _('colorPickerBasicAutomatic')
+        });
     }
-    return this.menu;
-}
+    return this.button;
+};
+
+ColorPickerBasicMenu.prototype.getMenuItems = function() {
+    return $(this.editor.getTemplate('color-picker-basic.menu', this.options))
+        .click(this.click.bind(this))
+        .mouseenter(this.mouseEnter.bind(this))
+        .mouseleave(this.mouseLeave.bind(this));
+};
 
 Raptor.registerUi(new ColorPickerBasicMenu());
