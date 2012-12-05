6edcec3ba8df3818f9453a8d1636095494ab0a30
Melissa <melissa@panmedia.co.nz>
2012-12-05 16:48:18 +1300

    made updates to the german and spanish language files

--- src/locales/es.js
+++ src/locales/es.js
@@ -3,6 +3,145 @@
  * @author Francisco Martínez (JnxF), paco.7070@hotmail.com, https://twitter.com/ElJnxF
  */
 registerLocale('es', 'Español', {
+    
+    alignCenterTitle: 'Centrar',
+    alignJustifyTitle: 'Justificar',
+    alignLeftTitle: 'Alinear a la Izquierda',
+    alignRightTitle: 'Alinear a la Derecha',
+    
+    cancelDialogCancelButton: 'Cerrar',
+    cancelDialogContent: '¿Está seguro de que desea detener la edición? ¡Todos los cambios serán perdidos!',
+    cancelDialogOKButton: 'Aceptar',
+    cancelDialogTitle: 'Cancel Editing',
+    cancelTitle: 'Cancel editing',
+    
+    clearFormattingTitle: 'Limpiar Formato',
+    clickButtonToEditPluginButton: "Haga clic para empezar a editar",
+
+    colorPickerBasicAutomatic: 'Automatic',
+    colorPickerBasicBlack: 'Black',
+    colorPickerBasicBlue: 'Blue',
+    colorPickerBasicGreen: 'Green',
+    colorPickerBasicGrey: 'Grey',
+    colorPickerBasicOrange: 'Orange',
+    colorPickerBasicPurple: 'Purple',
+    colorPickerBasicRed: 'Red',
+    colorPickerBasicTitle: 'Cambiar el color del texto seleccionado.',
+    colorPickerBasicWhite: 'White',
+    
+    dockToElementTitle: 'Dock editor to element',
+    dockToScreenTitle: 'Dock editor to screen',
+
+    embedTitle: 'Objeto incrustado',
+
+    errorPluginNoName: 'Plugin "{{plugin}}" is invalid (must have a name property)',
+    errorPluginNotObject: 'Plugin "{{plugin}}" is invalid (must be an object)',
+    errorPluginOverride: 'Plugin "{{name}}" has already been registered, and will be overwritten',
+    
+    errorUINoName: 'UI "{{ui}}" is invalid (must have a name property)',
+    errorUINotObject: 'UI "{{ui}}" is invalid (must be an object)',
+    errorUIOverride: 'UI "{{name}}" has already been registered, and will be overwritten',
+
+    floatLeftTitle: 'Flotar Imagen a la Izquierda',
+    floatNoneTitle: 'Remove image align',
+    floatRightTitle: 'Flotar Imagen a la Derecha',
+
+    guidesTitle: 'Mostrar Guías',
+
+    historyRedoTitle: 'Redo',
+    historyUndoTitle: 'Undo',
+
+    listOrderedTitle: 'Lista Ordenada',
+    listUnorderedTitle: 'Lista Desordenada',
+
+    linkDialogTitle: 'Insertar Enlace',
+    linkDialogOKButton: 'Insertar Enlace',
+    linkDialogCancelButton: 'Cancelar',
+    linkDialogMenuHeader: 'Escoja un tipo de enlace:',
+    
+    linkTypeEmailLabel: 'Dirección de correo electrónico',
+    linkTypeEmailHeader: 'Enlazar a una dirección de correo electrónico',
+    linkTypeEmailToLabel: 'Correo electrónico:',
+    linkTypeEmailToPlaceHolder: 'Introduzca una dirección de correo electrónico',
+    linkTypeEmailSubjectLabel: 'Sujeto (opcional):',
+    linkTypeEmailSubjectPlaceHolder: 'Introduzca un sujeto',
+    
+    linkTypeExternalLabel: 'Page on another website',
+    linkTypeExternalHeader: 'Link to a page on another website',
+    linkTypeExternalLocationLabel: 'Localización',
+    linkTypeExternalNewWindowHeader: 'Nueva ventana',
+    linkTypeExternalNewWindowLabel: 'Marque esta casilla para que el enlace se abra en una nueva ventana.',
+    linkTypeExternalInfo:
+        '<h2>¿No está seguro de qué poner en la caja anterior?</h2>' +
+        '<ol>' +
+        '    <li>Busque la página web a la que desee enlazar</li>' +
+        '    <li>Copie la dirección web desde la barra de dirección de su navegador y péguela en la caja superior.</li>' +
+        '</ol>',
+
+    linkTypeInternalLabel: 'Page on this website',
+    linkTypeInternalHeader: 'Link to a page on this website',
+    linkTypeInternalLocationLabel: 'Localización',
+    linkTypeInternalNewWindowHeader: 'Nueva ventana',
+    linkTypeInternalNewWindowLabel: 'Marque esta casilla para que el enlace se abra en una nueva ventana.',
+    linkTypeInternalInfo:
+        '<h2>¿No está seguro de qué poner en la caja anterior?</h2>' +
+        '<ol>' +
+        '    <li>Find the page on this site link to.</li>' +
+        '    <li>Copy the web address from your browser\'s address bar, excluding "{{domain}}" and paste it into the box above.</li>' +
+        '</ol>',
+    
+    logoTitle: 'Saber más sobre el editor WYSIWYG Raptor',
+    saveTitle: 'Guardar',
+
+    statisticsButtonCharacterOverLimit: '{{charactersRemaining}} carácter(es) sobre el límite',
+    statisticsButtonCharacterRemaining: 'Queda(n) {{charactersRemaining}} carácter(es)',
+    statisticsButtonCharacters: '{{characters}} carácteres',
+    statisticsDialogCharactersOverLimit: '{{characters}} carácter(es), {{charactersRemaining}} sobre el límite recomendado',
+    statisticsDialogCharactersRemaining: '{{characters}} carácter(es), queda(n) {{charactersRemaining}}',
+    statisticsDialogNotTruncated: 'El contenido no será truncado',
+    statisticsDialogOKButton: 'Aceptar',
+    statisticsDialogSentence: '{{sentences}} sentence',
+    statisticsDialogSentences: '{{sentences}} oraciones',
+    statisticsDialogTitle: 'Contenidos Estadísticos',
+    statisticsDialogTruncated: 'El contenido contiene más de {{limit}} carácteres y debe ser truncado',
+    statisticsDialogWord: '{{words}} palabra',
+    statisticsDialogWords: '{{words}} palabras',
+    statisticsTitle: 'Haga clic para ver las estadísticas',
+    
+    tableCreateTitle: 'Create table',
+    tableDeleteColumnTitle: 'Delete table column',
+    tableDeleteRowTitle: 'Delete table row',
+    tableInsertColumnTitle: 'Insert table column',
+    tableInsertRowTitle: 'Insert table row',
+
+    tagMenuTagH1: 'Encabezado&nbsp;1',
+    tagMenuTagH2: 'Encabezado&nbsp;2',
+    tagMenuTagH3: 'Encabezado&nbsp;3',
+    tagMenuTagNA: false,
+    tagMenuTagP: 'Párrafo',
+    tagMenuTitle: 'Change element style',
+    
+    tagTreeElementLink: 'Seleccionar el elemento {{element}}',
+    tagTreeElementTitle: 'Haga clic para selecionar el contenido del elemento "{{element}}"',
+    tagTreeRoot: 'orígen',
+    tagTreeRootLink: 'Seleccionar todo el contenido editable',
+    tagTreeRootTitle: 'Haga clic para seleccionar todo el contenido editable',
+
+    textBlockQuoteTitle: 'Cita en bloque',
+    textBoldTitle: 'Negrita',
+    textItalicTitle: 'Cursiva',
+    textStrikeTitle: 'Tachado',
+    textSubTitle: 'Subíndice',
+    textSuperTitle: 'Superíndice',
+    textUnderlineTitle: 'Subrayar',
+    
+    unsavedEditWarningText: 'Hay cambios sin guardar en esta página.',
+
+    viewSourceDialogCancelButton: 'Cerrar',
+    viewSourceDialogOKButton: 'Aplicar Fuente',
+    viewSourceDialogTitle: 'Content source code',
+    viewSourceTitle: 'Ver / Editar Código Fuente',
+    
     "A preview of your embedded object is displayed below.": "A continuación se muestra una vista previa de su objeto incrustado.",
     "Added link: {{link}}": "Enlace añadido: {{link}}",
     "All changes will be lost!": "¡Todos los cambios serán perdidos!",
