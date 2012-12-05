215cfd4e489823f4b36356abb04620252463a147
Melissa <melissa@panmedia.co.nz>
2012-12-05 16:50:08 +1300

    created tests for the image float (left, right and none) options and a test to check whether the buttons were disabled when an image has not been selected

---
+++ tests/cases/image-float/button-disable.php
@@ -0,0 +1,114 @@
+<!doctype html>
+<html>
+<head>
+    <script type="text/javascript" src="../../js/case.js"></script>
+    <?php $uri = '../../../src/'; include '../../../src/include.php'; ?>
+</head>
+<body class="simple">
+    <script type="text/javascript">
+        rangy.init();
+    </script>
+    <div class="test-1">
+        <h1>Left Float Button 1: Disabled When Image Not Selected</h1>
+        <div class="test-input">
+            <div class="editible">
+                <img src="../../images/raptor.png" alt="raptor logo" height="50" width="40" />
+                <p>
+                    {Some text here}
+                </p>
+                <div style="clear: both">
+                </div>
+            </div>
+        </div>
+        <div class="test-expected">
+            <div class="editible">
+                <img src="../../images/raptor.png" alt="raptor logo" height="50" width="40" />
+                <p>
+                    Some text here
+                </p>
+                <div style="clear: both">
+                </div>
+            </div>
+        </div>
+    </div>
+    <script type="text/javascript">
+        testEditor('.test-1', function(input) {
+            var floatLeftButton = input.find('.editible').data('editor').getLayout().getElement().find('.raptor-ui-float-left');
+            floatLeftButton.trigger('click');
+            
+            if (!floatLeftButton.is('.ui-button-disabled')) {
+                throw new Error('Button is not disabled');
+            } 
+            
+        });
+    </script>
+    
+    <div class="test-2">
+        <h1>Right Float Button 1: Disabled When Image Not Selected </h1>
+        <div class="test-input">
+            <div class="editible">
+                <img src="../../images/raptor.png" alt="raptor logo" height="50" width="40" />
+                <p>
+                    {Some text here}
+                </p>
+                <div style="clear: both">
+                </div>
+            </div>
+        </div>
+        <div class="test-expected">
+            <div class="editible">
+                <img src="../../images/raptor.png" alt="raptor logo" height="50" width="40" />
+                <p>
+                    Some text here
+                </p>
+                <div style="clear: both">
+                </div>
+            </div>
+        </div>
+    </div>
+    <script type="text/javascript">
+        testEditor('.test-2', function(input) {
+            var floatRightButton = input.find('.editible').data('editor').getLayout().getElement().find('.raptor-ui-float-right');
+            floatRightButton.trigger('click');
+            
+            if (!floatRightButton.is('.ui-button-disabled')) {
+                throw new Error('Button is not disabled');
+            } 
+        });
+    </script>
+    
+     <div class="test-3">
+        <h1>No Float Button 1: Disabled When Image Not Selected </h1>
+        <div class="test-input">
+            <div class="editible">
+                <img src="../../images/raptor.png" alt="raptor logo" height="50" width="40" />
+                <p>
+                    {Some text here}
+                </p>
+                <div style="clear: both">
+                </div>
+            </div>
+        </div>
+        <div class="test-expected">
+            <div class="editible">
+                <img src="../../images/raptor.png" alt="raptor logo" height="50" width="40" />
+                <p>
+                    Some text here
+                </p>
+                <div style="clear: both">
+                </div>
+            </div>
+        </div>
+    </div>
+    <script type="text/javascript">
+        testEditor('.test-3', function(input) {
+            var floatNoneButton = input.find('.editible').data('editor').getLayout().getElement().find('.raptor-ui-float-none');
+            floatNoneButton.trigger('click');
+            
+            if (!floatNoneButton.is('.ui-button-disabled')) {
+                throw new Error('Button is not disabled');
+            }
+        });
+    </script>
+</body>
+</html>
---
+++ tests/cases/image-float/float-right.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html>
+<head>
+    <script type="text/javascript" src="../../js/case.js"></script>
+    <?php $uri = '../../../src/'; include '../../../src/include.php'; ?>
+</head>
+<body class="simple">
+    <script type="text/javascript">
+        rangy.init();
+    </script>
+    <div class="test-1">
+        <h1>Right Float Button 1: Float Image - No Text</h1>
+        <div class="test-input">
+            <div class="editible">
+                {<img src="../../images/raptor.png" alt="raptor logo" height="50" width="40" />} 
+                    <div style="clear: both">
+                    </div>
+            </div>
+        </div>
+        <div class="test-expected">
+            <div class="editible">
+                <img src="../../images/raptor.png" alt="raptor logo" height="50" width="40" class="cms-float-right"  />
+                    <div style="clear: both">
+                    </div>
+            </div>
+        </div>
+    </div>
+    <script type="text/javascript">
+        testEditor('.test-1', function(input) {
+            input.find('.editible').data('editor').getLayout().getElement().find('.raptor-ui-float-right').trigger('click');
+        });
+    </script>
+    
+    <div class="test-2">
+        <h1>Right Float Button 2: Float Image With Text </h1>
+        <div class="test-input">
+            <div class="editible">
+                {<img src="../../images/raptor.png" alt="raptor logo" height="50" width="40"/>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+                    convallis dui id erat pellentesque et rhoncus nunc semper. Suspendisse
+                    malesuada hendrerit velit nec tristique.}
+                </p>
+                <div style="clear: both">
+                    </div>
+            </div>
+        </div>
+        <div class="test-expected">
+            <div class="editible">
+                <img src="../../images/raptor.png" alt="raptor logo" height="50" width="40" class="cms-float-right"  />
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+                    convallis dui id erat pellentesque et rhoncus nunc semper. Suspendisse
+                    malesuada hendrerit velit nec tristique.
+                </p>
+                <div style="clear: both">
+                </div>
+            </div>
+        </div>
+    </div>
+    <script type="text/javascript">
+        testEditor('.test-2', function(input) {
+            input.find('.editible').data('editor').getLayout().getElement().find('.raptor-ui-float-right').trigger('click');
+        });
+    </script>
+    
+    
+</body>
+</html>
