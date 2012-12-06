2122c6e9e9f0500748633d34242c02b96c4191e9
David Neilsen <david@panmedia.co.nz>
2012-12-06 17:07:17 +1300

    Update save UI

--- src/plugins/save/save.js
+++ src/plugins/save/save.js
@@ -1,5 +1,34 @@
 Raptor.registerUi(new Button({
     name: 'save',
+
     action: function() {
+        this.getPlugin().save();
+    },
+
+    init: function() {
+        var result = Button.prototype.init.apply(this, arguments);
+
+        // <strict>
+        if (!this.getPlugin()) {
+            handleError('Cannot find save plugin for UI.');
+        }
+        // </strict>
+        
+        this.raptor.bind('dirty', this.dirty.bind(this))
+        this.raptor.bind('cleaned', this.clean.bind(this))
+        this.clean();
+        return result;
+    },
+
+    getPlugin: function() {
+        return this.raptor.getPlugin(this.options.plugin);
+    },
+
+    dirty: function() {
+        aButtonEnable(this.button);
+    },
+
+    clean: function() {
+        aButtonDisable(this.button);
     }
 }));
