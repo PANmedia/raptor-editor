43538af57d0520ed5c48b310fa52b62970127aa9
David Neilsen <david@panmedia.co.nz>
2012-12-07 16:56:07 +1300

    Update tests

--- tests/cases/image-float/float-click-image-enables.php
+++ tests/cases/image-float/float-click-image-enables.php
@@ -34,11 +34,11 @@
     <script type="text/javascript">
         testEditor('.test-1', function(input) {
             input.find('img').trigger('click');
-            input.find('.editible').data('editor').getLayout().getElement().find('.raptor-ui-float-left').trigger('click');
+            input.find('.editible').data('raptor').getLayout().getElement().find('.raptor-ui-float-left').trigger('click');
             rangesToTokens(rangy.getSelection().getAllRanges());
         });
     </script>
-    
+
     <div class="test-2">
         <h1>Right Float Button 1: Enabled When Image Clicked On </h1>
         <div class="test-input">
@@ -65,11 +65,11 @@
     <script type="text/javascript">
         testEditor('.test-2', function(input) {
             input.find('img').trigger('click');
-            input.find('.editible').data('editor').getLayout().getElement().find('.raptor-ui-float-right').trigger('click');
+            input.find('.editible').data('raptor').getLayout().getElement().find('.raptor-ui-float-right').trigger('click');
             rangesToTokens(rangy.getSelection().getAllRanges());
         });
     </script>
-    
+
      <div class="test-3">
         <h1>No Float Button 1: Enabled When Image Clicked On </h1>
         <div class="test-input">
@@ -96,7 +96,7 @@
     <script type="text/javascript">
         testEditor('.test-3', function(input) {
             input.find('img').trigger('click');
-            input.find('.editible').data('editor').getLayout().getElement().find('.raptor-ui-float-none').trigger('click');
+            input.find('.editible').data('raptor').getLayout().getElement().find('.raptor-ui-float-none').trigger('click');
             rangesToTokens(rangy.getSelection().getAllRanges());
         });
     </script>
