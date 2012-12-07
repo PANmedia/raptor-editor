d71c8d00703c6375c6a0893a31cbe96b0615f2d6
David Neilsen <david@panmedia.co.nz>
2012-12-07 14:58:13 +1300

    Update scripts

--- scripts/expose.php
+++ scripts/expose.php
@@ -1,6 +1,8 @@
 <?php
+require_once __DIR__ . '/glob.php';
+
 $result = [];
-foreach (glob(__DIR__ . '/src/tools/*.js') as $file) {
+foreach (glob_recursive(__DIR__ . '/../src/tools/*.js') as $file) {
     $contents = file_get_contents($file);
     $matches = null;
     preg_match_all('/^function (.+?)\(/m', $contents, $matches);
@@ -9,12 +11,23 @@ foreach (glob(__DIR__ . '/src/tools/*.js') as $file) {
     }
 }
 sort($result);
+
+foreach (glob_recursive(__DIR__ . '/../src/components/*.js') as $file) {
+    $contents = file_get_contents($file);
+    $matches = null;
+    preg_match_all('/^function (.+?)\(/m', $contents, $matches);
+    foreach ($matches[1] as $function) {
+        $result[] = "    $function: $function";
+    }
+}
+sort($result);
+
 $result = implode(',' . PHP_EOL, $result);
 echo "
 $.extend($.ui.raptor, Raptor, {
 
     // <expose>
-$result    
+$result
     // </expose>
 
 });
