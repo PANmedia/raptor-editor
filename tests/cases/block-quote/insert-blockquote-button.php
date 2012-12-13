c1317f5bbe08496a83f11e7a2ba9361610ba49fc
Melissa <melissa@panmedia.co.nz>
2012-12-13 16:27:26 +1300

    updated the horizontal rule button test and created a test for creating and removing a block quote

---
+++ tests/cases/block-quote/insert-blockquote-button.php
@@ -0,0 +1,289 @@
+<!doctype html>
+<html>
+<head>
+    <script type="text/javascript" src="../../js/case.js"></script>
+    <?php $uri = '../../../src/'; include '../../../src/include.php'; ?>
+</head>
+<body class="simple">
+    <script type="text/javascript">
+        rangy.init();
+    </script>
+    <div class="test-1">
+        <h1>Block Quote Button 1: Word Group Selection</h1>
+        <div class="test-input">
+            <div class="editible">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+                    convallis {dui id erat pellentesque et rhoncus} nunc semper. Suspendisse
+                    malesuada hendrerit velit nec tristique. Aliquam gravida mauris at
+                    ligula venenatis rhoncus. Suspendisse interdum, nisi nec consectetur
+                    pulvinar, lorem augue ornare felis, vel lacinia erat nibh in velit.
+                </p>
+            </div>
+        </div>
+        <div class="test-expected">
+            <div class="editible">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+                    convallis <blockquote>{dui id erat pellentesque et rhoncus}</blockquote> nunc semper. Suspendisse
+                    malesuada hendrerit velit nec tristique. Aliquam gravida mauris at
+                    ligula venenatis rhoncus. Suspendisse interdum, nisi nec consectetur
+                    pulvinar, lorem augue ornare felis, vel lacinia erat nibh in velit.
+                </p>
+            </div>
+        </div>
+    </div>
+    <script type="text/javascript">
+        testEditor('.test-1', function(input) {
+            var blockquoteButton = input.find('.editible').data('raptor').getLayout().getElement().find('.raptor-ui-text-block-quote');
+            blockquoteButton.trigger('click');
+            rangesToTokens(rangy.getSelection().getAllRanges());
+            
+            if (!blockquoteButton.is('.ui-state-active')){
+                throw new Error('Button is not active');
+            }
+        });
+    </script>
+
+    <div class="test-2">
+        <h1>Block Quote Button 2: Single Word Selection</h1>
+        <div class="test-input">
+            <div class="editible">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur {adipiscing} elit. Maecenas
+                    convallis dui id erat pellentesque et rhoncus nunc semper. Suspendisse
+                    malesuada hendrerit velit nec tristique. Aliquam gravida mauris at
+                    ligula venenatis rhoncus. Suspendisse interdum, nisi nec consectetur
+                    pulvinar, lorem augue ornare felis, vel lacinia erat nibh in velit.
+                </p>
+            </div>
+        </div>
+        <div class="test-expected">
+            <div class="editible">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur <blockquote>{adipiscing}</blockquote> elit. Maecenas
+                    convallis dui id erat pellentesque et rhoncus nunc semper. Suspendisse
+                    malesuada hendrerit velit nec tristique. Aliquam gravida mauris at
+                    ligula venenatis rhoncus. Suspendisse interdum, nisi nec consectetur
+                    pulvinar, lorem augue ornare felis, vel lacinia erat nibh in velit.
+                </p>
+            </div>
+        </div>
+    </div>
+    <script type="text/javascript">
+        testEditor('.test-2', function(input) {
+            var blockquoteButton = input.find('.editible').data('raptor').getLayout().getElement().find('.raptor-ui-text-block-quote');
+            blockquoteButton.trigger('click');
+            rangesToTokens(rangy.getSelection().getAllRanges());
+            
+            if (!blockquoteButton.is('.ui-state-active')){
+                throw new Error('Button is not active');
+            }
+        });
+    </script>
+
+    <div class="test-3">
+        <h1>Block Quote Button 3: Part Word Selection</h1>
+        <div class="test-input">
+            <div class="editible">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+                    convallis dui id erat pel{lentesqu}e et rhoncus nunc semper. Suspendisse
+                    malesuada hendrerit velit nec tristique. Aliquam gravida mauris at
+                    ligula venenatis rhoncus. Suspendisse interdum, nisi nec consectetur
+                    pulvinar, lorem augue ornare felis, vel lacinia erat nibh in velit.
+                </p>
+            </div>
+        </div>
+        <div class="test-expected">
+            <div class="editible">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+                    convallis dui id erat pel<blockquote>{lentesqu}</blockquote>e et rhoncus nunc semper. Suspendisse
+                    malesuada hendrerit velit nec tristique. Aliquam gravida mauris at
+                    ligula venenatis rhoncus. Suspendisse interdum, nisi nec consectetur
+                    pulvinar, lorem augue ornare felis, vel lacinia erat nibh in velit.
+                </p>
+            </div>
+        </div>
+    </div>
+    <script type="text/javascript">
+        testEditor('.test-3', function(input) {
+            var blockquoteButton = input.find('.editible').data('raptor').getLayout().getElement().find('.raptor-ui-text-block-quote');
+            blockquoteButton.trigger('click');
+            rangesToTokens(rangy.getSelection().getAllRanges());
+            
+            if (!blockquoteButton.is('.ui-state-active')){
+                throw new Error('Button is not active');
+            }
+        });
+    </script>
+
+     <div class="test-4">
+        <h1>Block Quote Button 4: Multi-Paragraph Selection</h1>
+        <div class="test-input">
+            <div class="editible">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+                    convallis dui {id erat pellentesque et rhoncus nunc semper. Suspendisse
+                    malesuada hendrerit velit nec tristique.
+                </p>
+                <p>
+                    Aliquam gravida mauris at
+                    ligula venenatis rhoncus. Suspendisse} interdum, nisi nec consectetur
+                    pulvinar, lorem augue ornare felis, vel lacinia erat nibh in velit.
+                </p>
+            </div>
+        </div>
+        <div class="test-expected">
+            <div class="editible">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+                    convallis dui <blockquote>{id erat pellentesque et rhoncus nunc semper. Suspendisse
+                    malesuada hendrerit velit nec tristique.
+                </blockquote></p>
+                <p><blockquote>
+                    Aliquam gravida mauris at
+                    ligula venenatis rhoncus. Suspendisse}</blockquote> interdum, nisi nec consectetur
+                    pulvinar, lorem augue ornare felis, vel lacinia erat nibh in velit.
+                </p>
+            </div>
+        </div>
+    </div>
+    <script type="text/javascript">
+        testEditor('.test-4', function(input) {
+            var blockquoteButton = input.find('.editible').data('raptor').getLayout().getElement().find('.raptor-ui-text-block-quote');
+            blockquoteButton.trigger('click');
+            rangesToTokens(rangy.getSelection().getAllRanges());
+            
+            if (!blockquoteButton.is('.ui-state-active')){
+                throw new Error('Button is not active');
+            }
+        });
+    </script>
+
+    <div class="test-5">
+        <h1>Block Quote Button 5: Paragraph Selection</h1>
+        <div class="test-input">
+            <div class="editible">
+                <p>
+                    {Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+                    convallis dui id erat pellentesque et rhoncus nunc semper. Suspendisse
+                    malesuada hendrerit velit nec tristique.
+                </p><p>
+                    Aliquam gravida mauris at
+                    ligula venenatis rhoncus. Suspendisse interdum, nisi nec consectetur
+                    pulvinar, lorem augue ornare felis, vel lacinia erat nibh in velit.}
+                </p>
+            </div>
+        </div>
+        <div class="test-expected">
+            <div class="editible">
+                <p>
+                    <blockquote>{Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+                    convallis dui id erat pellentesque et rhoncus nunc semper. Suspendisse
+                    malesuada hendrerit velit nec tristique.
+                </blockquote></p>
+                <p><blockquote>
+                    Aliquam gravida mauris at
+                    ligula venenatis rhoncus. Suspendisse interdum, nisi nec consectetur
+                    pulvinar, lorem augue ornare felis, vel lacinia erat nibh in velit.}</blockquote>
+                </p>
+            </div>
+        </div>
+    </div>
+    <script type="text/javascript">
+        testEditor('.test-5', function(input) {
+            var blockquoteButton = input.find('.editible').data('raptor').getLayout().getElement().find('.raptor-ui-text-block-quote');
+            blockquoteButton.trigger('click');
+            rangesToTokens(rangy.getSelection().getAllRanges());
+            
+            if (!blockquoteButton.is('.ui-state-active')){
+                throw new Error('Button is not active');
+            }
+        });
+    </script>
+
+    <div class="test-6">
+        <h1>Block Quote Button 6: Empty Selection in Word</h1>
+        <div class="test-input">
+            <div class="editible">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+                    convallis dui id erat pellentesque et rhoncus nunc semper. Suspendisse
+                    malesuada hendrerit velit nec tristique.
+                </p><p>
+                    Aliquam gravida mauris at
+                    ligula venenatis rhoncus. Suspen{}disse interdum, nisi nec consectetur
+                    pulvinar, lorem augue ornare felis, vel lacinia erat nibh in velit.
+                </p>
+            </div>
+        </div>
+        <div class="test-expected">
+            <div class="editible">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+                    convallis dui id erat pellentesque et rhoncus nunc semper. Suspendisse
+                    malesuada hendrerit velit nec tristique.
+                </p><p><blockquote>
+                    Aliquam gravida mauris at
+                    ligula venenatis rhoncus. Suspen{}disse interdum, nisi nec consectetur
+                    pulvinar, lorem augue ornare felis, vel lacinia erat nibh in velit.</blockquote>
+                </p>
+            </div>
+        </div>
+    </div>
+    <script type="text/javascript">
+        testEditor('.test-6', function(input) {
+            var blockquoteButton = input.find('.editible').data('raptor').getLayout().getElement().find('.raptor-ui-text-block-quote');
+            blockquoteButton.trigger('click');
+            rangesToTokens(rangy.getSelection().getAllRanges());
+            
+            if (!blockquoteButton.is('.ui-state-active')){
+                throw new Error('Button is not active');
+            }
+        });
+    </script>
+
+    <div class="test-7">
+        <h1>Block Quote Button 7: Empty Selection at the Beginning of a Word</h1>
+        <div class="test-input">
+            <div class="editible">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+                    convallis dui id erat pellentesque et rhoncus nunc semper. Suspendisse
+                    malesuada {}hendrerit velit nec tristique.
+                </p><p>
+                    Aliquam gravida mauris at
+                    ligula venenatis rhoncus. Suspendisse interdum, nisi nec consectetur
+                    pulvinar, lorem augue ornare felis, vel lacinia erat nibh in velit.
+                </p>
+            </div>
+        </div>
+        <div class="test-expected">
+            <div class="editible">
+                <p>
+                    <blockquote>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+                    convallis dui id erat pellentesque et rhoncus nunc semper. Suspendisse
+                    malesuada {}hendrerit velit nec tristique.</blockquote>
+                </p><p>
+                    Aliquam gravida mauris at
+                    ligula venenatis rhoncus. Suspendisse interdum, nisi nec consectetur
+                    pulvinar, lorem augue ornare felis, vel lacinia erat nibh in velit.
+                </p>
+            </div>
+        </div>
+    </div>
+    <script type="text/javascript">
+        testEditor('.test-7', function(input) {
+            var blockquoteButton = input.find('.editible').data('raptor').getLayout().getElement().find('.raptor-ui-text-block-quote');
+            blockquoteButton.trigger('click');
+            rangesToTokens(rangy.getSelection().getAllRanges());
+            
+            if (!blockquoteButton.is('.ui-state-active')){
+                throw new Error('Button is not active');
+            }
+        });
+    </script>
+</body>
+</html>
