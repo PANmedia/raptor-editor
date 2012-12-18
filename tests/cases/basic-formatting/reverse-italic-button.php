6d3e705cec9366da9c4e5cb7b72e714a9566bec8
Melissa <melissa@panmedia.co.nz>
2012-12-18 16:01:20 +1300

    Created tests to check the subscript button is working properly and updated the reverse basic formatting tests to expect the right selection

--- tests/cases/basic-formatting/reverse-italic-button.php
+++ tests/cases/basic-formatting/reverse-italic-button.php
@@ -231,7 +231,7 @@
                     malesuada hendrerit velit nec tristique.
                 </p><p>
                     Aliquam gravida mauris at
-                    ligula venenatis rhoncus. {Suspendisse} interdum, nisi nec consectetur
+                    ligula venenatis rhoncus. Suspen{}disse interdum, nisi nec consectetur
                     pulvinar, lorem augue ornare felis, vel lacinia erat nibh in velit.
                 </p>
             </div>
@@ -269,7 +269,7 @@
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
                     convallis dui id erat pellentesque et rhoncus nunc semper. Suspendisse
-                    malesuada {hendrerit} velit nec tristique.
+                    malesuada {}hendrerit velit nec tristique.
                 </p><p>
                     Aliquam gravida mauris at
                     ligula venenatis rhoncus. Suspendisse interdum, nisi nec consectetur
--- tests/cases/subscript/reverse-subscript-button.php
+++ tests/cases/subscript/reverse-subscript-button.php
@@ -9,12 +9,12 @@
         rangy.init();
     </script>
     <div class="test-1">
-        <h1>Reverse Strike Button 1: Word Group Selection</h1>
+        <h1>Reverse Subscript Button 1: Word Group Selection</h1>
         <div class="test-input">
             <div class="editible">
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-                    convallis <del class="cms-strike">{dui id erat pellentesque et rhoncus}</del> nunc semper. Suspendisse
+                    convallis <sub class="cms-sub">{dui id erat pellentesque et rhoncus}</sub> nunc semper. Suspendisse
                     malesuada hendrerit velit nec tristique. Aliquam gravida mauris at
                     ligula venenatis rhoncus. Suspendisse interdum, nisi nec consectetur
                     pulvinar, lorem augue ornare felis, vel lacinia erat nibh in velit.
@@ -35,26 +35,27 @@
     </div>
     <script type="text/javascript">
         testEditor('.test-1', function(input) {
-            var strikeButton = input.find('.editible').data('raptor').getLayout().getElement().find('.raptor-ui-text-strike');
-            strikeButton.trigger('click');
+            var subButton = input.find('.editible').data('raptor').getLayout().getElement().find('.raptor-ui-text-sub');
+            subButton.trigger('click');
             rangesToTokens(rangy.getSelection().getAllRanges());
             
-            if (strikeButton.is('.ui-state-highlight')){
+            if (subButton.is('.ui-state-highlight')){
                 throw new Error('Button is active');
             }
         });
     </script>
 
     <div class="test-2">
-        <h1>Reverse Strike Button 2: Single Word Selection</h1>
+        <h1>Reverse Subscript Button 2: Single Word Selection</h1>
         <div class="test-input">
             <div class="editible">
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur <del class="cms-strike">{adipiscing}</del> elit. Maecenas
+                    Lorem ipsum dolor sit amet, consectetur <sub class="cms-sub">{adipiscing}</sub> elit. Maecenas
                     convallis dui id erat pellentesque et rhoncus nunc semper. Suspendisse
                     malesuada hendrerit velit nec tristique. Aliquam gravida mauris at
                     ligula venenatis rhoncus. Suspendisse interdum, nisi nec consectetur
                     pulvinar, lorem augue ornare felis, vel lacinia erat nibh in velit.
+                    
                 </p>
             </div>
         </div>
@@ -66,30 +67,29 @@
                     malesuada hendrerit velit nec tristique. Aliquam gravida mauris at
                     ligula venenatis rhoncus. Suspendisse interdum, nisi nec consectetur
                     pulvinar, lorem augue ornare felis, vel lacinia erat nibh in velit.
-
                 </p>
             </div>
         </div>
     </div>
     <script type="text/javascript">
         testEditor('.test-2', function(input) {
-            var strikeButton = input.find('.editible').data('raptor').getLayout().getElement().find('.raptor-ui-text-strike');
-            strikeButton.trigger('click');
+            var subButton = input.find('.editible').data('raptor').getLayout().getElement().find('.raptor-ui-text-sub');
+            subButton.trigger('click');
             rangesToTokens(rangy.getSelection().getAllRanges());
             
-            if (strikeButton.is('.ui-state-highlight')){
+            if (subButton.is('.ui-state-highlight')){
                 throw new Error('Button is active');
             }
         });
     </script>
 
     <div class="test-3">
-        <h1>Reverse Strike Button 3: Part Word Selection</h1>
+        <h1>Reverse Subscript Button 3: Part Word Selection</h1>
         <div class="test-input">
             <div class="editible">
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-                    convallis dui id erat pel<del class="cms-strike">{lentesqu}</del>e et rhoncus nunc semper. Suspendisse
+                    convallis dui id erat pel<sub class="cms-sub">{lentesqu}</sub>e et rhoncus nunc semper. Suspendisse
                     malesuada hendrerit velit nec tristique. Aliquam gravida mauris at
                     ligula venenatis rhoncus. Suspendisse interdum, nisi nec consectetur
                     pulvinar, lorem augue ornare felis, vel lacinia erat nibh in velit.
@@ -104,37 +104,35 @@
                     malesuada hendrerit velit nec tristique. Aliquam gravida mauris at
                     ligula venenatis rhoncus. Suspendisse interdum, nisi nec consectetur
                     pulvinar, lorem augue ornare felis, vel lacinia erat nibh in velit.
-
                 </p>
             </div>
         </div>
     </div>
     <script type="text/javascript">
         testEditor('.test-3', function(input) {
-            var strikeButton = input.find('.editible').data('raptor').getLayout().getElement().find('.raptor-ui-text-strike');
-            strikeButton.trigger('click');
+            var subButton = input.find('.editible').data('raptor').getLayout().getElement().find('.raptor-ui-text-sub');
+            subButton.trigger('click');
             rangesToTokens(rangy.getSelection().getAllRanges());
             
-            if (strikeButton.is('.ui-state-highlight')){
+            if (subButton.is('.ui-state-highlight')){
                 throw new Error('Button is active');
             }
         });
     </script>
 
      <div class="test-4">
-        <h1>Reverse Strike Button 4: Multi-Paragraph Selection</h1>
+        <h1>Reverse Subscript Button 4: Multi-Paragraph Selection</h1>
         <div class="test-input">
             <div class="editible">
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-                    convallis dui <del class="cms-strike">{id erat pellentesque et rhoncus nunc semper. Suspendisse
+                    convallis dui <sub class="cms-sub">{id erat pellentesque et rhoncus nunc semper. Suspendisse
                     malesuada hendrerit velit nec tristique.
-                </del></p>
-                <p><del class="cms-strike">
+                </sub></p>
+                <p><sub class="cms-sub">
                     Aliquam gravida mauris at
-                    ligula venenatis rhoncus. Suspendisse}</del> interdum, nisi nec consectetur
+                    ligula venenatis rhoncus. Suspendisse}</sub> interdum, nisi nec consectetur
                     pulvinar, lorem augue ornare felis, vel lacinia erat nibh in velit.
-
                 </p>
             </div>
         </div>
@@ -144,7 +142,8 @@
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
                     convallis dui {id erat pellentesque et rhoncus nunc semper. Suspendisse
                     malesuada hendrerit velit nec tristique.
-                </p><p>
+                </p>
+                <p>
                     Aliquam gravida mauris at
                     ligula venenatis rhoncus. Suspendisse} interdum, nisi nec consectetur
                     pulvinar, lorem augue ornare felis, vel lacinia erat nibh in velit.
@@ -154,29 +153,29 @@
     </div>
     <script type="text/javascript">
         testEditor('.test-4', function(input) {
-            var strikeButton = input.find('.editible').data('raptor').getLayout().getElement().find('.raptor-ui-text-strike');
-            strikeButton.trigger('click');
+            var subButton = input.find('.editible').data('raptor').getLayout().getElement().find('.raptor-ui-text-sub');
+            subButton.trigger('click');
             rangesToTokens(rangy.getSelection().getAllRanges());
             
-            if (strikeButton.is('.ui-state-highlight')){
+            if (subButton.is('.ui-state-highlight')){
                 throw new Error('Button is active');
             }
         });
     </script>
 
     <div class="test-5">
-        <h1>Reverse Strike Button 5: Paragraph Selection</h1>
+        <h1>Reverse Subscript Button 5: Paragraph Selection</h1>
         <div class="test-input">
             <div class="editible">
                 <p>
-                    <del class="cms-strike">{Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+                    <sub class="cms-sub">{Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
                     convallis dui id erat pellentesque et rhoncus nunc semper. Suspendisse
                     malesuada hendrerit velit nec tristique.
-                </del></p>
-                <p><del class="cms-strike">
+                </sub></p>
+                <p><sub class="cms-sub">
                     Aliquam gravida mauris at
                     ligula venenatis rhoncus. Suspendisse interdum, nisi nec consectetur
-                    pulvinar, lorem augue ornare felis, vel lacinia erat nibh in velit.}</del>
+                    pulvinar, lorem augue ornare felis, vel lacinia erat nibh in velit.}</sub>
                 </p>
             </div>
         </div>
@@ -196,18 +195,18 @@
     </div>
     <script type="text/javascript">
         testEditor('.test-5', function(input) {
-            var strikeButton = input.find('.editible').data('raptor').getLayout().getElement().find('.raptor-ui-text-strike');
-            strikeButton.trigger('click');
+            var subButton = input.find('.editible').data('raptor').getLayout().getElement().find('.raptor-ui-text-sub');
+            subButton.trigger('click');
             rangesToTokens(rangy.getSelection().getAllRanges());
             
-            if (strikeButton.is('.ui-state-highlight')){
+            if (subButton.is('.ui-state-highlight')){
                 throw new Error('Button is active');
             }
         });
     </script>
 
     <div class="test-6">
-        <h1>Reverse Strike Button 6: Empty Selection in Word</h1>
+        <h1>Reverse Subscript Button 6: Empty Selection in Word</h1>
         <div class="test-input">
             <div class="editible">
                 <p>
@@ -216,20 +215,20 @@
                     malesuada hendrerit velit nec tristique.
                 </p><p>
                     Aliquam gravida mauris at
-                    ligula venenatis rhoncus. <del class="cms-strike">Suspen{}disse</del> interdum, nisi nec consectetur
+                    ligula venenatis rhoncus. <sub class="cms-sub">Suspe{}ndisse</sub> interdum, nisi nec consectetur
                     pulvinar, lorem augue ornare felis, vel lacinia erat nibh in velit.
                 </p>
             </div>
         </div>
         <div class="test-expected">
             <div class="editible">
-                 <p>
+                <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
                     convallis dui id erat pellentesque et rhoncus nunc semper. Suspendisse
                     malesuada hendrerit velit nec tristique.
                 </p><p>
                     Aliquam gravida mauris at
-                    ligula venenatis rhoncus. Suspen{}disse interdum, nisi nec consectetur
+                    ligula venenatis rhoncus. Suspe{}ndisse interdum, nisi nec consectetur
                     pulvinar, lorem augue ornare felis, vel lacinia erat nibh in velit.
                 </p>
             </div>
@@ -237,24 +236,24 @@
     </div>
     <script type="text/javascript">
         testEditor('.test-6', function(input) {
-            var strikeButton = input.find('.editible').data('raptor').getLayout().getElement().find('.raptor-ui-text-strike');
-            strikeButton.trigger('click');
+            var subButton = input.find('.editible').data('raptor').getLayout().getElement().find('.raptor-ui-text-sub');
+            subButton.trigger('click');
             rangesToTokens(rangy.getSelection().getAllRanges());
             
-            if (strikeButton.is('.ui-state-highlight')){
+            if (subButton.is('.ui-state-highlight')){
                 throw new Error('Button is active');
             }
         });
     </script>
 
     <div class="test-7">
-        <h1>Reverse Strike Button 7: Empty Selection at the Beginning of a Word</h1>
+        <h1>Reverse Subscript Button 7: Empty Selection at the Beginning of a Word</h1>
         <div class="test-input">
             <div class="editible">
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
                     convallis dui id erat pellentesque et rhoncus nunc semper. Suspendisse
-                    malesuada <del class="cms-strike">{}hendrerit</del> velit nec tristique.
+                    malesuada <sub class="cms-sub">{}hendrerit</sub> velit nec tristique.
                 </p><p>
                     Aliquam gravida mauris at
                     ligula venenatis rhoncus. Suspendisse interdum, nisi nec consectetur
@@ -278,14 +277,14 @@
     </div>
     <script type="text/javascript">
         testEditor('.test-7', function(input) {
-            var strikeButton = input.find('.editible').data('raptor').getLayout().getElement().find('.raptor-ui-text-strike');
-            strikeButton.trigger('click');
+            var subButton = input.find('.editible').data('raptor').getLayout().getElement().find('.raptor-ui-text-sub');
+            subButton.trigger('click');
             rangesToTokens(rangy.getSelection().getAllRanges());
             
-            if (strikeButton.is('.ui-state-highlight')){
+            if (subButton.is('.ui-state-highlight')){
                 throw new Error('Button is active');
             }
         });
     </script>
 </body>
-</html>
+</html
