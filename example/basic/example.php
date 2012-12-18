3aa74606124c4c50fd4f699c0e36654aba1953d2
David Neilsen <david@panmedia.co.nz>
2012-12-19 09:34:49 +1300

    Update example

--- example/basic/example.php
+++ example/basic/example.php
@@ -16,7 +16,7 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <title>Raptor Editor - Save Rest Example</title>
+    <title>Raptor Editor - Basic Example</title>
     <link rel="stylesheet" href="../assets/style.css" />
     <?php if ($type === 'light'): ?>
         <link rel="stylesheet" href="../../src/dependencies/themes/aristo/jquery-ui.css" />
@@ -91,7 +91,7 @@
     </nav>
     <header class="editable" data-id="header">
         <?php ob_start(); ?>
-        <h1>Raptor Editor - Save Rest Example</h1>
+        <h1>Raptor Editor - Basic Example</h1>
         <?php
             $buffer = ob_get_clean();
             if (isset($content['header'])) {
@@ -107,9 +107,16 @@
         <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
             has been the industry's standard dummy text ever since the 1500s, when an unknown printer
-            took a galley of type and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting, remaining essentially
-            unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+            took a galley of type and scrambled it to make a type specimen book.
+        </p>
+        <blockquote>
+            <p>
+                It has survived not only five centuries, but also the leap into electronic typesetting,
+                remaining essentially unchanged.
+            </p>
+        </blockquote>
+        <p>
+            It was popularised in the 1960s with the release of Letraset sheets containing
             Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker
             including versions of Lorem Ipsum.
         </p>
@@ -119,7 +126,7 @@
             <span class="cms-green">This text is green.</span>
             <a href=".">This is a link.</a>
             <strong class="cms-bold">This text is bold.</strong>
-            <i class="cms-italic">This text is italic.</i>
+            <em class="cms-italic">This text is italic.</em>
         </p>
         <?php
             $buffer = ob_get_clean();
