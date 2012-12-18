3b94a8679e61c2e8e658b9631bb77e47abb963b9
David Neilsen <david@panmedia.co.nz>
2012-12-19 09:35:33 +1300

    Make CSSClassApplierButton a toggle button

--- src/components/ui/css-class-applier-button.js
+++ src/components/ui/css-class-applier-button.js
@@ -1,8 +1,8 @@
 function CSSClassApplierButton(options) {
-    PreviewButton.call(this, options);
+    PreviewToggleButton.call(this, options);
 }
 
-CSSClassApplierButton.prototype = Object.create(PreviewButton.prototype);
+CSSClassApplierButton.prototype = Object.create(PreviewToggleButton.prototype);
 
 CSSClassApplierButton.prototype.action = function() {
     // Test for 0 length selection
@@ -19,4 +19,17 @@ CSSClassApplierButton.prototype.action = function() {
         });
         applier.toggleSelection();
     }
+    this.selectionChange();
+};
+
+CSSClassApplierButton.prototype.selectionToggle = function() {
+    for (var i = 0, l = this.classes.length; i < l; i++) {
+        var applier = rangy.createCssClassApplier(this.options.cssPrefix + this.classes[i], {
+            elementTagName: this.tag || 'span'
+        });
+        if (!applier.isAppliedToSelection()) {
+            return false;
+        }
+    }
+    return true;
 };
