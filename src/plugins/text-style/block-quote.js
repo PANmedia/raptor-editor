932f06eeebb8bb9b03b7b5ab37e0cf7fa649932c
David Neilsen <david@panmedia.co.nz>
2012-12-19 09:33:42 +1300

    Update block quote

--- src/plugins/text-style/block-quote.js
+++ src/plugins/text-style/block-quote.js
@@ -1,8 +1,16 @@
-Raptor.registerUi(new PreviewButton({
+Raptor.registerUi(new PreviewToggleButton({
     name: 'textBlockQuote',
     action: function() {
-        selectionToggleWrapper('blockquote', { 
-            classes: this.options.classes || this.options.cssPrefix + 'blockquote' 
-        });
+        listToggle('blockquote', 'p', this.raptor.getElement());
+        this.selectionChange();
+    },
+    selectionToggle: function() {
+        var result = true;
+        selectionEachBlock(function(block) {
+            if ($(block).parentsUntil(this.raptor.getElement(), 'blockquote').length === 0) {
+                result = false;
+            }
+        }.bind(this), this.raptor.getElement());
+        return result;
     }
 }));
