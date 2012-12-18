36747b1af7a2bd6fb2806de265c8fdb483b6ab9d
David Neilsen <david@panmedia.co.nz>
2012-12-19 09:37:51 +1300

    Fix expose script

--- scripts/expose.php
+++ scripts/expose.php
@@ -10,7 +10,15 @@ foreach (glob_recursive(__DIR__ . '/../src/tools/*.js') as $file) {
         $result[] = "    $function: $function";
     }
 }
-sort($result);
+
+foreach (glob_recursive(__DIR__ . '/../src/adapters/*.js') as $file) {
+    $contents = file_get_contents($file);
+    $matches = null;
+    preg_match_all('/^function (.+?)\(/m', $contents, $matches);
+    foreach ($matches[1] as $function) {
+        $result[] = "    $function: $function";
+    }
+}
 
 foreach (glob_recursive(__DIR__ . '/../src/components/*.js') as $file) {
     $contents = file_get_contents($file);
@@ -24,11 +32,10 @@ sort($result);
 
 $result = implode(',' . PHP_EOL, $result);
 echo "
-$.extend($.ui.raptor, Raptor, {
-
-    // <expose>
+// <expose>
+$.extend(Raptor, {
 $result
-    // </expose>
-
 });
+window.Raptor = Raptor;
+// </expose>
 ";
