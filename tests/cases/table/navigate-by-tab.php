c92a007eed863e08c932c6cacd480d7626347ae8
Melissa <melissa@panmedia.co.nz>
2013-01-07 15:55:05 +1300

    template for table nav, needs more work but i'm stuck

--- tests/cases/table/navigate-by-tab.php
+++ tests/cases/table/navigate-by-tab.php
@@ -6,12 +6,12 @@
 </head>
 <body class="simple">
     <div class="test-1">
-        <h1>Insert Column 1</h1>
+        <h1>Navigate By Tab 1: Simple Tab</h1>
         <div class="test-input">
             <div class="editible">
                 <table>
                     <tr>
-                        <td>Cell 0,0</td>
+                        <td>Cell{} 0,0</td>
                         <td>Cell 1,0</td>
                         <td>Cell 2,0</td>
                     </tr>
@@ -33,23 +33,20 @@
                 <table>
                     <tr>
                         <td>Cell 0,0</td>
-                        <td></td>
-                        <td>Cell 1,0</td>
+                        <td>{Cell 1,0}</td>
                         <td>Cell 2,0</td>
                     </tr>
                     <tr>
                         <td>Cell 0,1</td>
-                        <td></td>
                         <td>Cell 1,1</td>
                         <td>Cell 2,1</td>
                     </tr>
                     <tr>
                         <td>Cell 0,2</td>
-                        <td></td>
                         <td>Cell 1,2</td>
                         <td>Cell 2,2</td>
                     </tr>
-            </table>
+                </table>
             </div>
         </div>
         <div class="test-output"></div>
@@ -57,13 +54,15 @@
     </div>
     <script type="text/javascript">
         testEditor('.test-1', function(input) {
-            input.find('.editible').data('raptor').getLayout().getElement().find('.raptor-ui-table-insert-column').trigger('click');
+            var e = jQuery.Event("keydown");
+            e.which = 9; //key code
+            $(this).blur().closest('td').next().find('td').focus();
             rangesToTokens(rangy.getSelection().getAllRanges());
         });
     </script>
 
     <div class="test-2">
-        <h1>Insert Column 2</h1>
+        <h1>Navigate By Tab 2: Tab in Header</h1>
         <div class="test-input">
             <div class="editible">
                 <table>
@@ -71,7 +70,7 @@
                         <tr>
                             <th>Header</th>
                             <th>Header</th>
-                            <th>Header</th>
+                            <th>{}Header</th>
                             <th>Header</th>
                             <th>Header</th>
                             <th>Header</th>
@@ -142,9 +141,8 @@
                         <tr>
                             <th>Header</th>
                             <th>Header</th>
-                            <th></th>
                             <th>Header</th>
-                            <th>Header</th>
+                            <th>{Header}</th>
                             <th>Header</th>
                             <th>Header</th>
                         </tr>
@@ -153,7 +151,6 @@
                         <tr>
                             <td>Cell</td>
                             <td>Cell</td>
-                            <td></td>
                             <td>Cell</td>
                             <td rowspan="2">Cell</td>
                             <td>Cell</td>
@@ -162,21 +159,18 @@
                         <tr>
                             <td>Cell</td>
                             <td colspan="2">Cell</td>
-                            <td></td>
                             <td rowspan="2">Cell</td>
                             <td>Cell</td>
                         </tr>
                         <tr>
                             <td>Cell</td>
                             <td>Cell</td>
-                            <td></td>
                             <td colspan="2">Cell</td>
                             <td>Cell</td>
                         </tr>
                         <tr>
                             <td>Cell</td>
                             <td>Cell</td>
-                            <td></td>
                             <td>Cell</td>
                             <td>Cell</td>
                             <td>Cell</td>
@@ -185,7 +179,6 @@
                         <tr>
                             <td>Cell</td>
                             <td colspan="2">Cell</td>
-                            <td></td>
                             <td>Cell</td>
                             <td>Cell</td>
                             <td>Cell</td>
@@ -193,7 +186,6 @@
                         <tr>
                             <td>Cell</td>
                             <td>Cell</td>
-                            <td></td>
                             <td>Cell</td>
                             <td>Cell</td>
                             <td>Cell</td>
@@ -204,7 +196,6 @@
                         <tr>
                             <th>Header</th>
                             <td>Cell</td>
-                            <td></td>
                             <td>Cell</td>
                             <td>Cell</td>
                             <td>Cell</td>
@@ -219,7 +210,159 @@
     </div>
      <script type="text/javascript">
         testEditor('.test-2', function(input) {
-            input.find('.editible').data('raptor').getLayout().getElement().find('.raptor-ui-table-insert-column').trigger('click');
+            rangesToTokens(rangy.getSelection().getAllRanges());
+        });
+    </script>
+
+    <div class="test-3">
+        <h1>Navigate By Tab 3: Tab in Footer</h1>
+        <div class="test-input">
+            <div class="editible">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Header</th>
+                            <th>Header</th>
+                            <th>Header</th>
+                            <th>Header</th>
+                            <th>Header</th>
+                            <th>Header</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                            <td rowspan="2">Cell</td>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                        </tr>
+                        <tr>
+                            <td>Cell</td>
+                            <td colspan="2">Cell</td>
+                            <td rowspan="2">Cell</td>
+                            <td>Cell</td>
+                        </tr>
+                        <tr>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                            <td colspan="2">Cell</td>
+                            <td>Cell</td>
+                        </tr>
+                        <tr>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                        </tr>
+                        <tr>
+                            <td>Cell</td>
+                            <td colspan="2">Cell</td>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                        </tr>
+                        <tr>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                        </tr>
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <th>Header</th>
+                            <td>{}Cell</td>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
+        <div class="test-expected">
+            <div class="editible">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Header</th>
+                            <th>Header</th>
+                            <th>Header</th>
+                            <th>Header</th>
+                            <th>Header</th>
+                            <th>Header</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                            <td rowspan="2">Cell</td>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                        </tr>
+                        <tr>
+                            <td>Cell</td>
+                            <td colspan="2">Cell</td>
+                            <td rowspan="2">Cell</td>
+                            <td>Cell</td>
+                        </tr>
+                        <tr>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                            <td colspan="2">Cell</td>
+                            <td>Cell</td>
+                        </tr>
+                        <tr>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                        </tr>
+                        <tr>
+                            <td>Cell</td>
+                            <td colspan="2">Cell</td>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                        </tr>
+                        <tr>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                        </tr>
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <th>Header</th>
+                            <td>Cell</td>
+                            <td>{Cell}</td>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                            <td>Cell</td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
+        <div class="test-output"></div>
+        <div class="test-diff"></div>
+    </div>
+     <script type="text/javascript">
+        testEditor('.test-3', function(input) {
             rangesToTokens(rangy.getSelection().getAllRanges());
         });
     </script>
