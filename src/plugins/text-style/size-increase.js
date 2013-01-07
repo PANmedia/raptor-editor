0fd1a72ea7e5d376c6940407f7bdf254aea4535b
David Neilsen <david@panmedia.co.nz>
2013-01-08 12:02:25 +1300

    Fix text size buttons

--- src/plugins/text-style/size-increase.js
+++ src/plugins/text-style/size-increase.js
@@ -1,6 +1,8 @@
 Raptor.registerUi(new PreviewButton({
     name: 'textSizeIncrease',
     action: function() {
+        selectionExpandToWord();
         selectionInverseWrapWithTagClass('big', this.options.cssPrefix + 'big', 'small', this.options.cssPrefix + 'small');
+        this.raptor.getElement().find('small.' + this.options.cssPrefix + 'small:empty, big.' + this.options.cssPrefix + 'big:empty').remove();
     }
 }));
