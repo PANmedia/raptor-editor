b5afa8abf5b8a896368519da11dcbe864b92d34f
Michael Robinson <mike@panmedia.co.nz>
2013-01-07 14:30:10 +1300

    Tidied documentation in base.js

--- src/presets/base.js
+++ src/presets/base.js
@@ -31,20 +31,25 @@ var basePreset = {
     bind: {},
 
     /**
-     * Namespace used for persistence to prevent conflicting with other stored values.
+     * Namespace used for persistence to prevent conflicting with other stored
+     * values.
      *
      * @type String
      */
     namespace: null,
 
     /**
-     * Switch to indicated that some events should be automatically applied to all editors that are 'unified'
+     * Switch to indicated that some events should be automatically applied to
+     * all editors that are 'unified'
+     *
      * @type boolean
      */
     unify: true,
 
     /**
-     * Switch to indicate weather or not to stored persistent values, if set to false the persist function will always return null
+     * Switch to indicate weather or not to stored persistent values, if set to
+     * false the persist function will always return null
+     *
      * @type boolean
      */
     persistence: true,
@@ -56,25 +61,32 @@ var basePreset = {
     persistenceName: 'uiEditor',
 
     /**
-     * Switch to indicate weather or not to a warning should pop up when the user navigates aways from the page and there are unsaved changes
+     * Switch to indicate weather or not to a warning should pop up when the
+     * user navigates aways from the page and there are unsaved changes
+     *
      * @type boolean
      */
     unloadWarning: true,
 
     /**
      * Switch to automatically enabled editing on the element
+     *
      * @type boolean
      */
     autoEnable: false,
 
     /**
      * Only enable editing on certian parts of the element
+     *
      * @type {jQuerySelector}
      */
     partialEdit: false,
 
     /**
-     * Switch to specify if the editor should automatically enable all plugins, if set to false, only the plugins specified in the 'plugins' option object will be enabled
+     * Switch to specify if the editor should automatically enable all plugins,
+     * if set to false, only the plugins specified in the 'plugins' option
+     * object will be enabled
+     *
      * @type boolean
      */
     enablePlugins: true,
@@ -86,13 +98,18 @@ var basePreset = {
     disabledPlugins: [],
 
     /**
-     * And array of arrays denoting the order and grouping of UI elements in the toolbar
+     * And array of arrays denoting the order and grouping of UI elements in the
+     * toolbar
+     *
      * @type String[]
      */
     uiOrder: null,
 
     /**
-     * Switch to specify if the editor should automatically enable all UI, if set to false, only the UI specified in the {@link Raptor.defaults.ui} option object will be enabled
+     * Switch to specify if the editor should automatically enable all UI, if
+     * set to false, only the UI specified in the {@link Raptor.defaults.ui}
+     * option object will be enabled
+     *
      * @type boolean
      */
     enableUi: true,
@@ -112,13 +129,19 @@ var basePreset = {
     },
 
     /**
-     * Switch to indicate that the element the editor is being applied to should be replaced with a div (useful for textareas), the value/html of the replaced element will be automatically updated when the editor element is changed
+     * Switch to indicate that the element the editor is being applied to should
+     * be replaced with a div (useful for textareas), the value/html of the
+     * replaced element will be automatically updated when the editor element is
+     * changed
+     *
      * @type boolean
      */
     replace: false,
 
     /**
-     * A list of styles that will be copied from the replaced element and applied to the editor replacement element
+     * A list of styles that will be copied from the replaced element and
+     * applied to the editor replacement element
+     *
      * @type String[]
      */
     replaceStyle: [
@@ -134,7 +157,9 @@ var basePreset = {
     baseClass: 'raptor',
 
     /**
-     * CSS class prefix that is prepended to inserted elements classes. E.g. "cms-bold"
+     * CSS class prefix that is prepended to inserted elements classes.
+     * E.g. "cms-bold"
+     *
      * @type String
      */
     cssPrefix: 'cms-',
