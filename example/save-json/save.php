fda3309ae65738e5302733077aadc6e7c1d32e8a
David Neilsen <david@panmedia.co.nz>
2013-01-11 13:33:00 +1300

    Update save JSON example

--- example/save-json/save.php
+++ example/save-json/save.php
@@ -1,9 +1,28 @@
 <?php
-var_dump($_POST);
 if (isset($_POST['raptor-content'])) {
-    $content = json_decode($_POST['raptor-content']);
-    if ($content) {
-        var_dump($content);
+    $file = __DIR__ . '/content.json';
+    $content = [];
+    if (file_exists(__DIR__ . '/content.json')) {
+        $content = file_get_contents($file);
+        $content = json_decode($content, true);
+        if ($content === false) {
+            $content = [];
+        }
+    }
+
+    $newContent = json_decode($_POST['raptor-content']);
+    if ($newContent) {
+        foreach ($newContent as $id => $html) {
+            $content[$id] = $html;
+        }
+    }
+
+    $content = json_encode($content, JSON_PRETTY_PRINT);
+    if ($content !== false) {
+        if (file_put_contents($file, $content)) {
+            echo json_encode(true);
+            return;
+        }
     }
 }
 echo json_encode(false);
