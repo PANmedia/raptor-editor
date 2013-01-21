b955e4f1eddebd540fe61e51f40d1d33c0dd6e1c
Michael Robinson <mike@panmedia.co.nz>
2013-01-21 18:56:13 +1300

    Check for valid wrapper before enabling list buttons

--- src/plugins/list/list-ordered.js
+++ src/plugins/list/list-ordered.js
@@ -1,6 +1,14 @@
 Raptor.registerUi(new PreviewButton({
     name: 'listOrdered',
+    init: function() {
+        var result = SelectMenu.prototype.init.call(this);
+        if (elementIsValid(this.raptor.getElement(), listValidParents())) {
+            this.raptor.bind('selectionChange', this.updateButton.bind(this));
+            return result;
+        }
+        return;
+    },
     action: function() {
-        listToggle('ol', this.raptor.getElement());
+        listToggle('ol', 'li', this.raptor.getElement());
     }
 }));
--- src/plugins/list/list-unordered.js
+++ src/plugins/list/list-unordered.js
@@ -1,5 +1,13 @@
 Raptor.registerUi(new PreviewButton({
     name: 'listUnordered',
+    init: function() {
+        var result = SelectMenu.prototype.init.call(this);
+        if (elementIsValid(this.raptor.getElement(), listValidParents())) {
+            this.raptor.bind('selectionChange', this.updateButton.bind(this));
+            return result;
+        }
+        return;
+    },
     action: function() {
         listToggle('ul', this.raptor.getElement());
     }
