df7b34049a49d548f7f3838800190ef524336a73
Melissa <melissa@panmedia.co.nz>
2013-01-24 16:41:40 +1300

    commenting

--- src/tools/node.js
+++ src/tools/node.js
@@ -1,4 +1,16 @@
+/**
+ * @fileOverview Find node parent helper function.
+ * @author David Neilsen david@panmedia.co.nz
+ * @author Michael Robinson michael@panmedia.co.nz
+ */
 
+
+/**
+ * Find the first parent of a node that is not a text node.
+ *
+ * @param {Node} node
+ * @returns {Node}
+ */
 function nodeFindParent(node) {
     while (node.nodeType === Node.TEXT_NODE) {
         node = node.parentNode;
