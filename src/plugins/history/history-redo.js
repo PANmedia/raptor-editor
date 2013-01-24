496f071f9a3651f6c9525967145ef1c5d9bc07eb
Michael Robinson <mike@panmedia.co.nz>
2013-01-25 12:17:59 +1300

    Updated history redo / undo to toggle button enabled state depending on history

--- src/plugins/history/history-redo.js
+++ src/plugins/history/history-redo.js
@@ -1,6 +1,29 @@
-Raptor.registerUi(new Button({
+function HistoryRedoButton(options) {
+    Button.call(this, options);
+}
+
+HistoryRedoButton.prototype = Object.create(Button.prototype);
+
+HistoryRedoButton.prototype.init = function() {
+    this.raptor.bind('historychange', this.historyChange.bind(this));
+    return Button.prototype.init.apply(this, arguments);
+};
+
+Raptor.registerUi(new HistoryRedoButton({
     name: 'historyRedo',
     action: function() {
         this.raptor.historyForward();
+    },
+    getButton: function() {
+        var button = Button.prototype.getButton.call(this);
+        aButtonDisable(button);
+        return button;
+    },
+    historyChange: function() {
+        if (this.raptor.present < this.raptor.history.length - 1) {
+            aButtonEnable(this.button);
+        } else {
+            aButtonDisable(this.button);
+        }
     }
 }));
