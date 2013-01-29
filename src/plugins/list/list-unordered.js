efe4a41676d77f7682b35ed3dcbeaa64b5302f55
Michael Robinson <mike@panmedia.co.nz>
2013-01-30 09:32:16 +1300

    Updated list buttons

--- src/plugins/list/list-unordered.js
+++ src/plugins/list/list-unordered.js
@@ -1,9 +1,8 @@
 Raptor.registerUi(new PreviewButton({
     name: 'listUnordered',
     init: function() {
-        var result = SelectMenu.prototype.init.call(this);
-        if (elementIsValid(this.raptor.getElement(), listValidParents())) {
-            this.raptor.bind('selectionChange', this.updateButton.bind(this));
+        var result = Button.prototype.init.apply(this, arguments);
+        if (elementIsValid(this.raptor.getElement(), listValidUlOlParents)) {
             return result;
         }
         return;
