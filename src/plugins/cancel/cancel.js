4ea8b40f56dc2e1e5ae42b3b289639958729c08a
David Neilsen <david@panmedia.co.nz>
2013-01-30 15:29:35 +1300

    Auto cancel if not dirty

--- src/plugins/cancel/cancel.js
+++ src/plugins/cancel/cancel.js
@@ -1,14 +1,21 @@
-var cancelDialog = null,
-    cancelInstance;
-
 Raptor.registerUi(new DialogButton({
     name: 'cancel',
     dialogOptions: {
         width: 500
     },
+
+    action: function() {
+        if (this.raptor.isDirty()) {
+            DialogButton.prototype.action.call(this);
+        } else {
+            this.applyAction();
+        }
+    },
+
     applyAction: function() {
         this.raptor.cancelEditing();
     },
+
     getDialogTemplate: function() {
         return $('<div>').html(_('cancelDialogContent'));
     }
