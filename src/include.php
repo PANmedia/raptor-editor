6dc3923a90a9eef51b6cfd85fc327201f3bd4689
Michael Robinson <mike@panmedia.co.nz>
2013-01-31 10:54:22 +1300

    Re-enabled blockquote

--- src/include.php
+++ src/include.php
@@ -139,7 +139,7 @@
 <script type="text/javascript" src="<?= $uri ?>plugins/text-align/justify.js"></script>
 
 <script type="text/javascript" src="<?= $uri ?>plugins/text-style/bold.js"></script>
-<?php /* <script type="text/javascript" src="<?= $uri ?>plugins/text-style/block-quote.js"></script> */ ?>
+<script type="text/javascript" src="<?= $uri ?>plugins/text-style/block-quote.js"></script>
 <script type="text/javascript" src="<?= $uri ?>plugins/text-style/italic.js"></script>
 <script type="text/javascript" src="<?= $uri ?>plugins/text-style/size-decrease.js"></script>
 <script type="text/javascript" src="<?= $uri ?>plugins/text-style/size-increase.js"></script>
