0140f39269c01cc378e6aa4eea8d90e1bef04d7d
Michael Robinson <mike@panmedia.co.nz>
2013-02-01 09:26:48 +1300

    Updated documentation

--- src/raptor-widget.js
+++ src/raptor-widget.js
@@ -9,12 +9,12 @@
  */
 
 /**
- * @lends $.editor.prototype
+ * @class
  */
 var RaptorWidget = {
 
     /**
-     * Constructor
+     * @constructs RaptorWidget
      */
     _init: function() {
         // Prevent double initialisation
@@ -142,6 +142,7 @@ var RaptorWidget = {
 
     /**
      * Attaches the editors internal events.
+     * @fires RaptorWidget#resize
      */
     attach: function() {
         this.bind('change', this.historyPush);
