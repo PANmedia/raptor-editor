b67cdda465449553b847ab82f3d2de4c00a5eb9f
David Neilsen <david@panmedia.co.nz>
2013-02-05 09:49:16 +1300

    Update links in example/include/head.php

--- example/include/head.php
+++ example/include/head.php
@@ -5,19 +5,18 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 <link rel="stylesheet" href="../include/style.css" />
 <?php if ($type === 'light'): ?>
-    <link rel="stylesheet" href="../../src/dependencies/themes/aristo/jquery-ui.css" />
-    <link rel="stylesheet" href="../../src/theme/theme.css" />
+    <link rel="stylesheet" type="text/css" href="../../src/dependencies/themes/aristo/jquery-ui.css" />
+    <link rel="stylesheet" type="text/css" href="../../src/theme/theme.css" />
     <script src="../../src/dependencies/jquery.js"></script>
     <script src="../../src/dependencies/jquery-ui.js"></script>
     <script src="../../packages/raptor.light.min.js"></script>
 <?php elseif ($type === 'rails'): ?>
     <link rel="stylesheet" type="text/css" href="../../src/dependencies/themes/redmond/jquery-ui.css" />
-    <link rel="stylesheet" href="../../packages/raptor-front-end.css" />
+    <link rel="stylesheet" type="text/css" href="../../packages/raptor-front-end.css" />
     <script src="../../src/dependencies/jquery.js"></script>
     <script src="../../src/dependencies/jquery-ui.js"></script>
     <script src="../../packages/raptor.rails.js"></script>
 <?php elseif ($type === 'mammoth'): ?>
-    <link rel="stylesheet" href="../include/mammoth-theme.css" />
     <link rel="stylesheet" type="text/css" href="../../src/dependencies/themes/mammoth/theme.css" />
     <link rel="stylesheet" type="text/css" href="../../src/dependencies/themes/mammoth/theme-icons.css" />
     <script src="../../src/dependencies/jquery.js"></script>
