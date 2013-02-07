fba42b0ec041b5b7444d7761193abe29dcbc627a
Melissa <melissa@panmedia.co.nz>
2013-02-07 17:01:24 +1300

    jsdocs commenting

--- src/plugins/text-align/center.js
+++ src/plugins/text-align/center.js
@@ -1,6 +1,19 @@
+/**
+ * @fileOverview Contains the center align button code.
+ * @author  David Neilsen <david@panmedia.co.nz>
+ * @author  Michael Robinson <michael@panmedia.co.nz>
+ * @author Melissa Richards <melissa@panmedia.co.nz>
+ */
+
+/**
+ * Creates a text align button to align text center.
+ *
+ * @todo param details?
+ * @param {type} param.
+ */
 Raptor.registerUi(new TextAlignButton({
     name: 'alignCenter',
     getClass: function() {
-        return this.options.cssPrefix + 'center'
+        return this.options.cssPrefix + 'center';
     }
 }));
