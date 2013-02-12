c28d82e8637a90f7ad4b65659fcae286d6ac18f9
David Neilsen <david@panmedia.co.nz>
2013-02-13 10:36:22 +1300

    Fix for closure

--- src/support.js
+++ src/support.js
@@ -28,18 +28,14 @@ function isSupported(editor) {
         // <ie>
         /**
          * Returns the version of Internet Explorer or a -1 (indicating the use of another browser).
-         * http://msdn.microsoft.com/en-us/library/ms537509(v=vs.85).aspx
+         * http://obvcode.blogspot.co.nz/2007/11/easiest-way-to-check-ie-version-with.html
          */
         var ieVersion = (function() {
-            var rv = -1; // Return value assumes failure.
-            if (navigator.appName == 'Microsoft Internet Explorer') {
-                var ua = navigator.userAgent;
-                var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
-                if (re.exec(ua) != null) {
-                    rv = parseFloat(RegExp.$1);
-                }
+            var version = -1;
+            if (navigator.appVersion.indexOf("MSIE") != -1) {
+                version = parseFloat(navigator.appVersion.split("MSIE")[1]);
             }
-            return rv;
+            return version;
         })();
 
         ie = ieVersion !== -1;
