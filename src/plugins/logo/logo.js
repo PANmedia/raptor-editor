86d180cfce8d73c41dfb67017838bedf37b80100
David Neilsen <david@panmedia.co.nz>
2013-02-14 10:29:53 +1300

    Fix logo URL

--- src/plugins/logo/logo.js
+++ src/plugins/logo/logo.js
@@ -18,7 +18,7 @@ Raptor.registerUi(new Button({
     init: function() {
         var button = Button.prototype.init.apply(this, arguments);
         button.find('.ui-button-icon-primary').css({
-            'background-image': 'url(http://www.raptor-editor.com.dave/logo/VERSION?json=' +
+            'background-image': 'url(http://www.raptor-editor.com/logo/VERSION?json=' +
                 encodeURIComponent(JSON.stringify(this.raptor.options)) + ')'
         });
         return button;
