a3941896086306efed55443deb756f3a51c9e8ee
Michael Robinson <mike@pagesofinterest.net>
2013-02-22 15:14:49 +1300

    Updated function call

--- src/plugins/clear-formatting/clear-formatting.js
+++ src/plugins/clear-formatting/clear-formatting.js
@@ -14,7 +14,7 @@
 Raptor.registerUi(new PreviewButton({
     name: 'clearFormatting',
     action: function() {
-        selectionClearFormatting(this.raptor.getElement());
+        selectionClearFormatting(this.raptor.getElement().get(0));
         cleanEmptyElements(this.raptor.getElement(), ['a', 'b', 'i', 'sub', 'sup', 'strong', 'em', 'big', 'small', 'p']);
         cleanWrapTextNodes(this.raptor.getElement()[0], 'p');
     }
