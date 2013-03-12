6a3abec0b13c83e4281e5a486c8a359ddd90afe7
Ivan Gromov <summer.is.gone@gmail.com>
2013-03-12 21:59:35 +0600

    Added experimental way Russian translation

---
+++ src/locales/ru.js
@@ -0,0 +1,226 @@
+/**
+ * @fileOverview Russian strings file.
+ * @author Ivan Gromov, summer.is.gone@gmail.com
+ */
+registerLocale('ru', 'Russian', {
+    alignCenterTitle: 'По центру',
+    alignJustifyTitle: 'По ширине',
+    alignLeftTitle: 'По левому краю',
+    alignRightTitle: 'По правому краю',
+
+    cancelDialogCancelButton: 'Продолжить редактирование',
+    cancelDialogContent: 'Вы уверены, что хотите завершить редактирование? Все изменения будут утеряны!',
+    cancelDialogOKButton: 'Завершить редактирование',
+    cancelDialogTitle: 'Завершить редактирование',
+    cancelTitle: 'Завершить редактирование',
+
+    classMenuTitle: 'Выбор стиля',
+    clearFormattingTitle: 'Очистить форматирование',
+    clickButtonToEditPluginButton: 'Нажмите для начала редактирования',
+
+    colorMenuBasicAutomatic: 'Автоматически',
+    colorMenuBasicBlack: 'Черный',
+    colorMenuBasicBlue: 'Синий',
+    colorMenuBasicGreen: 'Зеленый',
+    colorMenuBasicGrey: 'Серый',
+    colorMenuBasicOrange: 'Оранжевый',
+    colorMenuBasicPurple: 'Лиловый',
+    colorMenuBasicRed: 'Красный',
+    colorMenuBasicTitle: 'Изменить цевет текста',
+    colorMenuBasicWhite: 'Белый',
+
+    dockToElementTitle: 'Прикрепить редактор к элементу',
+    dockToScreenTitle: 'Прикрепить редактор к документу',
+
+    embedTitle: 'Встраиваемый объект',
+    embedDialogTitle: 'Встраиваемый объект',
+    embedDialogTabCode: 'Встраиваемый код',
+    embedDialogTabCodeContent: 'Вставьте код в поле ниже:',
+    embedDialogTabPreview: 'Предпросмотр',
+    embedDialogTabPreviewContent: 'Предпросмотр встраиваемого объекта:',
+    embedDialogOKButton: 'Встраиваемый объект',
+    embedDialogCancelButton: 'Отмена',
+
+    errorPluginNoName: 'Модуль "{{plugin}}" содержит ошибку (поле свойство обязательное)',
+    errorPluginNotObject: 'Модуль "{{plugin}}" содержит ошибку (предполагается наличие объекта)',
+    errorPluginOverride: 'Модуль "{{name}}" уже был зарегистрирован и будет перезаписан',
+
+    errorUINoName: 'Интерфейс "{{ui}}" содержит ошибку (поле свойство обязательное)',
+    errorUINotObject: 'Интерфейс "{{ui}}" содержит ошибку (предполагается наличие объекта)',
+    errorUIOverride: 'Интерфейс "{{name}}" уже был зарегистрирован и будет перезаписан',
+
+    floatLeftTitle: 'Выровнять изображение по левому краю',
+    floatNoneTitle: 'Отменить выравнивание изображения',
+    floatRightTitle: 'Выровнять изображение по правому краю',
+
+    guidesTitle: 'Показать направляющие',
+
+    historyRedoTitle: 'Повторить действие',
+    historyUndoTitle: 'Отменить действие',
+
+    hrCreateTitle: 'Вставить горизонтальную линию',
+
+    imageResizeButtonText: 'Изменить размер изображения',
+    imageResizeButtonDialogWidth: 'Ширина изображения',
+    imageResizeButtonDialogHeight: 'Высота изображения',
+    imageResizeButtonDialogWidthPlaceHolder: 'Ширина',
+    imageResizeButtonDialogHeightPlaceHolder: 'Высота',
+    imageResizeButtonDialogTitle: 'Изменить размер изображения',
+    imageResizeButtonDialogOKButton: 'Изменить размер',
+    imageResizeButtonDialogCancelButton: 'Отмена',
+
+    insertFileTitle: 'Вставить файл',
+    insertFileDialogOKButton: 'Вставить файл',
+    insertFileDialogCancelButton: 'Отмена',
+
+    languageMenuTitle: 'Изменить язык',
+    languageMenuEN: 'Английский',
+    languageMenuDE: 'Немецкий',
+    languageMenuES: 'Испанский',
+    languageMenuFR: 'Французский',
+    languageMenuNL: 'Голландский',
+    languageMenuSV: 'Шведский',
+    languageMenuZHCN: 'Упрощенный китайский',
+
+    listOrderedTitle: 'Нумерованый список',
+    listUnorderedTitle: 'Ненумерованый список',
+
+    linkCreateTitle: 'Вставить ссылку',
+    linkRemoveTitle: 'Удалить ссылку',
+
+    linkCreateDialogTitle: 'Вставить ссылку',
+    linkCreateDialogOKButton: 'Вставить ссылку',
+    linkCreateDialogCancelButton: 'Отмена',
+    linkCreateDialogMenuHeader: 'Выбарите тип ссылки',
+
+    linkTypeEmailLabel: 'Адрес эл. почты',
+    linkTypeEmailHeader: 'Ссылка на адрес эл. почты',
+    linkTypeEmailToLabel: 'Почта:',
+    linkTypeEmailToPlaceHolder: 'Введите адрес эл. почты',
+    linkTypeEmailSubjectLabel: 'Тема (необязательно):',
+    linkTypeEmailSubjectPlaceHolder: 'Введите тему',
+
+    linkTypeExternalLabel: 'Страница на другом сайте',
+    linkTypeExternalHeader: 'Ссылка на страницу другого сайта',
+    linkTypeExternalLocationLabel: 'Адрес:',
+    linkTypeExternalLocationPlaceHolder: 'Введите URL',
+    linkTypeExternalNewWindowHeader: 'В новом окне',
+    linkTypeExternalNewWindowLabel: 'Поставьте флажок, чтобы страница открылась в новой вкладке.',
+    linkTypeExternalInfo:
+        '<h2>Не знаете что ввести в это поле?</h2>' +
+        '<ol>' +
+        '    <li>Найдите страницу, на которую вы хотите сделать ссылку</li>' +
+        '    <li>Скопируйте адрес из строки браузера и вставьте в поле выше</li>' +
+        '</ol>',
+
+    linkTypeDocumentLabel: 'Документ или другой файл',
+    linkTypeDocumentHeader: 'Ссылка на документ или другой файл',
+    linkTypeDocumentLocationLabel: 'Адрес:',
+    linkTypeDocumentLocationPlaceHolder: 'Введите URL',
+    linkTypeDocumentNewWindowHeader: 'В новом окне',
+    linkTypeDocumentNewWindowLabel: 'Поставьте флажок, чтобы страница открылась в новой вкладке.',
+    linkTypeDocumentInfo:
+        '<h2>Не знаете что ввести в это поле?</h2>' +
+        '<ol>' +
+        '    <li>Удостоверьтесь, что файл был загружен на ваш сайт.</li>' +
+        '    <li>Откройте файл в браузере.</li>' +
+        '    <li>Скопируйте адрес из строки браузера в поле выше.</li>' +
+        '</ol>',
+
+    linkTypeInternalLabel: 'Страница на этом сайте',
+    linkTypeInternalHeader: 'Ссылка на страницу на этом сайте',
+    linkTypeInternalLocationLabel: '',
+    linkTypeInternalLocationPlaceHolder: 'Введите URL',
+    linkTypeInternalNewWindowHeader: 'В новом окне',
+    linkTypeInternalNewWindowLabel: 'Поставьте флажок, чтобы страница открылась в новой вкладке.',
+    linkTypeInternalInfo:
+        '<h2>Не знаете что ввести в это поле?</h2>' +
+        '<ol>' +
+        '    <li>Выберите страницу на сайте, на которую хотите сделать ссылку.</li>' +
+        '    <li>Скопируйте адрес из строки браузера, исключив "{{domain}}" и вставьте его в поле выше.</li>' +
+        '</ol>',
+
+    logoTitle: 'Узнать больше о редакторе Raptor',
+
+    navigateAway: '\nНа странице несохраненные изменения. \nЕсли вы покинете страницу, изменения будут утеряны',
+
+    pasteDialogTitle: 'Вставить',
+    pasteDialogOKButton: 'Вставить',
+    pasteDialogCancelButton: 'Отмена',
+    pasteDialogPlain: 'Простой текст',
+    pasteDialogFormattedCleaned: 'Отформатировано и очищено',
+    pasteDialogFormattedUnclean: 'Отформатировано без очистки',
+    pasteDialogSource: 'Код',
+
+    saveTitle: 'Сохранить содержимое',
+    saveJsonFail: 'Не удалось сохранить {{failed}} блок (блоков).',
+    saveJsonSaved: 'Сохранено {{saved}} блоков текста.',
+    saveRestFail: 'Не удалось сохранить {{failed}} блок (блоков).',
+    saveRestPartial: 'Сохранено {{saved}} из {{failed}} блоков текста.',
+    saveRestSaved: 'Сохранено {{saved}} блоков текста.',
+
+    snippetMenuTitle: 'Модули',
+
+    specialCharactersDialogOKButton: 'OK',
+    specialCharactersDialogTitle: 'Вставить специальные символы',
+    specialCharactersHelp: 'Нажмите на спецсимвол чтобы добавить. Нажмите "OK" для закрытия диалога',
+    specialCharactersTitle: 'Вставить специальные символы',
+
+    statisticsButtonCharacterOverLimit: 'Символов больше ограничения: {{charactersRemaining}}',
+    statisticsButtonCharacterRemaining: 'Символов осталось: {{charactersRemaining}}',
+    statisticsButtonCharacters: '{{characters}} символов',
+    statisticsDialogCharactersOverLimit: '{{characters}} символов, свыше ограничения: {{charactersRemaining}}',
+    statisticsDialogCharactersRemaining: '{{characters}} символов, до ограничения ещё {{charactersRemaining}}',
+    statisticsDialogNotTruncated: 'Текст не будет урезан',
+    statisticsDialogOKButton: 'Ок',
+    statisticsDialogSentence: '{{sentences}} предложение',
+    statisticsDialogSentences: '{{sentences}} предложений',
+    statisticsDialogTitle: 'Статистика содержимого',
+    statisticsDialogTruncated: 'Текст содержит более {{limit}} символов и может быть урезан',
+    statisticsDialogWord: '{{words}} слово',
+    statisticsDialogWords: '{{words}} слов',
+    statisticsTitle: 'Нажмите для просмотра статистики',
+
+    tableCreateTitle: 'Создать таблицу',
+    tableDeleteColumnTitle: 'Удалить колонку',
+    tableDeleteRowTitle: 'Удалить строку',
+    tableInsertColumnTitle: 'Вставить колонку',
+    tableInsertRowTitle: 'Вставить строку',
+    tableMergeCellsTitle: 'Объединить ячейки',
+    tableSplitCellsTitle: 'Разбить ячейки',
+
+    tagMenuTagH1: 'Заголовок&nbsp;1',
+    tagMenuTagH2: 'Заголовок&nbsp;2',
+    tagMenuTagH3: 'Заголовок&nbsp;3',
+    tagMenuTagH4: 'Заголовок&nbsp;4',
+    tagMenuTagNA: 'Н/Д',
+    tagMenuTagP: 'Параграф',
+    tagMenuTagDiv: 'Блок',
+    tagMenuTagPre: 'Форматированный',
+    tagMenuTagAddress: 'Адрес',
+    tagMenuTitle: 'Изменить стиль элемента',
+
+    tagTreeElementLink: 'ВЫбрано {{element}} элементов',
+    tagTreeElementTitle: 'Нажмите чтобы выбрать содержимое элемента "{{element}}"',
+    tagTreeRoot: 'корень',
+    tagTreeRootLink: 'Выбрать всё',
+    tagTreeRootTitle: 'Нажмите чтобы выбрать всё',
+
+    textBlockQuoteTitle: 'Цитата',
+    textBoldTitle: 'Жирный',
+    textItalicTitle: 'Курсив',
+    textStrikeTitle: 'Перечеркнутый',
+    textSubTitle: 'Нижний индекс',
+    textSuperTitle: 'Верхний индекс',
+    textUnderlineTitle: 'Подчеркнутый',
+    textSizeDecreaseTitle: 'Уменьшить размер шрифта',
+    textSizeIncreaseTitle: 'Увеличить размер шрифта',
+
+    unsavedEditWarningText: 'На странице имеются несохраненные изменения',
+
+    viewSourceDialogCancelButton: 'Закрыть',
+    viewSourceDialogOKButton: 'Примеить код',
+    viewSourceDialogTitle: 'Исходный код содержимого',
+    viewSourceTitle: 'Посмотреть или изменить код'
+
+});
