cf29ce3aed110ac3db9733a9755657a6a2b9a8c4
Michael Robinson <mike@pagesofinterest.net>
2013-03-21 12:05:45 +1300

    Added new button icons

---
+++ src/plugins/revisions/revisions-apply-button.js
@@ -0,0 +1,29 @@
+/**
+ * @fileOverview Contains the apply revisions button code.
+ * @author  Michael Robinson <michael@panmedia.co.nz>
+ * @author  David Neilsen <david@panmedia.co.nz>
+ * @author Melissa Richards <melissa@panmedia.co.nz>
+ */
+
+/**
+ * Creates an instance of the apply button that applies & saves the selected revision
+ *
+ * @param {type} param
+ */
+var RevisionsApplyButton = new Button({
+    name: 'revisionsApplyButton',
+    title: _('revisionsApplyButtonTitle'),
+    text: _('revisionsApplyButtonTitle'),
+
+    action: function() {
+        this.raptor.setHtml(this.options.revision.content);
+        this.raptor.setOriginalHtml(this.raptor.getHtml());
+        this.getSavePlugin().save();
+    },
+
+    getSavePlugin: function() {
+        return this.raptor.getPlugin(this.options.savePlugin);
+    }
+
+});
+
