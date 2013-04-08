591e692b32fdf3d2dd656664da43ef6f3c26aa48
Michael Robinson <mike@pagesofinterest.net>
2013-04-09 10:45:26 +1200

    Updated language menu documentation

--- src/plugins/language-menu/language-menu.js
+++ src/plugins/language-menu/language-menu.js
@@ -1,3 +1,19 @@
+/**
+ * @fileOverview Contains the insert file button code.
+ * @license http://www.raptor-editor.com/license
+ *
+ * @author David Neilsen <david@panmedia.co.nz>
+ * @author Michael Robinson <michael@panmedia.co.nz>
+ * @author Melissa Richards <melissa@panmedia.co.nz>
+ */
+
+/**
+ * The language menu class.
+ *
+ * @constructor
+ * @augments SelectMenu
+ * @param {Object} options
+ */
 function LanguageMenu(options) {
     SelectMenu.call(this, {
         name: 'languageMenu'
@@ -6,13 +22,23 @@ function LanguageMenu(options) {
 
 LanguageMenu.prototype = Object.create(SelectMenu.prototype);
 
+/**
+ * Initialize the language menu.
+ *
+ * @return {Element}
+ */
 LanguageMenu.prototype.init = function() {
     var result = Menu.prototype.init.call(this);
     aButtonSetLabel(this.button.button, _('languageMenu' + currentLocale.toUpperCase().replace('-', '')));
     aButtonSetIcon(this.button.button, 'ui-icon-flag-' + currentLocale.toLowerCase());
     return result;
-}
+};
 
+/**
+ * Change the editor's language to the current selection.
+ *
+ * @param {Event} event
+ */
 LanguageMenu.prototype.changeLanguage = function(event) {
     var locale = $(event.currentTarget).data('value');
     setTimeout(function() {
@@ -20,6 +46,9 @@ LanguageMenu.prototype.changeLanguage = function(event) {
     }, 1);
 };
 
+/**
+ * @return {jQuery}
+ */
 LanguageMenu.prototype.getMenuItems = function() {
     var items = '';
     for (var locale in locales) {
@@ -30,7 +59,7 @@ LanguageMenu.prototype.getMenuItems = function() {
         });
     }
     return $(items)
-        .click(this.changeLanguage.bind(this))
+        .click(this.changeLanguage.bind(this));
 };
 
 Raptor.registerUi(new LanguageMenu());
