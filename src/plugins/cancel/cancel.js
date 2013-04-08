3ed71ca44773a1f2cf0b827a169158df95922e5e
Michael Robinson <mike@pagesofinterest.net>
2013-04-09 10:45:17 +1200

    Added @license

--- src/plugins/cancel/cancel.js
+++ src/plugins/cancel/cancel.js
@@ -1,5 +1,7 @@
 /**
  * @fileOverview Contains the cancel editing dialog code.
+ * @license http://www.raptor-editor.com/license
+ *
  * @author David Neilsen <david@panmedia.co.nz>
  * @author Michael Robinson <michael@panmedia.co.nz>
  * @author Melissa Richards <melissa@panmedia.co.nz>
