28f06dc8526236a97c7dae9d87375e5a6bd0b960
David Neilsen <david@panmedia.co.nz>
2013-04-23 16:35:17 +1200

    Fix test tests/cases/embed-dialog/embed-object.php

--- tests/cases/embed-dialog/embed-object.php
+++ tests/cases/embed-dialog/embed-object.php
@@ -7,6 +7,20 @@
 <body class="simple">
     <script type="text/javascript">
         rangy.init();
+        function testEmbed(input) {
+            var embedButton = getLayoutElement(input).find('.raptor-ui-embed');
+            embedButton.trigger('click');
+
+            var video = '<video src="http://www.youtube.com/watch?v=KQbySwxU8L4" controls></video>';
+
+            var textarea = $('.raptor-ui-embed-code-tab textarea');
+            textarea.val(video);
+
+            var embedObjectButton = $('.raptor-ui-embed-dialog button:contains(Embed Object)');
+            embedObjectButton.trigger('click');
+
+            rangesToTokens(rangy.getSelection().getAllRanges());
+        }
     </script>
     <div class="test-1">
         <h1>Embed Object Button 1: Word Group Selection</h1>
@@ -34,20 +48,7 @@
         </div>
     </div>
     <script type="text/javascript">
-        testEditor('.test-1', function(input) {
-            var embedButton = getLayoutElement(input).find('.raptor-ui-embed');
-            embedButton.trigger('click');
-
-            var video = 'http://www.youtube.com/watch?v=KQbySwxU8L4';
-
-            var textarea = $(document.querySelector('.raptor-ui-embed-tab > textarea'));
-            textarea.value = video;
-
-            var embedObjectButton = $('.raptor-ui-embed-dialog button:contains(Embed Object)');
-            embedObjectButton.trigger('click');
-
-            rangesToTokens(rangy.getSelection().getAllRanges());
-        });
+        testEditor('.test-1', testEmbed);
     </script>
 
     <div class="test-2">
@@ -76,21 +77,7 @@
         </div>
     </div>
     <script type="text/javascript">
-        testEditor('.test-2', function(input) {
-            var embedButton = getLayoutElement(input).find('.raptor-ui-embed');
-            embedButton.trigger('click');
-
-            var video = 'http://www.youtube.com/watch?v=KQbySwxU8L4';
-
-            var textarea = $(document.querySelector('.raptor-ui-embed-tab > textarea'));
-            textarea.value = video;
-
-            var embedObjectButton = $('.raptor-ui-embed-dialog button:contains(Embed Object)');
-            embedObjectButton.trigger('click');
-
-            rangesToTokens(rangy.getSelection().getAllRanges());
-        });
-        });
+        testEditor('.test-2', testEmbed);
     </script>
 
     <div class="test-3">
@@ -119,20 +106,7 @@
         </div>
     </div>
     <script type="text/javascript">
-        testEditor('.test-3', function(input) {
-            var embedButton = getLayoutElement(input).find('.raptor-ui-embed');
-            embedButton.trigger('click');
-
-            var video = 'http://www.youtube.com/watch?v=KQbySwxU8L4';
-
-            var textarea = $(document.querySelector('.raptor-ui-embed-tab > textarea'));
-            textarea.value = video;
-
-            var embedObjectButton = $('.raptor-ui-embed-dialog button:contains(Embed Object)');
-            embedObjectButton.trigger('click');
-
-            rangesToTokens(rangy.getSelection().getAllRanges());
-        });
+        testEditor('.test-3', testEmbed);
     </script>
 
      <div class="test-4">
@@ -166,20 +140,7 @@
         </div>
     </div>
     <script type="text/javascript">
-        testEditor('.test-4', function(input) {
-            var embedButton = getLayoutElement(input).find('.raptor-ui-embed');
-            embedButton.trigger('click');
-
-            var video = 'http://www.youtube.com/watch?v=KQbySwxU8L4';
-
-            var textarea = $(document.querySelector('.raptor-ui-embed-tab > textarea'));
-            textarea.value = video;
-
-            var embedObjectButton = $('.raptor-ui-embed-dialog button:contains(Embed Object)');
-            embedObjectButton.trigger('click');
-
-            rangesToTokens(rangy.getSelection().getAllRanges());
-        });
+        testEditor('.test-4', testEmbed);
     </script>
 
     <div class="test-5">
@@ -206,66 +167,7 @@
         </div>
     </div>
     <script type="text/javascript">
-        testEditor('.test-5', function(input) {
-            var embedButton = getLayoutElement(input).find('.raptor-ui-embed');
-            embedButton.trigger('click');
-
-            var video = 'http://www.youtube.com/watch?v=KQbySwxU8L4';
-
-            var textarea = $(document.querySelector('.raptor-ui-embed-tab > textarea'));
-            textarea.value = video;
-
-            var embedObjectButton = $('.raptor-ui-embed-dialog button:contains(Embed Object)');
-            embedObjectButton.trigger('click');
-
-            rangesToTokens(rangy.getSelection().getAllRanges());
-        });
-    </script>
-
-    <div class="test-6">
-        <h1>Embed Object Button 6: Empty Selection in Word</h1>
-        <div class="test-input">
-            <div class="editable">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-                    convallis dui id erat pellentesque et rhoncus nunc semper. Suspendisse
-                    malesuada hendrerit velit nec tristique.
-                </p><p>
-                    Aliquam gravida mauris at
-                    ligula venenatis rhoncus. Suspen{}disse interdum, nisi nec consectetur
-                    pulvinar, lorem augue ornare felis, vel lacinia erat nibh in velit.
-                </p>
-            </div>
-        </div>
-        <div class="test-expected">
-            <div class="editable">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-                    convallis dui id erat pellentesque et rhoncus nunc semper. Suspendisse
-                    malesuada hendrerit velit nec tristique.
-                </p><p>
-                    Aliquam gravida mauris at
-                    ligula venenatis rhoncus. <video src="http://www.youtube.com/watch?v=KQbySwxU8L4" controls></video>{} interdum, nisi nec consectetur
-                    pulvinar, lorem augue ornare felis, vel lacinia erat nibh in velit.
-                </p>
-            </div>
-        </div>
-    </div>
-    <script type="text/javascript">
-        testEditor('.test-6', function(input) {
-            var embedButton = getLayoutElement(input).find('.raptor-ui-embed');
-            embedButton.trigger('click');
-
-            var video = 'http://www.youtube.com/watch?v=KQbySwxU8L4';
-
-            var textarea = $(document.querySelector('.raptor-ui-embed-tab > textarea'));
-            textarea.value = video;
-
-            var embedObjectButton = $('.raptor-ui-embed-dialog button:contains(Embed Object)');
-            embedObjectButton.trigger('click');
-
-            rangesToTokens(rangy.getSelection().getAllRanges());
-        });
+        testEditor('.test-5', testEmbed);
     </script>
 
     <div class="test-7">
@@ -298,20 +200,7 @@
         </div>
     </div>
     <script type="text/javascript">
-        testEditor('.test-7', function(input) {
-            var embedButton = getLayoutElement(input).find('.raptor-ui-embed');
-            embedButton.trigger('click');
-
-            var video = 'http://www.youtube.com/watch?v=KQbySwxU8L4';
-
-            var textarea = $(document.querySelector('.raptor-ui-embed-tab > textarea'));
-            textarea.value = video;
-
-            var embedObjectButton = $('.raptor-ui-embed-dialog button:contains(Embed Object)');
-            embedObjectButton.trigger('click');
-
-            rangesToTokens(rangy.getSelection().getAllRanges());
-        });
+        testEditor('.test-7', testEmbed);
     </script>
 </body>
 </html>
