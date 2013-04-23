692d0629781f52373e6f9bbe27a46c0a354ccea4
David Neilsen <david@panmedia.co.nz>
2013-04-23 16:42:14 +1200

    Use JS trim

--- tests/js/beautify-html.js
+++ tests/js/beautify-html.js
@@ -1,32 +1,3 @@
-function trim(str, charlist) {
-    var whitespace, l = 0,
-    i = 0;
-    str += '';
-    if (!charlist) {
-        // default list
-        whitespace = " \n\r\t\f\x0b\xa0\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u200b\u2028\u2029\u3000";
-    } else {
-        // preg_quote custom list
-        charlist += '';
-        whitespace = charlist.replace(/([\[\]\(\)\.\?\/\*\{\}\+\$\^\:])/g, '$1');
-    }
-    l = str.length;
-    for (i = 0; i < l; i++) {
-        if (whitespace.indexOf(str.charAt(i)) === -1) {
-            str = str.substring(i);
-            break;
-        }
-    }
-    l = str.length;
-    for (i = l - 1; i >= 0; i--) {
-        if (whitespace.indexOf(str.charAt(i)) === -1) {
-            str = str.substring(0, i + 1);
-            break;
-        }
-    }
-    return whitespace.indexOf(str.charAt(0)) === -1 ? str : '';
-}
-
 function getIndent(level) {
     var result = '',
         i = level * 4;
@@ -40,7 +11,7 @@ function getIndent(level) {
 }
 
 function style_html(html) {
-    html = trim(html);
+    html = html.trim();
     var result = '',
         indentLevel = 0,
         tokens = html.split(/</);
@@ -59,9 +30,9 @@ function style_html(html) {
                 result += '<';
             }
 
-            result += trim(parts[0]) + ">\n";
-            if (trim(parts[1]) !== '') {
-                result += getIndent(indentLevel) + trim(parts[1]).replace(/\s+/g, ' ') + "\n";
+            result += parts[0].trim() + ">\n";
+            if (parts[1].trim() !== '') {
+                result += getIndent(indentLevel) + parts[1].trim().replace(/\s+/g, ' ') + "\n";
             }
 
             if (parts[0].match(/^(img|hr|br)/)) {
