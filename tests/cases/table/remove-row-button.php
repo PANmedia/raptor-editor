effd836da250cc0c67c9f782eb209919b763254b
David Neilsen <david@panmedia.co.nz>
2013-04-23 16:32:45 +1200

    Fix test tests/cases/table/remove-row-button.php

--- tests/cases/table/remove-row-button.php
+++ tests/cases/table/remove-row-button.php
@@ -17,7 +17,7 @@
                     </tr>
                     <tr>
                         <td>Cell 0,1</td>
-                        <td>Cell 1,1</td>
+                        <td>Cell 1,1{}</td>
                         <td>Cell 2,1</td>
                     </tr>
                     <tr>
@@ -33,32 +33,22 @@
                 <table>
                     <tr>
                         <td>Cell 0,0</td>
-                        <td></td>
                         <td>Cell 1,0</td>
                         <td>Cell 2,0</td>
                     </tr>
                     <tr>
-                        <td>Cell 0,1</td>
-                        <td></td>
-                        <td>Cell 1,1</td>
-                        <td>Cell 2,1</td>
-                    </tr>
-                    <tr>
                         <td>Cell 0,2</td>
-                        <td></td>
                         <td>Cell 1,2</td>
                         <td>Cell 2,2</td>
                     </tr>
             </table>
             </div>
         </div>
-        <div class="test-output"></div>
-        <div class="test-diff"></div>
     </div>
     <script type="text/javascript">
         testEditor('.test-1', function(input) {
-            clickButton(input, '.raptor-ui-table-remove-row');
-            rangesToTokens(rangy.getSelection().getAllRanges());
+            var ranges = tokensToRanges(input);
+            clickButton(input, '.raptor-ui-table-delete-row');
         });
     </script>
 
@@ -81,7 +71,7 @@
                         <tr>
                             <td>Cell</td>
                             <td>Cell</td>
-                            <td>Cell</td>
+                            <td>Cell{}</td>
                             <td rowspan="2">Cell</td>
                             <td>Cell</td>
                             <td>Cell</td>
@@ -142,7 +132,6 @@
                         <tr>
                             <th>Header</th>
                             <th>Header</th>
-                            <th></th>
                             <th>Header</th>
                             <th>Header</th>
                             <th>Header</th>
@@ -153,30 +142,21 @@
                         <tr>
                             <td>Cell</td>
                             <td>Cell</td>
-                            <td></td>
                             <td>Cell</td>
-                            <td rowspan="2">Cell</td>
+                            <td rowspan="1">Cell</td>
                             <td>Cell</td>
                             <td>Cell</td>
                         </tr>
                         <tr>
+                            <td>Cell</td>
                             <td>Cell</td>
                             <td colspan="2">Cell</td>
-                            <td></td>
-                            <td rowspan="2">Cell</td>
+                            <td rowspan="1">Cell</td>
                             <td>Cell</td>
                         </tr>
                         <tr>
                             <td>Cell</td>
                             <td>Cell</td>
-                            <td></td>
-                            <td colspan="2">Cell</td>
-                            <td>Cell</td>
-                        </tr>
-                        <tr>
-                            <td>Cell</td>
-                            <td>Cell</td>
-                            <td></td>
                             <td>Cell</td>
                             <td>Cell</td>
                             <td>Cell</td>
@@ -185,7 +165,6 @@
                         <tr>
                             <td>Cell</td>
                             <td colspan="2">Cell</td>
-                            <td></td>
                             <td>Cell</td>
                             <td>Cell</td>
                             <td>Cell</td>
@@ -193,7 +172,6 @@
                         <tr>
                             <td>Cell</td>
                             <td>Cell</td>
-                            <td></td>
                             <td>Cell</td>
                             <td>Cell</td>
                             <td>Cell</td>
@@ -204,7 +182,6 @@
                         <tr>
                             <th>Header</th>
                             <td>Cell</td>
-                            <td></td>
                             <td>Cell</td>
                             <td>Cell</td>
                             <td>Cell</td>
@@ -214,13 +191,11 @@
                 </table>
             </div>
         </div>
-        <div class="test-output"></div>
-        <div class="test-diff"></div>
     </div>
      <script type="text/javascript">
         testEditor('.test-2', function(input) {
-            clickButton(input, '.raptor-ui-table-remove-row');
-            rangesToTokens(rangy.getSelection().getAllRanges());
+            var ranges = tokensToRanges(input);
+            clickButton(input, '.raptor-ui-table-delete-row');
         });
     </script>
 </body>
