72ac8ef803dca974013860bd23404916725e6de2
David Neilsen <david@panmedia.co.nz>
2013-04-23 16:35:40 +1200

    Fix test tests/cases/dock/dock-to-screen-button.php

--- tests/cases/dock/dock-to-screen-button.php
+++ tests/cases/dock/dock-to-screen-button.php
@@ -13,148 +13,31 @@
         <div class="test-input">
             <div class="editable">
                 <p>
-                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-                   convallis dui id erat pellentesque et rhoncus nunc semper. Suspendisse
-                   malesuada hendrerit velit nec tristique. Aliquam gravida mauris at
-                   ligula venenatis rhoncus. Suspendisse interdum, nisi nec consectetur
-                   pulvinar, lorem augue ornare felis, vel lacinia erat nibh in velit.
-               </p>
-               <p>
-                   Hendrerit, felis ac fringilla lobortis, massa ligula aliquet justo, sit
-                   amet tincidunt enim quam sollicitudin nisi. Maecenas ipsum augue,
-                   commodo sit amet aliquet ut, laoreet ut nunc. Vestibulum ante ipsum
-                   primis in faucibus orci luctus et ultrices posuere cubilia Curae;
-                   Pellentesque tincidunt eros quis tellus laoreet ac dignissim turpis
-                   luctus. Integer nunc est, pulvinar ac tempor ac, pretium ut odio.
-               </p>
-               <p>
-                   Pellentesque in arcu sit amet odio scelerisque tincidunt. Lorem ipsum
-                   dolor sit amet, consectetur adipiscing elit. Pellentesque habitant morbi
-                   tristique senectus et netus et malesuada fames ac turpis egestas.
-               </p>
+                   Some Content.
+                </p>
             </div>
         </div>
         <div class="test-expected">
             <div class="editable">
                 <p>
-                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-                   convallis dui id erat pellentesque et rhoncus nunc semper. Suspendisse
-                   malesuada hendrerit velit nec tristique. Aliquam gravida mauris at
-                   ligula venenatis rhoncus. Suspendisse interdum, nisi nec consectetur
-                   pulvinar, lorem augue ornare felis, vel lacinia erat nibh in velit.
-               </p>
-               <p>
-                   Hendrerit, felis ac fringilla lobortis, massa ligula aliquet justo, sit
-                   amet tincidunt enim quam sollicitudin nisi. Maecenas ipsum augue,
-                   commodo sit amet aliquet ut, laoreet ut nunc. Vestibulum ante ipsum
-                   primis in faucibus orci luctus et ultrices posuere cubilia Curae;
-                   Pellentesque tincidunt eros quis tellus laoreet ac dignissim turpis
-                   luctus. Integer nunc est, pulvinar ac tempor ac, pretium ut odio.
-               </p>
-               <p>
-                   Pellentesque in arcu sit amet odio scelerisque tincidunt. Lorem ipsum
-                   dolor sit amet, consectetur adipiscing elit. Pellentesque habitant morbi
-                   tristique senectus et netus et malesuada fames ac turpis egestas.
-               </p>
+                   Some Content.
+                </p>
             </div>
         </div>
     </div>
     <script type="text/javascript">
         testEditor('.test-1', function(input) {
             var dockToScreen = getLayoutElement(input).find('.raptor-ui-dock-to-screen');
-            dockToScreen.trigger('click');
-            rangesToTokens(rangy.getSelection().getAllRanges());
-
             if (!dockToScreen.is('.ui-state-highlight')) {
-                throw new Error('Button not active')
+                throw new Error('Button not active (Raptor should have started docked)')
             }
-        });
-    </script>
-    <div class="test-2">
-        <h1>Dock to Screen Button 2: Load Page With toolbar docked to screen</h1>
-        <div class="test-input">
-            <div class="editable">
-                <p>
-                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-                   convallis dui id erat pellentesque et rhoncus nunc semper. Suspendisse
-                   malesuada hendrerit velit nec tristique. Aliquam gravida mauris at
-                   ligula venenatis rhoncus. Suspendisse interdum, nisi nec consectetur
-                   pulvinar, lorem augue ornare felis, vel lacinia erat nibh in velit.
-               </p>
-               <p>
-                   Hendrerit, felis ac fringilla lobortis, massa ligula aliquet justo, sit
-                   amet tincidunt enim quam sollicitudin nisi. Maecenas ipsum augue,
-                   commodo sit amet aliquet ut, laoreet ut nunc. Vestibulum ante ipsum
-                   primis in faucibus orci luctus et ultrices posuere cubilia Curae;
-                   Pellentesque tincidunt eros quis tellus laoreet ac dignissim turpis
-                   luctus. Integer nunc est, pulvinar ac tempor ac, pretium ut odio.
-               </p>
-               <p>
-                   Pellentesque in arcu sit amet odio scelerisque tincidunt. Lorem ipsum
-                   dolor sit amet, consectetur adipiscing elit. Pellentesque habitant morbi
-                   tristique senectus et netus et malesuada fames ac turpis egestas.
-               </p>
-            </div>
-        </div>
-        <div class="test-expected">
-            <div class="editable">
-                <p>
-                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-                   convallis dui id erat pellentesque et rhoncus nunc semper. Suspendisse
-                   malesuada hendrerit velit nec tristique. Aliquam gravida mauris at
-                   ligula venenatis rhoncus. Suspendisse interdum, nisi nec consectetur
-                   pulvinar, lorem augue ornare felis, vel lacinia erat nibh in velit.
-               </p>
-               <p>
-                   Hendrerit, felis ac fringilla lobortis, massa ligula aliquet justo, sit
-                   amet tincidunt enim quam sollicitudin nisi. Maecenas ipsum augue,
-                   commodo sit amet aliquet ut, laoreet ut nunc. Vestibulum ante ipsum
-                   primis in faucibus orci luctus et ultrices posuere cubilia Curae;
-                   Pellentesque tincidunt eros quis tellus laoreet ac dignissim turpis
-                   luctus. Integer nunc est, pulvinar ac tempor ac, pretium ut odio.
-               </p>
-               <p>
-                   Pellentesque in arcu sit amet odio scelerisque tincidunt. Lorem ipsum
-                   dolor sit amet, consectetur adipiscing elit. Pellentesque habitant morbi
-                   tristique senectus et netus et malesuada fames ac turpis egestas.
-               </p>
-            </div>
-        </div>
-    </div>
-    <script type="text/javascript">
-        testEditor('.test-2', function(input) {
-            var dockToScreen = getLayoutElement(input).find('.raptor-ui-dock-to-screen');
             dockToScreen.trigger('click');
-            rangesToTokens(rangy.getSelection().getAllRanges());
-
-            //if the toolbar is already docked
-                //undock it and re dock it
-                //if there are two white space divs then throw an error
-
-           //else
-                //dock the toolbar dockToScreen.triggrt('click');
-
-            var dockState = null;
-            function undock() {
-                if (dockState) {
-                    var toolbar = undockFromScreen(dockState);
-                    toolbar.prependTo('.wrapper');
-                    dockState = null;
-                }
+            if (dockToScreen.is('.ui-state-highlight')) {
+                throw new Error('Button is active (Raptor should be undocked)')
             }
-            $('[name=undock]').click(undock);
-            $('[name=dock-to]').click(function() {
-                undock();
-                dockState = dockToScreen($('.element-to-dock'), {
-                    position: $(this).data('position'),
-                    spacer: $(this).data('spacer')
-                });
-            });
-
-            //some code to check whether the editor is loading docked to the screen
-
+            dockToScreen.trigger('click');
             if (!dockToScreen.is('.ui-state-highlight')) {
-                throw new Error('Button not active')
+                throw new Error('Button not active (Raptor should be docked)')
             }
         });
     </script>
