64355dba7c741a9642b27d1e54172c5a83545b7d
In4matik <in4matik@hotmail.fr>
2013-06-18 09:40:31 +0200

    Translated locale `fr`

--- src/locales/fr.js
+++ src/locales/fr.js
@@ -14,44 +14,44 @@ registerLocale('fr', 'Français', {
     cancelDialogCancelButton: 'Fermer',
     cancelDialogContent: 'Êtes-vous sûr(e) de vouloir arrêter la modification ? Toutes les modifications seront perdues !',
     cancelDialogOKButton: 'OK',
-    cancelDialogTitle: 'Cancel Editing',
-    cancelTitle: 'Cancel editing',
+    cancelDialogTitle: 'Annuler l\'édition',
+    cancelTitle: 'Annuler l\'édition',
 
-    clearFormattingTitle: 'Clear Formatting',
+    clearFormattingTitle: 'Effacer la mise en forme',
     clickButtonToEditPluginButton: 'Cliquer pour commencer la modification',
 
-    colorMenuBasicAutomatic: 'Automatic',
-    colorMenuBasicBlack: 'Black',
-    colorMenuBasicBlue: 'Blue',
-    colorMenuBasicGreen: 'Green',
-    colorMenuBasicGrey: 'Grey',
+    colorMenuBasicAutomatic: 'Automatique',
+    colorMenuBasicBlack: 'Noir',
+    colorMenuBasicBlue: 'Bleu',
+    colorMenuBasicGreen: 'Vert',
+    colorMenuBasicGrey: 'Gris',
     colorMenuBasicOrange: 'Orange',
-    colorMenuBasicPurple: 'Purple',
-    colorMenuBasicRed: 'Red',
-    colorMenuBasicTitle: 'Change text color',
-    colorMenuBasicWhite: 'White',
+    colorMenuBasicPurple: 'Violet',
+    colorMenuBasicRed: 'Rouge',
+    colorMenuBasicTitle: 'Changer le couleur du texte',
+    colorMenuBasicWhite: 'Blanc',
 
-    dockToElementTitle: 'Dock editor to element',
-    dockToScreenTitle: 'Dock editor to screen',
+    dockToElementTitle: 'Fixer l\'éditeur à l\'élément',
+    dockToScreenTitle: 'Fixer l\'éditeur à l\'écran',
 
     embedTitle: 'Object intégré',
 
-    errorPluginNoName: 'Plugin "{{plugin}}" is invalid (must have a name property)',
-    errorPluginNotObject: 'Plugin "{{plugin}}" is invalid (must be an object)',
-    errorPluginOverride: 'Plugin "{{name}}" has already been registered, and will be overwritten',
+    errorPluginNoName: 'Plugin "{{plugin}}" est invalide (doit posséder une propriété "name")',
+    errorPluginNotObject: 'Plugin "{{plugin}}" est invalide (doit être un objet)',
+    errorPluginOverride: 'Plugin "{{name}}" a déjà été défini, et sera écrasé',
 
-    errorUINoName: 'UI "{{ui}}" is invalid (must have a name property)',
-    errorUINotObject: 'UI "{{ui}}" is invalid (must be an object)',
-    errorUIOverride: 'UI "{{name}}" has already been registered, and will be overwritten',
+    errorUINoName: 'UI "{{ui}}" est invalide (doit posséder une propriété "name")',
+    errorUINotObject: 'UI "{{ui}}" est invalide (doit être un objet)',
+    errorUIOverride: 'UI "{{name}}" a déjà été définie, et sera écrasée',
 
-    floatLeftTitle: 'Float Image Left',
-    floatNoneTitle: 'Remove image align',
-    floatRightTitle: 'Float Image Right',
+    floatLeftTitle: 'Image à gauche du texte',
+    floatNoneTitle: 'Retirer l\'aligement de l\'image',
+    floatRightTitle: 'Image à droite du texte',
 
     guidesTitle: 'Afficher les guides',
 
-    historyRedoTitle: 'Redo',
-    historyUndoTitle: 'Undo',
+    historyRedoTitle: 'Rétablir',
+    historyUndoTitle: 'Annuler',
 
     listOrderedTitle: 'Liste ordonnée',
     listUnorderedTitle: 'Liste non ordonnée',
@@ -68,8 +68,8 @@ registerLocale('fr', 'Français', {
     linkTypeEmailSubjectLabel: 'Sujet (facultatif):',
     linkTypeEmailSubjectPlaceHolder: 'Saisir le sujet',
 
-    linkTypeExternalLabel: 'Page on another website',
-    linkTypeExternalHeader: 'Link to a page on another website',
+    linkTypeExternalLabel: 'Page sur une autre site',
+    linkTypeExternalHeader: 'Lien vers une page sur un autre site',
     linkTypeExternalLocationLabel: 'Emplacement',
     linkTypeExternalNewWindowHeader: 'Nouvelle fenêtre',
     linkTypeExternalNewWindowLabel: 'Cochez cette case pour ouvrir le lien dans une nouvelle fenêtre de navigateur.',
@@ -77,11 +77,11 @@ registerLocale('fr', 'Français', {
         '<h2>Pas sûr(e) de savoir quoi mettre dans le champ ci-dessus ?</h2>' +
         '<ol>' +
         '    <li>Trouvez la page web que vous voulez lier.</li>' +
-        '    <li>Copy the web address from your browser\'s address bar and paste it into the box above.</li>' +
+        '    <li>Copiez l\'adresse de la bar d\'adresse de votre navigateur et collez la dans le champs ci-dessous.</li>' +
         '</ol>',
 
-    linkTypeInternalLabel: 'Page on this website',
-    linkTypeInternalHeader: 'Link to a page on this website',
+    linkTypeInternalLabel: 'Page de ce site',
+    linkTypeInternalHeader: 'Lien vers une page de ce site',
     linkTypeInternalLocationLabel: 'Emplacement',
     linkTypeInternalNewWindowHeader: 'Nouvelle fenêtre',
     linkTypeInternalNewWindowLabel: 'Cochez cette case pour ouvrir le lien dans une nouvelle fenêtre de navigateur.',
@@ -89,7 +89,7 @@ registerLocale('fr', 'Français', {
         '<h2>Pas sûr(e) de savoir quoi mettre dans le champ ci-dessus ?</h2>' +
         '<ol>' +
         '    <li>Trouvez la page web que vous voulez lier.</li>' +
-        '    <li>Copy the web address from your browser\'s address bar, excluding "{{domain}}" and paste it into the box above.</li>' +
+        '    <li>Copiez l\'adresse de la bar d\'adresse de votre navigateur, sans "{{domain}}" et collez la dans le champs ci-dessous..</li>' +
         '</ol>',
 
     logoTitle: 'En savoir plus sur l\'éditeur WYSIWYG Raptor',
@@ -102,26 +102,26 @@ registerLocale('fr', 'Français', {
     statisticsDialogCharactersRemaining: '{{characters}} caractères, {{charactersRemaining}} restants',
     statisticsDialogNotTruncated: 'Le contenu ne sera pas tronqué',
     statisticsDialogOKButton: 'OK',
-    statisticsDialogSentence: '{{sentences}} sentence',
+    statisticsDialogSentence: '{{sentences}} phrase',
     statisticsDialogSentences: '{{sentences}} phrases',
     statisticsDialogTitle: 'Statistiques de contenu',
     statisticsDialogTruncated: 'Le contenu contient plus de {{limit}} caractères et peut être tronqué',
     statisticsDialogWord: '{{words}} mot',
     statisticsDialogWords: '{{words}} mots',
-    statisticsTitle: 'Click to view statistics',
+    statisticsTitle: 'Cliquez pour voir les statistiques',
 
-    tableCreateTitle: 'Create table',
-    tableDeleteColumnTitle: 'Delete table column',
-    tableDeleteRowTitle: 'Delete table row',
-    tableInsertColumnTitle: 'Insert table column',
-    tableInsertRowTitle: 'Insert table row',
+    tableCreateTitle: 'Créer un tableau',
+    tableDeleteColumnTitle: 'Supprimer une colonne du tableau',
+    tableDeleteRowTitle: 'Supprimer une ligne du tableau',
+    tableInsertColumnTitle: 'Insérer une colonne',
+    tableInsertRowTitle: 'Insérer une ligne',
 
     tagMenuTagH1: 'Titre&nbsp;1',
     tagMenuTagH2: 'Titre&nbsp;2',
     tagMenuTagH3: 'Titre&nbsp;3',
     tagMenuTagNA: 'N/A',
     tagMenuTagP: 'Paragraphe',
-    tagMenuTitle: 'Change element style',
+    tagMenuTitle: 'Modifier le style de l\'élément',
 
     tagTreeElementLink: 'Sélectionner l\'élément {{element}}',
     tagTreeElementTitle: 'Cliquer pour sélectionner le contenu de l\'élément "{{element}}"',
@@ -137,10 +137,10 @@ registerLocale('fr', 'Français', {
     textSuperTitle: 'Exposant',
     textUnderlineTitle: 'Souligné',
 
-    unsavedEditWarningText: 'There are unsaved changes on this page',
+    unsavedEditWarningText: 'Il y a des modifications non enregistrées sur cette page',
 
     viewSourceDialogCancelButton: 'Fermer',
     viewSourceDialogOKButton: 'Appliquer la source code',
-    viewSourceDialogTitle: 'Content source code',
+    viewSourceDialogTitle: 'Code source du contenu',
     viewSourceTitle: 'Voir / Modifier la source code'
 });
