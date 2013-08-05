6cb1373b4a8963f54a9d35f027d0840baa824768
David Neilsen <david@panmedia.co.nz>
2013-08-05 13:48:43 +1200

    Add gold to php/functions/load-raptor.php

--- php/functions/load-raptor.php
+++ php/functions/load-raptor.php
@@ -3,4 +3,5 @@ function loadRaptor() {
     require_once RAPTOR_COMMON_DIR . 'src/include.php';
     require_once RAPTOR_EDITOR_DIR . 'src/include.php';
     require_once RAPTOR_FILE_MANAGER_DIR . 'src/include.php';
+    require_once RAPTOR_GOLD_DIR . 'src/include.php';
 }
