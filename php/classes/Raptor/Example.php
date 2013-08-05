87d91c22439daaf3247c8b8dc1706bf3b42f6eb7
David Neilsen <david@panmedia.co.nz>
2013-08-05 14:20:54 +1200

    Add saveJson to php/classes/Raptor/Example.php

--- php/classes/Raptor/Example.php
+++ php/classes/Raptor/Example.php
@@ -46,4 +46,32 @@ class Example {
         return ob_get_clean();
     }
 
+    public function saveJson($file) {
+        if (isset($_POST['raptor-content'])) {
+            $content = [];
+            if (file_exists($file)) {
+                $content = file_get_contents($file);
+                $content = json_decode($content, true);
+                if ($content === false) {
+                    $content = [];
+                }
+            }
+
+            $newContent = json_decode($_POST['raptor-content']);
+            if ($newContent) {
+                foreach ($newContent as $id => $html) {
+                    $content[$id] = $html;
+                }
+            }
+
+            $content = json_encode($content, JSON_PRETTY_PRINT);
+            if ($content !== false) {
+                if (file_put_contents($file, $content)) {
+                    return json_encode(true);
+                }
+            }
+        }
+        return json_encode(false);
+    }
+
 }
