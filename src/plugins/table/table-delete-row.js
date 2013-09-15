526d5408a50886c34520e28bfa9f7fabfc113499
David Neilsen <david@panmedia.co.nz>
2013-09-16 09:04:28 +1200

    Fix delete table row

--- src/plugins/table/table-delete-row.js
+++ src/plugins/table/table-delete-row.js
@@ -13,6 +13,6 @@
 Raptor.registerUi(new TableCellButton({
     name: 'tableDeleteRow',
     applyToElement: function(cell) {
-        tableDeleteRow(cell.parentNode.parentNode.parentNode, tableGetCellIndex(cell).x);
+        tableDeleteRow(cell.parentNode.parentNode.parentNode, tableGetCellIndex(cell).y);
     }
 }));
