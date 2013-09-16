c1c137773d625d4d0ad206e69863a3b7eb59ec0e
David Neilsen <david@panmedia.co.nz>
2013-09-16 13:37:02 +1200

    Fix reselection on remove row/column in tables, remove table if empty.

--- src/plugins/table/table-delete-row.js
+++ src/plugins/table/table-delete-row.js
@@ -13,6 +13,21 @@
 Raptor.registerUi(new TableCellButton({
     name: 'tableDeleteRow',
     applyToElement: function(cell) {
-        tableDeleteRow(cell.parentNode.parentNode.parentNode, tableGetCellIndex(cell).y);
+        var position = tableGetCellIndex(cell),
+            table = cell.parentNode.parentNode.parentNode,
+            nextCell;
+        tableDeleteRow(cell.parentNode.parentNode.parentNode, position.y);
+        if (tableIsEmpty(table)) {
+            table.parentNode.removeChild(table);
+            return;
+        }
+        nextCell = tableGetCellByIndex(table, position);
+        if (!nextCell && position.y > 0) {
+            nextCell = tableGetCellByIndex(table, {
+                x: position.x,
+                y: position.y - 1
+            });
+        }
+        selectionSelectInner(nextCell);
     }
 }));
