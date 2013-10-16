210deaff74e6b2913f36755d56297d967c93b488
David Neilsen <david@panmedia.co.nz>
2013-10-17 10:50:53 +1300

    Update tests

--- tests/cases/history-actions/undo.php
+++ tests/cases/history-actions/undo.php
@@ -3,7 +3,7 @@
 <html>
 <head>
     <script type="text/javascript" src="../../js/case.js"></script>
-    <?php $uri = '../../../src/'; include __DIR__ . '/../../../src/include.php'; ?>
+    <?php include __DIR__ . '/../../include.php'; ?>
 </head>
 <body class="simple">
     <script type="text/javascript">
